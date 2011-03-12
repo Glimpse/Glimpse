88cbffb23b5482048a06dd40285ed6ec98f62faf
nikmd23 <nikmd23@gmail.com>
2011-03-11 22:39:14 -0500

    Merged Config plugin's into one

---
+++ source/Glimpse.Net/Plugin/Configuration/Config.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Web;
+using Glimpse.Protocol;
+
+namespace Glimpse.Net.Plugin.Configuration
+{
+    [GlimpsePlugin]
+    public class Config : NameValueCollectionPlugin
+    {
+        public override string Name
+        {
+            get { return "Config"; }
+        }
+
+        public override object GetData(HttpApplication application)
+        {
+            var ConnectionStrings = new Dictionary<string, string>();
+            foreach (ConnectionStringSettings item in ConfigurationManager.ConnectionStrings)
+            {
+                ConnectionStrings.Add(item.Name, item.ConnectionString);
+            }
+
+            if (ConnectionStrings.Count == 0) return null;
+
+            return new
+                       {
+                           AppSettings = Process(ConfigurationManager.AppSettings, application),
+                           ConnectionStrings
+                       };
+        }
+    }
+}
