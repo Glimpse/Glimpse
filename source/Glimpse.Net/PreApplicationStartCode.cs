a125abb33a1de6c186bc0a5bbaa3f9764e059183
nikmd23 <nikmd23@gmail.com>
2011-03-28 22:44:11 -0400

    Using PreApplicationStartCode now to reduce we.config HttpModules section noise.

---
+++ source/Glimpse.Net/PreApplicationStartCode.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+namespace Glimpse.Net
+{
+    public class PreApplicationStartCode
+    {
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(Module));
+        }
+    }
+}
