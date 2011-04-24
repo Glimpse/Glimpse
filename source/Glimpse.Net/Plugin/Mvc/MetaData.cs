bb5f34cb162a2747fa14ca97160985edad31e932
avanderhoorn <anthony.vanderhoorn@gmail.com>
2011-04-24 08:58:57 -0400

    JS UI Client - Update - Stability improvements to deal with when no meta data is available and bolding of no default values

--- source/Glimpse.Net/Plugin/Mvc/MetaData.cs
+++ source/Glimpse.Net/Plugin/Mvc/MetaData.cs
@@ -41,33 +41,40 @@ namespace Glimpse.Net.Plugin.Mvc
                     var viewContext = callMetadata.GlimpseView.ViewContext;
                     var metaData = viewContext.ViewData.ModelMetadata;
 
-                    object detailResult = new
+                    object detailResult = null;
+                    string meatDataType = null;
+
+                    if (metaData != null)
+                    {
+                        detailResult = new
                                               {
-                                                  metaData.ConvertEmptyStringToNull, //A flag which indicates whether empty strings that are posted back in forms should be converted into NULLs. Default: true
-                                                  metaData.DataTypeName, //A string which can used to give meta information about the data type (for example, to let you know that this string is actually an e-mail address). Some well-known data type names include “EmailAddress”, “Html”, “Password”, and “Url”. Default: null
-                                                  metaData.Description, //A long-form textual description of this model. Default: null
-                                                  metaData.DisplayFormatString, //A format string that will be used when displaying this model value in a template. Default: null
-                                                  metaData.DisplayName, //The display name of this model value. Used in templates and Html.Label/LabelFor to generate the label text. Default: null
-                                                  metaData.EditFormatString, //A format string that will be used when editing this model value in a template. Default: null
-                                                  metaData.HideSurroundingHtml, //A flag which indicates that this field should not have any of its surrounding HTML (for example, a label). Often used when a template will be generating a hidden input. Default: null
+                                                  ConvertEmptyStringToNull = ParseValue(metaData.ConvertEmptyStringToNull, true), //A flag which indicates whether empty strings that are posted back in forms should be converted into NULLs. Default: true
+                                                  DataTypeName = ParseValue(metaData.DataTypeName, null), //A string which can used to give meta information about the data type (for example, to let you know that this string is actually an e-mail address). Some well-known data type names include “EmailAddress”, “Html”, “Password”, and “Url”. Default: null
+                                                  Description = ParseValue(metaData.Description, null), //A long-form textual description of this model. Default: null
+                                                  DisplayFormatString = ParseValue(metaData.DisplayFormatString, null), //A format string that will be used when displaying this model value in a template. Default: null
+                                                  DisplayName = ParseValue(metaData.DisplayName, null), //The display name of this model value. Used in templates and Html.Label/LabelFor to generate the label text. Default: null
+                                                  EditFormatString = ParseValue(metaData.EditFormatString, null), //A format string that will be used when editing this model value in a template. Default: null
+                                                  HideSurroundingHtml = ParseValue(metaData.HideSurroundingHtml, false), //A flag which indicates that this field should not have any of its surrounding HTML (for example, a label). Often used when a template will be generating a hidden input. Default: null
                                                   metaData.IsComplexType, //A flag which indicates whether the system considers this to be a complex type (and therefore will default to the complex object template rather than the string template). Not user-settable
                                                   metaData.IsNullableValueType, //A flag which indicates whether the model is a nullable value type (namely, Nullable<T>). Not user-settable
-                                                  metaData.IsReadOnly, //A flag which indicates if this value is read-only (for example, because the property does not have a setter). Default: false
-                                                  metaData.IsRequired, //A flag which indicates if this value is required. Default: true for non-nullable value types; false for all others.
-                                                  metaData.NullDisplayText, //The text which should be used when attempting to display a null model. Default: null
-                                                  metaData.ShortDisplayName, //The short display name of this mode value. Intended to be used in the title of tabular list views. If this field is null, then DisplayName should be used. Default: null
-                                                  metaData.ShowForDisplay, //A flag which indicates if this model should be shown in display mode. Default: true
-                                                  metaData.ShowForEdit, //A flag which indicates if this model should be shown in edit mode. Default: true
+                                                  IsReadOnly = ParseValue(metaData.IsReadOnly, false), //A flag which indicates if this value is read-only (for example, because the property does not have a setter). Default: false
+                                                  IsRequired = ParseValue(metaData.IsRequired, metaData.IsNullableValueType), //A flag which indicates if this value is required. Default: true for non-nullable value types; false for all others.
+                                                  NullDisplayText = ParseValue(metaData.NullDisplayText, null), //The text which should be used when attempting to display a null model. Default: null
+                                                  ShortDisplayName = ParseValue(metaData.ShortDisplayName, null), //The short display name of this mode value. Intended to be used in the title of tabular list views. If this field is null, then DisplayName should be used. Default: null
+                                                  ShowForDisplay = ParseValue(metaData.ShowForDisplay, true), //A flag which indicates if this model should be shown in display mode. Default: true
+                                                  ShowForEdit = ParseValue(metaData.ShowForEdit, true), //A flag which indicates if this model should be shown in edit mode. Default: true
                                                   metaData.SimpleDisplayText, //Text which should be shown for this model when summarizing what would otherwise be a complex object display. Default: see below
-                                                  metaData.TemplateHint, //A string which indicates a hint as to what template should be used for this model. Default: null
-                                                  metaData.Watermark, //Text that might be displayed as a watermark when editing this model in a text box. Default: null
+                                                  TemplateHint = ParseValue(metaData.TemplateHint, null), //A string which indicates a hint as to what template should be used for this model. Default: null
+                                                  Watermark = ParseValue(metaData.Watermark, null), //Text that might be displayed as a watermark when editing this model in a text box. Default: null
                                               };
-
-
+                        meatDataType = metaData.ModelType.ToString();
+                    }
+                    else
+                        detailResult = "No MetaData found.";
 
                     result.Add(new[]
                                    {
-                                       "Primary View Model", metaData.ModelType.ToString(), detailResult
+                                       "Primary View Model", meatDataType, detailResult
                                    });
 
                     break;
@@ -75,6 +82,19 @@ namespace Glimpse.Net.Plugin.Mvc
             }
 
             return result;
+        }
+
+        protected object ParseValue(object currentValue, object defaultValue)
+        {
+            if (currentValue == null)
+                return currentValue;
+
+            var cur = currentValue.ToString();
+            var def = defaultValue == null ? "" : defaultValue.ToString();
+
+            if (cur != def)
+                return "*" + cur + "*";
+            return cur;
         }
 
         public void SetupInit(HttpApplication application)
