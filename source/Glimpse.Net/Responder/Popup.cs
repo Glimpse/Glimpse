5d34c7a120e2a5c6e2b6e8f241884112c66f2c66
avanderhoorn <anthony.vanderhoorn@gmail.com>
2011-05-08 11:37:41 -0400

    JS UI Client - Update - Icon style updates, start of info client plugin

--- source/Glimpse.Net/Responder/Popup.cs
+++ source/Glimpse.Net/Responder/Popup.cs
@@ -26,7 +26,7 @@ namespace Glimpse.Net.Responder
             var requestId = ""; //TODO need to get this id from somewhere
 
             response.Write("<!DOCTYPE html><html><head><title>Glimpse - Popup</title>");
-            response.Write("<style type='text/css'>html{color:#000;background:#FFF;} body{margin:0;padding:0;} .glimpse-holder { position:relative !important; display: block !important; } .glimpse-buttons, .glimpse-open { display:none !important; } .glimpse-panel { overflow:visible !important; }</style>");
+            response.Write("<style type='text/css'>html{color:#000;background:#FFF;} body{margin:0;padding:0;} .glimpse-holder { position:relative !important; display: block !important; } .glimpse-popout, .glimpse-popout, .glimpse-terminate, .glimpse-open { display:none !important; } .glimpse-panel { overflow:visible !important; }</style>");
             response.Write("</head><body>");
             response.Write(string.Format(@"<script type='text/javascript' id='glimpseData' data-glimpse-requestID='{1}'>var glimpse, glimpsePath = '{0}'</script>", path, requestId));
             response.Write("<script src='/Scripts/jquery-1.4.4.min.js' type='text/javascript'></script><script type='text/javascript' id='glimpseClient' src='/Glimpse/glimpseClient.js'></script>");
