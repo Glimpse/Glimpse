7c8055b53447c7c992a6e3afc280360f17177931
avanderhoorn <anthony.vanderhoorn@gmail.com>
2011-05-14 05:18:56 -0400

    Glimpse Site - Update - Started help documentation, landing page for help

---
+++ source/Glimpse.Site/Controllers/HelpController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Glimpse.Site.Controllers
+{
+    public partial class HelpController : Controller
+    {
+        //
+        // GET: /Help/
+
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+
+        public virtual ActionResult Plugin(string name)
+        {
+            return View();
+        }
+    }
+}
