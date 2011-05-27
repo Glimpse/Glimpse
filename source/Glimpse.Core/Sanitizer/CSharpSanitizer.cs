26e840486f3fc64ce70ef9fa3bae081ab31c4937
Adam Schroder <adamschroder@gmail.com>
2011-05-27 20:48:26 +1000

    Recursively replace [] associated with string version of generics

--- source/Glimpse.Core/Sanitizer/CSharpSanitizer.cs
+++ source/Glimpse.Core/Sanitizer/CSharpSanitizer.cs
@@ -6,12 +6,9 @@ namespace Glimpse.Core.Sanitizer
 {
     public class CSharpSanitizer:IGlimpseSanitizer
     {
-        private static readonly Regex GenericRegex = new Regex(@"\`[0-9]+\[([^\s^,^:,^;^""]+)\]", RegexOptions.Compiled);
-
         public string Sanitize(string json)
         {
-            json = GenericRegex.Replace(json, @"<$1>"); //Replace '`1[]' for generics
-            json = GenericRegex.Replace(json, @"<$1>"); //Replace '`1[]' for generics (twice for nested)
+            json = FixGenerics(json); //Replace '`1[]' for generics    
             json = Regex.Replace(json, @"(?<=System\.Nullable<.+)>(?= )", @"?"); //Add '?' for nullable types
             json = Regex.Replace(json, @"System.Nullable<", @""); //Add '?' for nullable types
             json = json.Replace("System.Boolean", "bool"); //Convert CLR type names to c# keywords
@@ -47,6 +44,18 @@ namespace Glimpse.Core.Sanitizer
                 }
             }
 
+            return json;
+        }
+
+        private static readonly Regex GenericRegex = new Regex(@"\`[0-9]+\[([^\s^,^:,^;^""]+)\]", RegexOptions.Compiled);
+        private static string FixGenerics(string json)
+        {
+            var i = 0;
+            while (GenericRegex.IsMatch(json) && i < 10)
+            {
+                json = GenericRegex.Replace(json, @"<$1>");
+                i++;
+            }
             return json;
         }
     }
