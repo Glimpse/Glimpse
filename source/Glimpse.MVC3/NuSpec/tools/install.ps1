bc5b82294bc42d05187dac8ae7d34c9812e79dd4
nikmd23 <nikmd23@gmail.com>
2011-05-29 22:52:55 -0400

    Added auto open readme script

---
+++ source/Glimpse.MVC3/NuSpec/tools/install.ps1
@@ -0,0 +1,5 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$path = [System.IO.Path]
+$readmefile = $path::Combine($path::GetDirectoryName($project.FileName), "App_Readme\glimpse.mvc3.readme.txt")
+$DTE.ExecuteCommand("File.OpenFile", $readmefile)
