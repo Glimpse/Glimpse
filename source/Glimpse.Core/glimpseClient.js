59b6d2b3dc25b80d4fe5b2a11168da33e0022c67
avanderhoorn <anthony.vanderhoorn@gmail.com>
2011-07-30 14:37:42 -0400

    JS UI Client - Fix - Fixed https://github.com/Glimpse/Glimpse/issues/35 and https://github.com/Glimpse/Glimpse/issues/98 sync ajax calls now supported

--- source/Glimpse.Core/glimpseClient.js
+++ source/Glimpse.Core/glimpseClient.js
@@ -960,6 +960,7 @@ if (window.jQueryGlimpse) { (function ($) {
         startTime: null,
         duration: null,
         logRow: null,
+        mimeType: null,
         send: function () {
             $.glimpseAjax.callStarted(this);
         },
@@ -968,154 +969,241 @@ if (window.jQueryGlimpse) { (function ($) {
         }
     };
 
-    var XMLHttpRequestWrapper = function (activeXObject) {
-        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    var XMLHttpRequestWrapper = function(activeXObject) { 
         // XMLHttpRequestWrapper internal variables
 
         var xhrRequest = (typeof activeXObject != "undefined" ? activeXObject : new _XMLHttpRequest()),
-            spy = new XHRSpy(), that = this;
-
-        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+            spy = new XHRSpy(), 
+            that = this,
+            isIE6 = $.browser.msie && $.browser.version == "6.0",
+            supportsApply = !isIE6 && xhrRequest && xhrRequest.open && typeof xhrRequest.open.apply != "undefined";
+            numberOfXHRProperties = 0;
+             
         // XMLHttpRequestWrapper internal methods
+        var updateSelfPropertiesIgnore = {
+            abort: 1,
+            channel: 1,
+            getAllResponseHeaders: 1,
+            getInterface: 1,
+            getResponseHeader: 1,
+            mozBackgroundRequest: 1,
+            multipart: 1,
+            onreadystatechange: 1,
+            open: 1,
+            send: 1,
+            setRequestHeader: 1
+        };
+         
+        var updateSelfProperties = function() {
+            if (supportsXHRIterator) {
+                for (var propName in xhrRequest) {
+                    if (propName in updateSelfPropertiesIgnore)
+                        continue; 
+                    try {
+                        var propValue = xhrRequest[propName]; 
+                        if (propValue && !$.isFunction(propValue))
+                            that[propName] = propValue;
+                    }
+                    catch(E) {  }
+                }
+            }
+            else { 
+                if (xhrRequest.readyState == 4) {
+                    that.status = xhrRequest.status;
+                    that.statusText = xhrRequest.statusText;
+                    that.response = xhrRequest.response;
+                    that.responseText = xhrRequest.responseText;
+                    that.responseType = xhrRequest.responseType;
+                    that.responseXML = xhrRequest.responseXML;  
+                }
+            }
+        };
+        
+        var updateXHRPropertiesIgnore = {
+            channel: 1,
+            onreadystatechange: 1,
+            readyState: 1,
+            responseBody: 1,
+            responseText: 1,
+            responseXML: 1,
+            status: 1,
+            statusText: 1,
+            upload: 1
+        };
+         
+        var updateXHRProperties = function() {
+            for (var propName in that) {
+                if (propName in updateXHRPropertiesIgnore)
+                    continue; 
+                try {
+                    var propValue = that[propName]; 
+                    if (propValue && !xhrRequest[propName]) 
+                        xhrRequest[propName] = propValue; 
+                }
+                catch(E) {  }
+            }
+        };
 
+        var finished = false;
         var finishXHR = function () {
-            var duration = new Date().getTime() - spy.startTime;
-            var success = xhrRequest.status == 200;
+            if (finished) { return; }
+            finished = true;
+            
+            var duration = new Date().getTime() - spy.startTime, 
+                success = xhrRequest.status == 200;
 
             //Pull out the header information
-            var responseHeadersText = xhrRequest.getAllResponseHeaders();
-            var responses = responseHeadersText ? responseHeadersText.split(/[\n\r]/) : [];
-            var reHeader = /^(\S+):\s*(.*)/;
-            for (var i = 0, l = responses.length; i < l; i++) {
-                var match = responses[i].match(reHeader);
+            var responseHeadersText = xhrRequest.getAllResponseHeaders(), 
+                responses = responseHeadersText ? responseHeadersText.split(/[\n\r]/) : [],
+                regHeader = /^(\S+):\s*(.*)/;
+            for (var i = 0, l = responses.length; i < l; i++) { 
+                var text = responses[i], match = text.match(regHeader); 
                 if (match)
-                    spy.responseHeaders[match[1]] = match[2];
+                {
+                    var name = match[1], value = match[2]; 
+                    if (name == "Content-Type")
+                        spy.mimeType = value; 
+                    spy.responseHeaders[name] = value;
+                }
             }
-
-            //Trigger the finish a bit latter
-            setTimeout(function () { spy.finish(); }, 200);
-
-            //Get the rest of the information
             spy.success = success;
             spy.loaded = true;
             spy.status = xhrRequest.status;
             spy.statusText = xhrRequest.statusText;
             spy.responseText = xhrRequest.responseText;
-            spy.duration = duration;
+            spy.duration = duration; 
+  
+            //Trigger the finish a bit latter
+            setTimeout(function () { spy.finish(); }, 200);
+             
+            updateSelfProperties();
         };
-
+         
         var handleStateChange = function () {
             that.readyState = xhrRequest.readyState;
 
-            if (xhrRequest.readyState == 4) {
-                that.statusText = xhrRequest.statusText;
-                that.status = xhrRequest.status;
-                that.response = xhrRequest.response;
-                that.responseText = xhrRequest.responseText;
-                that.responseType = xhrRequest.responseType;
-                that.responseXML = xhrRequest.responseXML;
-
+            if (xhrRequest.readyState == 4) { 
                 finishXHR();
                 xhrRequest.onreadystatechange = function () { };
             }
             that.onreadystatechange();
         };
 
-        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-        // XMLHttpRequestWrapper public properties and handlers
-
-        this.readyState = 0;
-
+        // XMLHttpRequestWrapper public properties and handlers 
+        this.readyState = 0; 
         this.onreadystatechange = function () { };
-
-        this.response = null;
-        this.responseText = null;
-        this.responseType = null;
-        this.responseXML = null;
-        this.status = null;
-        this.statusText = null;
-
-        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-        // XMLHttpRequestWrapper public methods
-
-        this.open = function (method, url, async) {
+ 
+        // XMLHttpRequestWrapper public methods 
+        this.open = function (method, url, async, user, password) { 
+            updateSelfProperties();
+            
             if (spy.loaded)
                 spy = new XHRSpy();
+            
             spy.method = method;
             spy.url = url;
             spy.href = url;
             spy.async = async;
-            spy.xhrRequest = xhrRequest;
-            //spy.urlParams = parseURLParamsArray(url);
+            spy.xhrRequest = xhrRequest; 
 
-            if (!$.browser.msie && async)
-                xhrRequest.onreadystatechange = handleStateChange;
-
-            // xhr.open.apply not available in IE
-            if (xhrRequest.open.apply)                                              //TODO: Need to see if this applies
-                xhrRequest.open.apply(xhrRequest, arguments)
-            else
-                xhrRequest.open(method, url, async);
-
-            if ($.browser.msie && async)
-                xhrRequest.onreadystatechange = handleStateChange;
-
+            try {
+                // xhrRequest.open.apply may not be available in IE
+                if (supportsApply)
+                    xhrRequest.open.apply(xhrRequest, arguments);
+                else
+                    xhrRequest.open(method, url, async, user, password);
+            }
+            catch(e) {
+                throw e;
+            }
+        
+            xhrRequest.onreadystatechange = handleStateChange;
         };
-
+         
         this.send = function (data) {
             spy.data = data;
             spy.startTime = new Date().getTime();
-
+            
+            updateXHRProperties();
+            
             try {
                 xhrRequest.send(data);
             }
-            catch (e) {
+            catch (e) { 
                 throw e;
             }
             finally {
                 spy.send();
                 if (!spy.async) {
                     that.readyState = xhrRequest.readyState;
-                    finishXHR();
+                    try { finishXHR(); } 
+                    catch(E) { } 
                 }
             }
         };
-
+         
         this.setRequestHeader = function (header, value) {
             spy.requestHeaders[header] = value;
             xhrRequest.setRequestHeader(header, value);
         };
-
+         
         this.getResponseHeader = function (header) {
             return xhrRequest.getResponseHeader(header);
         };
-
+         
         this.getAllResponseHeaders = function () {
             return xhrRequest.getAllResponseHeaders();
         };
-
+         
         this.abort = function () {
-            return xhrRequest.abort();
+            xhrRequest.abort();
+            updateSelfProperties();
         };
-
+         
+        //Clone XHR object
+        for (var propName in xhrRequest) {
+            numberOfXHRProperties++;
+        
+            if (propName in updateSelfPropertiesIgnore)
+                continue;
+        
+            try {
+                var propValue = xhrRequest[propName];  
+                if (isFunction(propValue)) {
+                    if (typeof that[propName] == "undefined") {
+                        this[propName] = (function(name, xhr) { 
+                            return supportsApply ? 
+                                function() { return xhr[name].apply(xhr, arguments); } :
+                                function(a,b,c,d,e) { return xhr[name](a,b,c,d,e); }; 
+                        })(propName, xhrRequest);
+                    } 
+                }
+                else
+                    this[propName] = propValue;
+            }
+            catch(E) { }
+        } 
+        var supportsXHRIterator = numberOfXHRProperties > 0;
+        
         return this;
     };
 
     var _ActiveXObject;
     if ($.browser.msie && $.browser.version == "6.0") {
-        window._ActiveXObject = window.ActiveXObject;
-        window.ActiveXObject = function (name) {
-            var error = null;
-
+        _ActiveXObject = window.ActiveXObject;
+    
+        var xhrObjects = " MSXML2.XMLHTTP.5.0 MSXML2.XMLHTTP.4.0 MSXML2.XMLHTTP.3.0 MSXML2.XMLHTTP Microsoft.XMLHTTP ";
+    
+        window.ActiveXObject = function(name) {
+            var error = null; 
             try {
-                var activeXObject = new window._ActiveXObject(name);
+                var activeXObject = new _ActiveXObject(name);
             }
-            catch (e) {
+            catch(e) {
                 error = e;
             }
             finally {
                 if (!error) {
-                    var xhrObjects = " MSXML2.XMLHTTP.5.0 MSXML2.XMLHTTP.4.0 MSXML2.XMLHTTP.3.0 MSXML2.XMLHTTP Microsoft.XMLHTTP ";
                     if (xhrObjects.indexOf(" " + name + " ") != -1)
                         return new XMLHttpRequestWrapper(activeXObject);
                     else
@@ -1128,9 +1216,9 @@ if (window.jQueryGlimpse) { (function ($) {
     }
     else {
         var _XMLHttpRequest = XMLHttpRequest;
-        window.XMLHttpRequest = function () {
+        window.XMLHttpRequest = function() {
             return new XMLHttpRequestWrapper();
-        }
+        };
     }
     //#endregion
 
@@ -1146,7 +1234,7 @@ if (window.jQueryGlimpse) { (function ($) {
         adjustProtocol: function (data) {
             var ga = this, metadata;
 
-            data[ga.static.key] = ''
+            data[ga.static.key] = '';
 
             if ((metadata = data._metadata) && (metadata = metadata.plugins))
                 (metadata[ga.static.key] = {})['helpUrl'] = 'http://getglimpse.com/Help/Plugin/Ajax'; 
