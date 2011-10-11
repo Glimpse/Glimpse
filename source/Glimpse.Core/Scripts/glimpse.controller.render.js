8c3f1fbd99108b2a7cff5f6766b17473ed579a85
avanderhoorn <anthony.vanderhoorn@gmail.com>
2011-10-11 11:18:13 -0400

    JS UI Client - Refactoring - Enabled tabs to be selected

--- source/Glimpse.Core/Scripts/glimpse.controller.render.js
+++ source/Glimpse.Core/Scripts/glimpse.controller.render.js
@@ -2,14 +2,20 @@
     var //Support
         wireListeners = function() {
             pubsub.subscribe('state.render', render); 
+            pubsub.subscribe('data.elements.processed', wireDomListeners); 
+            pubsub.subscribe('action.tab.select', function(subject, payload) { selectedTab(payload); }); 
         },
+        wireDomListeners = function() {
+            elements.tabHolder.find('li:not(.glimpse-active, .glimpse-disabled)').live('click', function () { pubsub.publish('action.tab.select', $(this)); return false; });
+        },
+
         constructTabs = function (items) {
             var html = '', itemKey, disabled, item;
             for (itemKey in items) {
                 item = items[itemKey];
                 disabled = (item.data === undefined || item.data === null) ? ' glimpse-disabled' : '';
 
-                html += '<li class="glimpse-tabitem-' + itemKey + disabled + '" data-sort="' + itemKey + '">' + item.name + '</li>';
+                html += '<li class="glimpse-tabitem-' + itemKey + disabled + '" data-glimpseKey="' + itemKey + '">' + item.name + '</li>';
             }
             return html;
         },
@@ -18,6 +24,12 @@
             elements.tabHolder.append(constructTabs(items)); 
             util.sortElements(elements.tabHolder, elements.tabHolder.find('li'));
         },
+        selectedTab = function (selectedItem) {
+            var key = selectedItem.data('glimpseKey');
+
+            elements.tabHolder.find('.glimpse-active, .glimpse-hover').removeClass('glimpse-active').removeClass('glimpse-hover'); 
+            selectedItem.addClass('glimpse-active');
+        }
 
         //Main
         render = function () {
@@ -25,7 +37,7 @@
             renderTabs(items);
         },
         init = function () {
-            wireListeners();
+            wireListeners(); 
         };
     
     init();  
