14924a133222d2ab2aa88ae3d8a3ad981c12f5eb
avanderhoorn <anthony.vanderhoorn@gmail.com>
2011-10-13 20:12:13 -0400

    JS UI Client - Refactoring - Clean up of the earlier implementation of the render.

--- source/Glimpse.Core/Scripts/glimpse.js
+++ source/Glimpse.Core/Scripts/glimpse.js
@@ -226,46 +226,42 @@ var glimpse = (function ($, scope) {
                 wireListeners = function() {
                     pubsub.subscribe('state.render', renderLayout); 
                     pubsub.subscribe('data.elements.processed', wireDomListeners); 
-                    pubsub.subscribe('action.tab.select', function(subject, payload) { selectedTab(payload); }); 
+                    pubsub.subscribe('action.tab.select', function(subject, payload) { selectedItem(payload); }); 
                 },
                 wireDomListeners = function() {
-                    elements.tabHolder.find('li:not(.glimpse-active, .glimpse-disabled)').live('click', function () { pubsub.publish('action.tab.select', $(this)); return false; });
+                    elements.tabHolder.find('li:not(.glimpse-active, .glimpse-disabled)').live('click', function () { pubsub.publish('action.tab.select', $(this).attr('data-glimpseKey')); return false; });
                     elements.tabHolder.find('li:not(.glimpse-active, .glimpse-disabled)').live('mouseover mouseout', function (e) {
                         var pluginData = $(this);
                         if (e.type == 'mouseover') { pluginData.addClass('glimpse-hover'); } else { pluginData.removeClass('glimpse-hover'); }
                     });
                 },
         
-                cleanPreRender = function() {
+                clearPreviousLayout = function () {
                     elements.tabHolder.find('.glimpse-tab:not(.glimpse-permanent)').remove();
                     elements.panelHolder.find('.glimpse-panel:not(.glimpse-permanent)').remove(); 
                 },
+                buildNewLayout = function () {
+                    renderTabs(data.getCurrent());
+                },
         
-                constructTabs = function (pluginDataCollection) {
+                renderTabs = function (pluginDataSet) {
+                    elements.tabHolder.append(constructTabs(pluginDataSet)); 
+                    util.sortElements(elements.tabHolder, elements.tabHolder.find('li'));
+                },
+                constructTabs = function (pluginDataSet) {
                     var html = '', key, disabled, pluginData;
-                    for (key in pluginDataCollection) {
-                        pluginData = pluginDataCollection[key];
+                    for (key in pluginDataSet) {
+                        pluginData = pluginDataSet[key];
                         disabled = (pluginData.data === undefined || pluginData.data === null) ? ' glimpse-disabled' : '';
         
                         html += '<li class="glimpse-tab glimpse-tabitem-' + key + disabled + '" data-glimpseKey="' + key + '">' + pluginData.name + '</li>';
                     }
                     return html;
                 },
-                renderTabs = function (pluginDataCollection) {
-                    elements.tabHolder.append(constructTabs(pluginDataCollection)); 
-                    util.sortElements(elements.tabHolder, elements.tabHolder.find('li'));
+                 
+                renderPanel = function (key, pluginData, pluginMetadata) {
+                    elements.panelHolder.append(constuctPanel(key, pluginData, pluginMetadata)); 
                 },
-                selectedTab = function (selectedItem) {
-                    var key = selectedItem.attr('data-glimpseKey');
-        
-                    //Switch style states
-                    elements.tabHolder.find('.glimpse-active, .glimpse-hover').removeClass('glimpse-active').removeClass('glimpse-hover'); 
-                    selectedItem.addClass('glimpse-active');
-        
-                    //Make sure Panel assoc panel is shown 
-                    selectedPanel(key);
-                },
-        
                 constuctPanel = function (key, pluginData, pluginMetadata) {
                     var start = new Date().getTime();
                     
@@ -277,8 +273,24 @@ var glimpse = (function ($, scope) {
         
                     return html;
                 },
-                renderPanel = function (key, pluginData, pluginMetadata) {
-                    elements.panelHolder.append(constuctPanel(key, pluginData, pluginMetadata)); 
+        
+                
+                selectedItem = function (key) {
+                    var oldItem = elements.tabHolder.find('.glimpse-active');
+                     
+                    if (oldItem.length > 0) { pubsub.publish('action.plugin.deactive', oldItem.attr('data-glimpseKey')); } 
+        
+                    selectedTab(key);
+                    selectedPanel(key);
+                     
+                    pubsub.publish('action.plugin.active', key); 
+                }, 
+                selectedTab = function (key) {
+                    var tab = elements.tabHolder.find('.glimpse-tab[data-glimpseKey="' + key + '"]');
+        
+                    //Switch style states
+                    elements.tabHolder.find('.glimpse-active, .glimpse-hover').removeClass('glimpse-active').removeClass('glimpse-hover'); 
+                    tab.addClass('glimpse-active');
                 },
                 selectedPanel = function (key) {
                     var panel = elements.panelHolder.find('.glimpse-panel[data-glimpseKey="' + key + '"]'); 
@@ -288,17 +300,17 @@ var glimpse = (function ($, scope) {
                         renderPanel(key, data.getCurrent()[key], data.getCurrentMeta().plugins[key]);  
                         panel = elements.panelHolder.find('.glimpse-panel[data-glimpseKey="' + key + '"]');
                     }
-        
-                    //Switch display states
-                    elements.panelHolder.find('.glimpse-panel:visible').hide();
-                    panel.show();
+                    
+                    //Switch style states
+                    elements.panelHolder.find('.glimpse-active').removeClass('glimpse-active'); 
+                    panel.addClass('glimpse-active');
                 },
         
         
                 //Main
                 renderLayout = function () { 
-                    cleanPreRender();
-                    renderTabs(data.getCurrent());
+                    clearPreviousLayout();
+                    buildNewLayout();
                 },
                 init = function () {
                     wireListeners(); 
