70754d7e39cdca0f9e5ae33a360a5e9b73b0ddbb
avanderhoorn <anthony.vanderhoorn@gmail.com>
2011-10-15 08:34:29 -0400

    JS UI Client - Refactoring - Support for restoring the panel to its opened state

--- source/Glimpse.Core/Scripts/glimpse.js
+++ source/Glimpse.Core/Scripts/glimpse.js
@@ -536,26 +536,26 @@ var glimpse = (function ($, scope) {
         tollbarController = function () {
             var //Support
                 wireListeners = function() {
-                    pubsub.subscribe('action.open', open);
+                    pubsub.subscribe('action.open', function(topic, payload) { open(payload); });
                     pubsub.subscribe('action.minimize', function() { close(false); });
                     pubsub.subscribe('action.close', function() { close(true); }); 
                     pubsub.subscribe('state.build.shell.modify', wireDomListeners); 
                     pubsub.subscribe('state.build.rendered', restore); 
                 },
                 wireDomListeners = function() {
-                    elements.scope.find('.glimpse-open').click(function () { pubsub.publish('action.open'); return false; });
+                    elements.scope.find('.glimpse-open').click(function () { pubsub.publish('action.open', false); return false; });
                     elements.scope.find('.glimpse-minimize').click(function () { pubsub.publish('action.minimize'); return false; });
                     elements.scope.find('.glimpse-close').click(function () { pubsub.publish('action.close'); return false; });
                     elements.scope.find('.glimpse-popout').click(function () { pubsub.publish('action.popout'); return false; });
                 }, 
                     
                 //Main 
-                open = function () {
+                open = function (shortCircuitSlide) {
                     settings.open = true;
                     pubsub.publish('state.persist');
         
                     elements.opener.hide(); 
-                    $.fn.add.call(elements.holder, elements.spacer).show().animate({ height : settings.height }, 'fast');   
+                    $.fn.add.call(elements.holder, elements.spacer).show().animate({ height : settings.height }, (shortCircuitSlide ? 0 : 'fast'));   
                 },
                 close = function (remove) {
                     settings.open = false;
@@ -574,12 +574,16 @@ var glimpse = (function ($, scope) {
                 },  
                 restore = function () {
                     var key = settings.activeTab,
+                        opened = settings.open,
                         tab = elements.tabHolder.find('.glimpse-tab[data-glimpseKey="' + key + '"]');
         
                     if (tab.length == 0 || tab.hasClass('glimpse-disabled'))
                         key = elements.tabHolder.find('.glimpse-tab:not(.glimpse-disabled):first').attr('data-glimpseKey');
         
                     pubsub.publish('action.tab.select', key);
+        
+                    if (opened)
+                        pubsub.publish('action.open', true);
                 },
                 init = function () {
                     wireListeners();
