102835802cb843b32ece0e243c8c0643e99db44c
avanderhoorn <anthony.vanderhoorn@gmail.com>
2011-10-26 19:56:16 -0400

    JS UI Client - Refactoring - Changing up the way that the html gets into the ajax grid to support only rendering delta changes

--- source/Glimpse.Core/Scripts/glimpse.ajax.plugin.js
+++ source/Glimpse.Core/Scripts/glimpse.ajax.plugin.js
@@ -7,8 +7,13 @@
         resultCount = 0,
         notice = undefined,
         wireListener = function () {  
+            glimpse.pubsub.subscribe('data.elements.processed', wireDomListeners); 
             glimpse.pubsub.subscribe('action.plugin.deactive', function (topic, payload) { if (payload == 'Ajax') { deactive(); } }); 
             glimpse.pubsub.subscribe('action.plugin.active', function (topic, payload) {  if (payload == 'Ajax') { active(); } }); 
+        },
+        wireDomListeners = function () {
+            glimpse.elements.holder.find('.glimpse-clear-ajax').live('click', function () { return false; });
+            glimpse.elements.findPanel('Ajax').find('a').live('click', function () { return false; });
         },
         alterCurrent = function () {
             var data = glimpse.data.current().data,
@@ -19,7 +24,7 @@
         },
         active = function () {
             isActive = true;
-            glimpse.elements.options.html('<div class="glimpse-clear"><a href="#">Clear</a></div><div class="glimpse-notice gdisconnect"><div class="icon"></div><span>Disconnected...</span></div>');
+            glimpse.elements.options.html('<div class="glimpse-clear"><a href="#" class="glimpse-clear-ajax">Clear</a></div><div class="glimpse-notice gdisconnect"><div class="icon"></div><span>Disconnected...</span></div>');
             notice = new glimpse.objects.ConnectionNotice(glimpse.elements.options.find('.glimpse-notice')); 
             
             getData(); 
@@ -27,6 +32,7 @@
         deactive = function () {
             isActive = false; 
             glimpse.elements.options.html('');
+            notice = null;
         }, 
         getData = function () { 
             if (!isActive) { return; } 
@@ -39,18 +45,26 @@
                 contentType: 'application/json',
                 complete : function(jqXHR, textStatus) {
                     notice.complete(textStatus);
-                    setTimeout(getData, 1000);
+                    if (textStatus != "Success")
+                        setTimeout(getData, 1000);
                 },
                 success: function (result) {
                     if (resultCount != result.length)
                         processData(result);
                     resultCount = result.length; 
+                    setTimeout(getData, 1000);
                 }
             });
         },
         processData = function (result) { 
             var panel = glimpse.elements.findPanel('Ajax'); 
-            panel.html(glimpse.render.build(result));
+            if (resultCount == 0)
+                panel.html(glimpse.render.build([['Request URL', 'Method', 'Duration', 'Date/Time', 'View']])).find('table').append('<tbody></tbody>');
+            
+            for (var x = result.length; --x >= resultCount;) {
+                var item = result[x];
+                panel.find('tbody').prepend('<tr class="' + (x % 2 == 0 ? 'even' : 'odd') + '"><td>' + item.url + '</td><td>' + item.method + '</td><td>' + item.duration + '<span class="glimpse-soft"> ms</span></td><td>' + item.requestTime + '</td><td><a href="#">Inspect</a></td></tr>');
+            }
         },
 
         //Main 
--- source/Glimpse.Core/Scripts/glimpse.js
+++ source/Glimpse.Core/Scripts/glimpse.js
@@ -1570,8 +1570,13 @@ var glimpseAjaxPlugin = (function ($, glimpse) {
         resultCount = 0,
         notice = undefined,
         wireListener = function () {  
+            glimpse.pubsub.subscribe('data.elements.processed', wireDomListeners); 
             glimpse.pubsub.subscribe('action.plugin.deactive', function (topic, payload) { if (payload == 'Ajax') { deactive(); } }); 
             glimpse.pubsub.subscribe('action.plugin.active', function (topic, payload) {  if (payload == 'Ajax') { active(); } }); 
+        },
+        wireDomListeners = function () {
+            glimpse.elements.holder.find('.glimpse-clear-ajax').live('click', function () { return false; });
+            glimpse.elements.findPanel('Ajax').find('a').live('click', function () { return false; });
         },
         alterCurrent = function () {
             var data = glimpse.data.current().data,
@@ -1582,7 +1587,7 @@ var glimpseAjaxPlugin = (function ($, glimpse) {
         },
         active = function () {
             isActive = true;
-            glimpse.elements.options.html('<div class="glimpse-clear"><a href="#">Clear</a></div><div class="glimpse-notice gdisconnect"><div class="icon"></div><span>Disconnected...</span></div>');
+            glimpse.elements.options.html('<div class="glimpse-clear"><a href="#" class="glimpse-clear-ajax">Clear</a></div><div class="glimpse-notice gdisconnect"><div class="icon"></div><span>Disconnected...</span></div>');
             notice = new glimpse.objects.ConnectionNotice(glimpse.elements.options.find('.glimpse-notice')); 
             
             getData(); 
@@ -1590,6 +1595,7 @@ var glimpseAjaxPlugin = (function ($, glimpse) {
         deactive = function () {
             isActive = false; 
             glimpse.elements.options.html('');
+            notice = null;
         }, 
         getData = function () { 
             if (!isActive) { return; } 
@@ -1602,18 +1608,26 @@ var glimpseAjaxPlugin = (function ($, glimpse) {
                 contentType: 'application/json',
                 complete : function(jqXHR, textStatus) {
                     notice.complete(textStatus);
-                    setTimeout(getData, 1000);
+                    if (textStatus != "Success")
+                        setTimeout(getData, 1000);
                 },
                 success: function (result) {
                     if (resultCount != result.length)
                         processData(result);
                     resultCount = result.length; 
+                    setTimeout(getData, 1000);
                 }
             });
         },
         processData = function (result) { 
             var panel = glimpse.elements.findPanel('Ajax'); 
-            panel.html(glimpse.render.build(result));
+            if (resultCount == 0)
+                panel.html(glimpse.render.build([['Request URL', 'Method', 'Duration', 'Date/Time', 'View']])).find('table').append('<tbody></tbody>');
+            
+            for (var x = result.length; --x >= resultCount;) {
+                var item = result[x];
+                panel.find('tbody').prepend('<tr class="' + (x % 2 == 0 ? 'even' : 'odd') + '"><td>' + item.url + '</td><td>' + item.method + '</td><td>' + item.duration + '<span class="glimpse-soft"> ms</span></td><td>' + item.requestTime + '</td><td><a href="#">Inspect</a></td></tr>');
+            }
         },
 
         //Main 
