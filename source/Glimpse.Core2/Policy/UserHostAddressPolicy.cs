c68e3883952e7d661cd636b578bb5199d2005391
Nik Molnar <nikmd23@gmail.com>
2012-01-13 21:09:40 -0500

    Added incomplete implementation of UserHostAddressPolicy, thinking about removing this feature from Glimpse in favor of a simplified isLocal policy like ASP.NET's customErrorsMode setting

---
+++ source/Glimpse.Core2/Policy/UserHostAddressPolicy.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using Glimpse.Core2.Extensibility;
+
+namespace Glimpse.Core2.Policy
+{
+    //Commented out to avoid autodiscovery [RuntimePolicy(RuntimeEvent.BeginRequest)]
+    public class UserHostAddressPolicy:ConfigurationSection, IRuntimePolicy
+    {
+        public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
+        {
+            //TODO: Should we keep this policy, or leverage a simple "isLocal" policy similar to customErrorMode?
+            throw new System.NotImplementedException("Have not yet implemented UserHostAddessPolicy");
+        }
+    }
+}
