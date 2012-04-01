50ff66c79f3f8dc4c2576fc3190ed4ac8a950e77
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-03-31 23:04:13 -0400

    Update the l\paths in the server side of the resource

--- source/Glimpse.Core2/Framework/GlimpseMetadata.cs
+++ source/Glimpse.Core2/Framework/GlimpseMetadata.cs
@@ -8,10 +8,14 @@ namespace Glimpse.Core2.Framework
         public GlimpseMetadata()
         {
             plugins = new Dictionary<string, PluginMetadata>();
+
+            //TODO: this is really bad... Nik needs to work on how we want to do this. Version numbers need to be taken intoaccount too
+            paths = new { history = "History", paging = "Pager", ajax = "Ajax", config = "Config", logo = "/Glimpse.axd?n=logo.png&Version=1.0.0", sprite = "/Glimpse.axd?n=sprite.png&Version=1.0.0", popup = "test-popup.html" };
         }
 
-        public string version{ get; set; }
+        public string version { get; set; }
         public IDictionary<string,PluginMetadata> plugins { get; set; }
+        public object paths { get; set; }
     }
 
     public class PluginMetadata
