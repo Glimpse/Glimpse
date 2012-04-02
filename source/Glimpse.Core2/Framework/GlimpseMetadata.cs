18201f00de91abb203e8d697e006ae613beb0936
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-04-02 09:06:19 -0400

    Adjusted casing on metadata object to work with the client notation

--- source/Glimpse.Core2/Framework/GlimpseMetadata.cs
+++ source/Glimpse.Core2/Framework/GlimpseMetadata.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Glimpse.Core2.Framework
 {
@@ -7,22 +8,28 @@ namespace Glimpse.Core2.Framework
     {
         public GlimpseMetadata()
         {
-            plugins = new Dictionary<string, PluginMetadata>();
+            Plugins = new Dictionary<string, PluginMetadata>();
 
             //TODO: this is really bad... Nik needs to work on how we want to do this. Version numbers need to be taken intoaccount too
-            paths = new { history = "History", paging = "Pager", ajax = "Ajax", config = "Config", logo = "/Glimpse.axd?n=logo.png&Version=1.0.0", sprite = "/Glimpse.axd?n=sprite.png&Version=1.0.0", popup = "test-popup.html" };
+            Paths = new { history = "History", paging = "Pager", ajax = "Ajax", config = "Config", logo = "/Glimpse.axd?n=logo.png&Version=1.0.0", sprite = "/Glimpse.axd?n=sprite.png&Version=1.0.0", popup = "test-popup.html" };
         }
 
-        public string version { get; set; }
-        public IDictionary<string,PluginMetadata> plugins { get; set; }
-        public object paths { get; set; }
+        [JsonProperty(PropertyName = "version")]
+        public string Version { get; set; }
+
+        [JsonProperty(PropertyName = "plugins")]
+        public IDictionary<string,PluginMetadata> Plugins { get; set; }
+
+        [JsonProperty(PropertyName = "paths")]
+        public object Paths { get; set; }
     }
 
     public class PluginMetadata
     {
+        [JsonProperty(PropertyName = "documentationUri")]
         public string DocumentationUri { get; set; }
 
-        public bool HasMetadata { get { return !string.IsNullOrEmpty(DocumentationUri); }
-        }
+        [JsonProperty(PropertyName = "hasMetadata")]
+        public bool HasMetadata { get { return !string.IsNullOrEmpty(DocumentationUri); } }
     }
 }
