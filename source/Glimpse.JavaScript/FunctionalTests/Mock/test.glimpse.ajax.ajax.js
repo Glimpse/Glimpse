24cb3329658430fc39ac4098b3ec6db17b51938b
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-07-10 15:41:48 +1000

    Fixed issue in History tester
    History was returning correlation data for the ajax results. Also the
    paths of the test results where inconsistent with what was expected.

--- source/Glimpse.JavaScript/FunctionalTests/Mock/test.glimpse.ajax.ajax.js
+++ source/Glimpse.JavaScript/FunctionalTests/Mock/test.glimpse.ajax.ajax.js
@@ -1,18 +1,18 @@
 ﻿ajax = function () {
     var result = [],
-        possibleResults = [{ method : 'Get', duration : 213, browser : '', clientName : '', requestTime : '2011/11/09 12:00:12', requestId : 'ajax0', parentId : '1234', isAjax : true, url : '/News'},
-            { method : 'Get', duration : 123, browser : '', clientName : '', requestTime : '2011/11/09 12:10:34', requestId : 'ajax1', parentId : '1234', isAjax : true, url : '/Shares'},
-            { method : 'Get', duration : 234, browser : '', clientName : '', requestTime : '2011/11/09 12:12:23', requestId : 'ajax2', parentId : '1234', isAjax : true, url : '/Order/230'},
-            { method : 'Post', duration : 342, browser : '', clientName : '', requestTime : '2011/11/09 12:17:52', requestId : 'ajax3', parentId : '1234', isAjax : true, url : '/Order/Add'},
-            { method : 'Post', duration : 211, browser : '', clientName : '', requestTime : '2011/11/24 12:00:35', requestId : 'ajax4', parentId : '1234', isAjax : true, url : '/History/Results'},
-            { method : 'Post', duration : 242, browser : '', clientName : '', requestTime : '2011/11/09 12:27:23', requestId : 'ajax5', parentId : '1234', isAjax : true, url : '/News/List'},
-            { method : 'Get', duration : 1234, browser : '', clientName : '', requestTime : '2011/11/09 12:29:14', requestId : 'ajax6', parentId : '1234', isAjax : true, url : '/News'}],
+        possibleResults = [{ method : 'Get', duration : 213, browser : '', clientName : '', requestTime : '2011/11/09 12:00:12', requestId : 'ajax0', parentId : '1234', isAjax : true, url : '/Product'},
+            { method : 'Get', duration : 123, browser : '', clientName : '', requestTime : '2011/11/09 12:10:34', requestId : 'ajax1', parentId : '1234', isAjax : true, url : '/Product/Trip'},
+            { method : 'Get', duration : 234, browser : '', clientName : '', requestTime : '2011/11/09 12:12:23', requestId : 'ajax2', parentId : '1234', isAjax : true, url : '/Product/230'},
+            { method : 'Post', duration : 342, browser : '', clientName : '', requestTime : '2011/11/09 12:17:52', requestId : 'ajax3', parentId : '1234', isAjax : true, url : '/Product/Add'},
+            { method : 'Post', duration : 211, browser : '', clientName : '', requestTime : '2011/11/24 12:00:35', requestId : 'ajax4', parentId : '1234', isAjax : true, url : '/Product/Results'},
+            { method : 'Post', duration : 242, browser : '', clientName : '', requestTime : '2011/11/09 12:27:23', requestId : 'ajax5', parentId : '1234', isAjax : true, url : '/Product/List'},
+            { method : 'Get', duration : 1234, browser : '', clientName : '', requestTime : '2011/11/09 12:29:14', requestId : 'ajax6', parentId : '1234', isAjax : true, url : '/Product'}],
         index = 0,
         lastId = 0,
         generate = function (data) {  
             if ((index < 6 && data.clientRequestID == 1234) || index < 3)
                 result.push(possibleResults[index++]);
-            return result;
+            return $.extend(true, [], result);
         },
         trigger = function (param) { 
             if (param.data.clientRequestID != lastId) {
