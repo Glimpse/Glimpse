ef671c4f0fec9af6fc81f43237ead7484b4cdd27
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-07-13 14:16:32 +1000

    Fix: requestId not being passing to popup right

--- source/Glimpse.JavaScript/glimpse.controller.toolbar.js
+++ source/Glimpse.JavaScript/glimpse.controller.toolbar.js
@@ -35,7 +35,7 @@
 
             //TODO !!!! This needs to be updated once we get going !!!!
             var path = data.currentMetadata().paths.popup,
-                url = path + (path.indexOf('?') > -1 ? '&' : '?') + 'requestId=' + $('#glimpseData').data('glimpse-requestID');
+                url = path + (path.indexOf('?') > -1 ? '&' : '?') + 'requestId=' + data.current().requestId;
             window.open(url, 'GlimpsePopup', 'width=1100,height=600,status=no,toolbar=no,menubar=no,location=no,resizable=yes,scrollbars=yes');
         },
             
--- source/Glimpse.JavaScript/glimpse.js
+++ source/Glimpse.JavaScript/glimpse.js
@@ -664,7 +664,7 @@ var glimpse = (function ($, scope) {
         
                     //TODO !!!! This needs to be updated once we get going !!!!
                     var path = data.currentMetadata().paths.popup,
-                        url = path + (path.indexOf('?') > -1 ? '&' : '?') + 'requestId=' + $('#glimpseData').data('glimpse-requestID');
+                        url = path + (path.indexOf('?') > -1 ? '&' : '?') + 'requestId=' + data.current().requestId;
                     window.open(url, 'GlimpsePopup', 'width=1100,height=600,status=no,toolbar=no,menubar=no,location=no,resizable=yes,scrollbars=yes');
                 },
                     
