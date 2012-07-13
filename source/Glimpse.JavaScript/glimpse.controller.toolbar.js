3a433f08a8ff510e7caae00056de635e6eb36a65
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-07-13 12:15:12 +1000

    Popup enhancements and test fixes

--- source/Glimpse.JavaScript/glimpse.controller.toolbar.js
+++ source/Glimpse.JavaScript/glimpse.controller.toolbar.js
@@ -34,7 +34,8 @@
             util.cookie('glimpseKeepPopup', '1');
 
             //TODO !!!! This needs to be updated once we get going !!!!
-            var url = data.currentMetadata().paths.popup; // + '&glimpseRequestID=' + $('#glimpseData').data('glimpse-requestID');
+            var path = data.currentMetadata().paths.popup,
+                url = path + (path.indexOf('?') > -1 ? '&' : '?') + 'requestId=' + $('#glimpseData').data('glimpse-requestID');
             window.open(url, 'GlimpsePopup', 'width=1100,height=600,status=no,toolbar=no,menubar=no,location=no,resizable=yes,scrollbars=yes');
         },
             
