a8f0101b63ef75a8a66f5d83fbc5ee2fd59876b0
nikmd23 <nikmd23@gmail.com>
2012-08-03 16:38:27 -0400

    Added GlimpseSecurityPolicy source code transform to allow users to easily opt into using ASP.NET Role providers for security

---
+++ source/Glimpse.AspNet/NuSpec/content/GlimpseSecurityPolicy.cs.pp
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using Glimpse.Core2;
+using Glimpse.Core2.Extensibility;
+
+namespace $rootnamespace$
+{
+    public class GlimpseSecurityPolicy:IRuntimePolicy
+    {
+        public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
+        {
+            var context = policyContext.GetRequestContext<HttpContextBase>();
+
+            //You can perform a check like the one below to control Glimpse's permissions within your application.
+            //if (!context.User.IsInRole("Administrator")) 
+            //    return RuntimePolicy.Off;
+
+            return RuntimePolicy.On;
+        }
+
+        public RuntimeEvent ExecuteOn
+        {
+            get { return RuntimeEvent.EndRequest; }
+        }
+    }
+}
