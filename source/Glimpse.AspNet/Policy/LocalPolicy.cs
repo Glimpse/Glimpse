9c44806e55770fe3021a08a8643bc1cba07a270c
nikmd23 <nikmd23@gmail.com>
2012-08-06 15:26:13 -0400

    Added LocalPolicy to ensure that Glimpse only runs for local requests (by default). Also added tests and xunit.extensions package for theory support.

---
+++ source/Glimpse.AspNet/Policy/LocalPolicy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using Glimpse.Core2;
+using Glimpse.Core2.Extensibility;
+
+namespace Glimpse.AspNet.Policy
+{
+    public class LocalPolicy:IRuntimePolicy
+    {
+        public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
+        {
+            if (policyContext == null) throw new ArgumentNullException("policyContext");
+
+            var httpContext = policyContext.GetRequestContext<HttpContextBase>();
+
+            if (httpContext.Request.IsLocal)
+                return RuntimePolicy.On;
+
+            return RuntimePolicy.Off;
+        }
+
+        public RuntimeEvent ExecuteOn { 
+            get { return RuntimeEvent.BeginRequest; }
+        }
+    }
+}
---
+++ source/Glimpse.Test.AspNet/Policy/LocalPolicyShould.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web;
+using Glimpse.AspNet.Policy;
+using Glimpse.Core2;
+using Glimpse.Core2.Extensibility;
+using Moq;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Glimpse.Test.AspNet.Policy
+{
+    public class LocalPolicyShould
+    {
+        [Fact]
+        public void ExecuteAsEarlyAsPossible()
+        {
+            var policy = new LocalPolicy();
+            Assert.Equal(RuntimeEvent.BeginRequest, policy.ExecuteOn);
+        }
+
+        [Fact]
+        public void ThrowExceptionWithNullPolicyContext()
+        {
+            var policy = new LocalPolicy();
+
+            Assert.Throws<ArgumentNullException>(() => policy.Execute(null));
+        }
+
+        [Theory]
+        [InlineData(true, RuntimePolicy.On)]
+        [InlineData(false, RuntimePolicy.Off)]
+        public void LeaveGlimpseOnWithLocalRequest(bool isLocal, RuntimePolicy expectedPolicy)
+        {
+            var httpContextMock = new Mock<HttpContextBase>();
+            httpContextMock.Setup(m => m.Request.IsLocal).Returns(isLocal);
+
+            var policyContextMock = new Mock<IRuntimePolicyContext>();
+            policyContextMock.Setup(m => m.GetRequestContext<HttpContextBase>()).Returns(httpContextMock.Object);
+
+            var policy = new LocalPolicy();
+            var result = policy.Execute(policyContextMock.Object);
+
+            Assert.Equal(expectedPolicy, result);
+        }
+    }
+}
