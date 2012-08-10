5b1ee98f4107ee296d58ab0c4e0c10ebae2de025
nikmd23 <nikmd23@gmail.com>
2012-08-10 09:23:02 -0400

    Added placeholder tab for timeline

---
+++ source/Glimpse.AspNet/Tab/Timeline.cs
@@ -0,0 +1,18 @@
+﻿using Glimpse.AspNet.Extensibility;
+using Glimpse.Core2.Extensibility;
+
+namespace Glimpse.AspNet.Tab
+{
+    public class Timeline:AspNetTab
+    {
+        public override object GetData(ITabContext context)
+        {
+            return null;
+        }
+
+        public override string Name
+        {
+            get { return "Timeline"; }
+        }
+    }
+}
