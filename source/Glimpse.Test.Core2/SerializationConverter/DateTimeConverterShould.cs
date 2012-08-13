0611f33feae6504fb1241f1c84319aa2d97c8c9d
nikmd23 <nikmd23@gmail.com>
2012-08-13 16:49:21 -0400

    Added DateTime converter to convert dates to human readable (not JS readable) strings

---
+++ source/Glimpse.Test.Core2/SerializationConverter/DateTimeConverterShould.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Glimpse.Core2.SerializationConverter;
+using Xunit;
+
+namespace Glimpse.Test.Core2.SerializationConverter
+{
+    public class DateTimeConverterShould
+    {
+        [Fact]
+        public void HandleNull()
+        {
+            var converter = new DateTimeConverter();
+
+            var result = converter.Convert(null);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void SupportDateTimes()
+        {
+            var converter = new DateTimeConverter();
+
+            var result = converter.SupportedTypes;
+
+            Assert.Contains(typeof(DateTime), result);
+            Assert.Contains(typeof(DateTime?), result);
+        }
+
+        [Fact]
+        public void HandleNullableDateTimes()
+        {
+            DateTime? input = new DateTime(2000,1,1);
+
+            var converter = new DateTimeConverter();
+
+            var result = converter.Convert(input);
+
+            Assert.Equal("01/01/2000 00:00:00", result);
+        }
+
+        [Fact]
+        public void HandleDateTimes()
+        {
+            DateTime input = new DateTime(2000, 1, 1);
+
+            var converter = new DateTimeConverter();
+
+            var result = converter.Convert(input);
+
+            Assert.Equal("01/01/2000 00:00:00", result);
+        }
+
+        [Fact]
+        public void HandleNullableDateTimesWithNoValue()
+        {
+            DateTime? input = null;
+
+            var converter = new DateTimeConverter();
+
+            var result = converter.Convert(input);
+
+            Assert.Null(result);
+        }
+    }
+}
