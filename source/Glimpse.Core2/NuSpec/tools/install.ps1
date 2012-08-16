eab24c7224b65a2fd6f143cc8104f2a02460480f
nikmd23 <nikmd23@gmail.com>
2012-08-16 14:58:38 -0400

    Updated nuspec to open a readme.html file.

---
+++ source/Glimpse.Core2/NuSpec/tools/install.ps1
@@ -0,0 +1,5 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$path = [System.IO.Path]
+$readmefile = $path::Combine($installPath, "glimpse.readme.html")
+$DTE.ItemOperations.Navigate($readmefile)
