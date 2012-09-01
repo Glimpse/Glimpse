6906fb0583b5b2faed4e084251ad1356e91f9e74
Kristoffer Ahl <git@77dynamite.com>
2012-09-01 18:55:35 +0200

    - Added tests for FormattingExtensions.

---
+++ source/Glimpse.Test.Core/Plugin/Assist/FormattingExtensionsTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using Glimpse.Core.Plugin.Assist;
+using Xunit;
+
+namespace Glimpse.Test.Core.Plugin.Assist
+{
+	public class FormattingExtensionsFact
+	{
+		[Fact]
+		public void GlimpseRow_Bold_AppliesBoldToLastColumn()
+		{
+			var row = Row.Bold();
+
+			Assert.Equal(row.Columns.Last().Data, "*Text*");
+		}
+
+		[Fact]
+		public void GlimpseRow_Underline_AppliesUnderlineToLastColumn()
+		{
+			var row = Row.Underline();
+
+			Assert.Equal(row.Columns.Last().Data, "_Text_");
+		}
+
+		[Fact]
+		public void GlimpseRow_RowOperation_IsInvalidForRowsWithoutColumns()
+		{
+			var row = new GlimpseRow();
+
+			Assert.Throws<InvalidOperationException>(() => row.Quiet());
+		}
+
+		[Fact]
+		public void GlimpseRow_Quiet_AddsColumnWithQuiet()
+		{
+			var row = Row.Quiet();
+
+			Assert.Equal(row.Columns.Last().Data, "quiet");
+		}
+
+		[Fact]
+		public void GlimpseRow_Selected_AddsColumnWithSelected()
+		{
+			var row = Row.Selected();
+
+			Assert.Equal(row.Columns.Last().Data, "selected");
+		}
+
+		[Fact]
+		public void GlimpseRow_Warn_AddsColumnWithWarn()
+		{
+			var row = Row.Warn();
+
+			Assert.Equal(row.Columns.Last().Data, "warn");
+		}
+
+		private GlimpseRow Row { get; set; }
+
+		public FormattingExtensionsFact()
+		{
+			Row = new GlimpseRow().Column("Text");
+		}
+	}
+}
