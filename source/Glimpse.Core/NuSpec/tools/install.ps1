87e657504f7b520b8303d63bac036f422e256132
nikmd23 <nikmd23@gmail.com>
2012-09-05 19:28:25 -0400

    Added support for detecting upgrades for dynamic readme file

--- source/Glimpse.Core/NuSpec/tools/install.ps1
+++ source/Glimpse.Core/NuSpec/tools/install.ps1
@@ -1,3 +1,40 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-$DTE.ItemOperations.Navigate("http://getglimpse.com/?id=" + $package.Id + "/" + $package.Version)
+$from = $null
+
+$tempDir = $env:TEMP
+$log = [System.IO.Path]::Combine($tempDir,"Glimpse\install.log")
+
+if ([System.IO.File]::Exists($log))
+{
+    $lines = Get-Content $log | where {$_.Contains($project.FileName)} | where {$_.Contains($package.Id)}
+
+    if ($lines -ne $null){
+		    
+		if ($lines.GetType().Name -eq 'String'){
+			$lastline = $lines
+		} else {
+			$lastLine = $lines[$lines.Length-1]
+		}
+		       
+        $props = $lastLine.Split(',')
+        
+        $versionRemoved = $props[1]
+        $timeRemoved = $props[2]
+        $timeRemoved = Get-Date -Date $timeRemoved
+        $now = Get-Date
+        
+        $delta = New-TimeSpan $timeRemoved $now
+        
+        if ($delta.Minutes -lt 5){
+            $from = $versionRemoved
+        }
+    }
+}
+
+if ($from -ne $null -and $from -ne $package.Version){
+    $DTE.ItemOperations.Navigate("http://getglimpse.com/?id=" + $package.Id + "&v=" + $package.Version + "&from=" + $from)
+}
+else{
+    $DTE.ItemOperations.Navigate("http://getglimpse.com/?id=" + $package.Id + "&v=" + $package.Version)
+}
---
+++ source/Glimpse.Core/NuSpec/tools/uninstall.ps1
@@ -0,0 +1,9 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$tempDir = $env:TEMP
+$tempDir = [System.IO.Path]::Combine($tempDir,"Glimpse")
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$file = [System.IO.Path]::Combine($tempDir, "install.log")
+$datetime = Get-Date
+
+$package.Id + "," + $package.Version + "," + $datetime.ToString() + "," + $project.FileName | out-file $file -append
