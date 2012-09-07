b00bfac6ffdad5a3c5a7a7c5e412e3b3f2d93527
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-09-07 11:52:10 +1000

    Starting work for the shell controls

---
+++ source/Glimpse.JavaScript/Modules/glimpse.shell.controls.js
@@ -0,0 +1,22 @@
+﻿(function($) {
+    var wireListeners = function () {
+        
+            elements.scope.find('.glimpse-open').click(function () { pubsub.publish('action.open', false); return false; });
+            elements.scope.find('.glimpse-minimize').click(function () { pubsub.publish('action.minimize'); return false; });
+            elements.scope.find('.glimpse-close').click(function () { pubsub.publish('action.close'); return false; });
+        },
+        open = function() {
+
+        },
+        minimize = function() {
+
+        },
+        close = function() {
+
+        };
+    
+    pubsub.subscribe('trigger.shell.open', open);
+    pubsub.subscribe('trigger.shell.minimize', minimize);
+    pubsub.subscribe('trigger.shell.close', close);
+    pubsub.subscribe('trigger.shell.listener.subscriptions', wireListeners);
+})(jQueryGlimpse);
