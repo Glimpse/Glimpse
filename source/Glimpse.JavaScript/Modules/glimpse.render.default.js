3fd4f15e0bb91cd39617d4bdd6552ee8314be532
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-09-10 21:35:33 +1000

    Added new defalt module which takes care of how the system state at render (i.e. open/closed, etc)

---
+++ source/Glimpse.JavaScript/Modules/glimpse.render.default.js
@@ -0,0 +1,20 @@
+﻿(function(settings, pubsub, elements) { 
+    var start = function () {
+            var current = settings.local('view'),
+                isOpen = settings.local('isOpen');
+        
+            if (!current)
+                current = elements.tabHolder().find('li:not(.glimpse-active, .glimpse-disabled):first').attr('data-glimpseKey'); 
+            pubsub.publish('trigger.tab.select.' + current, { key: current });
+        
+            if (isOpen) {
+                pubsub.publish('trigger.shell.open', true);
+            }
+    },
+        selected = function (options) {
+            settings.local('view', options.key);
+        };
+
+    pubsub.subscribe('trigger.shell.default.view', start);
+    pubsub.subscribe('trigger.tab.select', selected);
+})(glimpse.settings, glimpse.pubsub, glimpse.elements);
