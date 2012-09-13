5b7a2b062e2390465a20684a0e375e1d9c21977d
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-09-13 11:45:46 +1000

    Version check module ported over

---
+++ source/Glimpse.JavaScript/Modules/glimpse.shell.version.js
@@ -0,0 +1,48 @@
+﻿glimpse.versionCheck = (function ($, pubsub, settings, elements) {
+    var wireListeners = function () {
+        
+        },
+        retrieveStamp = function () {
+            if (!settings.local('stamp'))
+                settings.local('stamp', (new Date()).getTime()); 
+            return settings.local('stamp');
+        },
+        ready = function() {
+            var nextChecked = local('nextCheckedVersionTime'),
+                hasNewerVersion = local('hasNewerVersion'),
+                now = new Date();
+            
+            if (hasNewerVersion)
+                elements.holder.find('.glimpse-meta-update').show();
+
+            if (nextChecked) {
+                var nextCheckedTickes = parseInt(nextChecked),
+                    currentTimeTickes = now.getTime();
+                if (nextCheckedTickes > currentTimeTickes)
+                    return;
+            }
+            
+            var metadata = data.currentMetadata(),
+                url = util.replaceTokens(metadata.resources.glimpse_version_check, { stamp: retrieveStamp() });
+            
+            $.ajax({
+                url: url, 
+                type: 'GET',
+                dataType: 'jsonp',
+                crossDomain: true,
+                jsonpCallback: 'glimpse.versionCheck.result'   //TODO: Need to setup a correct callback
+            });
+            
+            local('nextCheckedVersionTime', now.setDate(now.getDate() + 1).getTime());
+        },
+        result = function(data) {
+            local('hasNewerVersion', data.hasNewer);
+        };
+    
+    pubsub.subscribe('trigger.shell.listener.subscriptions', wireListeners);
+    pubsub.subscribe('trigger.system.ready', ready);
+
+    return {
+        result: result
+    };
+})(jQueryGlimpse, glimpse.pubsub, glimpse.settings, glimpse.elements)
