4f9c549cfa7044c08ca90f818efff3d8eb92955c
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-09-24 22:05:07 +1000

    Formatting fixes to prg module

--- source/Glimpse.JavaScript/Modules/glimpse.shell.prg.js
+++ source/Glimpse.JavaScript/Modules/glimpse.shell.prg.js
@@ -1,7 +1,7 @@
-﻿(function ($, data, elements) {
+﻿(function($, data, elements) {
     var wireListeners = function() {
             elements.title().find('.glimpse-url a').live('click', function() { data.retrieve($(this).attr('data-requestId'), 'prg'); });
-        }, 
+        },
         buildHtml = function(request, requestMetadata) {
             var correlation = requestMetadata.correlation,
                 html = request.uri;
@@ -22,21 +22,21 @@
                 html = '<span class="glimpse-drop">' + currentLeg + '<span class="glimpse-drop-arrow-holder"><span class="glimpse-drop-arrow"></span></span></span><div class="glimpse-drop-over">' + html + '<div class="loading"><span class="icon"></span><span>Loaded...</span></div></div>';
             }
             return html;
-        }. 
+        }.
         render = function() {
             var html = buildHtml(data.currentData(), data.currentMetadata());
-            
+
             elements.titleHolder().find('.glimpse-url').html(html);
-        }, 
+        },
         startingRetrieve = function() {
             elements.titleHolder().find('.glimpse-url .loading').fadeIn();
         },
         completedRetrieve = function() {
             elements.titleHolder().find('.glimpse-url .loading').fadeOut();
         };
-    
-    pubsub.subscribe('action.shell.title.loaded', render); 
+
+    pubsub.subscribe('action.shell.loaded', render);
     pubsub.subscribe('trigger.shell.listener.subscriptions', wireListeners);
     pubsub.subscribe('action.data.retrieve.starting.prg', startingRetrieve);
     pubsub.subscribe('action.data.retrieve.completed.prg', completedRetrieve);
-})(jQueryGlimpse, glimpse.data, glimpse.elements)
+})(jQueryGlimpse, glimpse.data, glimpse.elements);
