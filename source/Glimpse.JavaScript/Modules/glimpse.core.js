bb205e16aec7f640ce2bb6656f5e47e521a2ca42
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-09-25 12:14:25 +1000

    Moved trigger of UI render to when we have data
    Previously it was on document ready which could be slower

--- source/Glimpse.JavaScript/Modules/glimpse.core.js
+++ source/Glimpse.JavaScript/Modules/glimpse.core.js
@@ -1,21 +1,3 @@
-﻿glimpse = (function($) {
-    var obj = {},
-        ready = function() {
-            obj.pubsub.publish('trigger.system.init');
-            
-            obj.pubsub.publish('action.system.starting'); 
-            obj.pubsub.publish('trigger.shell.render');
-            obj.pubsub.publish('action.system.started');
-            
-            obj.pubsub.publish('trigger.shell.default.view');
-            
-            obj.pubsub.publish('trigger.system.ready');
-        };
-
-    $(function() {
-        obj.pubsub.subscribe('trigger.system.start', ready); 
-        obj.pubsub.publishAsync('trigger.system.start');
-    });
-
-    return obj;
+﻿glimpse = (function($) { 
+    return {};
 })(jQueryGlimpse);
---
+++ source/Glimpse.JavaScript/Modules/glimpse.ready.js
@@ -0,0 +1,20 @@
+﻿(function(pubsub) {
+    var ready = function() {
+            pubsub.publish('trigger.system.init');
+            
+            pubsub.publish('action.system.starting'); 
+            pubsub.publish('trigger.shell.render');
+            pubsub.publish('action.system.started');
+            
+            pubsub.publish('trigger.shell.default.view');
+            
+            pubsub.publish('trigger.system.ready');
+        },
+        start = function() {
+            // Trigger this as async 
+            pubsub.publishAsync('trigger.system.start');
+        };
+
+    pubsub.subscribe('trigger.system.start', ready); 
+    pubsub.subscribe('action.data.initial.changed', start);
+})(glimpse.pubsub);
