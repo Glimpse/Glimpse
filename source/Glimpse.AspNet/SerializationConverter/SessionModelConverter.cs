4396270ddd48adf98b44dde32c6d7a679650f539
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-10-29 10:41:26 +1000

    Minor code cleanup in the session tab

--- source/Glimpse.AspNet/SerializationConverter/SessionModelConverter.cs
+++ source/Glimpse.AspNet/SerializationConverter/SessionModelConverter.cs
@@ -9,11 +9,12 @@ namespace Glimpse.AspNet.SerializationConverter
     {
         public override object Convert(List<SessionModel> obj)
         {
-            var result = new List<object[]> { new[] {"Key", "Value", "Type"} };
+            var result = new List<object[]> { new[] { "Key", "Value", "Type" } };
             foreach (var item in obj)
             {
                 result.Add(new[] { item.Key, item.Value, item.Type });
             }
+
             return result;
         }
     }
