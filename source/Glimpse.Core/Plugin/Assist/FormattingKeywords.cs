93b639c93a204423513a9b8133021d42a7ab6b17
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-11-03 11:39:31 +1000

    Convert FormattingKeyword enum to it class name counterparts

--- source/Glimpse.Core/Plugin/Assist/FormattingKeywords.cs
+++ source/Glimpse.Core/Plugin/Assist/FormattingKeywords.cs
@@ -10,5 +10,30 @@
         public const string Quiet = "quiet";
         public const string Selected = "selected";
         public const string Warn = "warn";
+
+        public static string Convert(FormattingKeywordEnum keyword)
+        {
+            switch (keyword)
+            {
+                case FormattingKeywordEnum.Error:
+                    return Error;
+                case FormattingKeywordEnum.Fail:
+                    return Fail;
+                case FormattingKeywordEnum.Info:
+                    return Info;
+                case FormattingKeywordEnum.Loading:
+                    return Loading;
+                case FormattingKeywordEnum.Quite:
+                    return Quiet;
+                case FormattingKeywordEnum.Selected:
+                    return Selected;
+                case FormattingKeywordEnum.System:
+                    return Ms;
+                case FormattingKeywordEnum.Warn:
+                    return Warn;
+                default:
+                    return null;
+            }
+        }
     }
 }
