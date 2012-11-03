a48dea5d21e57d15746993103f7910c1a79ca111
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-11-03 11:37:07 +1000

    Add new enum for Formatting Keywords

---
+++ source/Glimpse.Core/Plugin/Assist/FormattingKeywordEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace Glimpse.Core.Plugin.Assist
+{
+    public enum FormattingKeywordEnum
+    {
+        None,
+        Warn,
+        Info,
+        Error,
+        Fail,
+        Quite,
+        Loading,
+        Selected,
+        System
+    }
+}
