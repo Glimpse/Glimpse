7970fb0a004e4a48b16df9158da3b7c51f70395e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-11-03 11:45:45 +1000

    Trace tab implementation

---
+++ source/Glimpse.AspNet/SerializationConverter/TraceModelConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Glimpse.AspNet.Model;
+using Glimpse.Core.Extensibility;
+using Glimpse.Core.Plugin.Assist;
+
+namespace Glimpse.AspNet.SerializationConverter
+{
+    public class TraceModelConverter : SerializationConverter<IList<TraceModel>>
+    {
+        public override object Convert(IList<TraceModel> obj)
+        {
+            var root = new TabSection("Category", "Trace", "From First", "From Last");
+            foreach (var item in obj)
+            {
+                root.AddRow().Column(item.Category == FormattingKeywordEnum.None ? string.Empty : item.Category.ToString()).Column(item.Message).Column(item.FromFirst.ToString("0.##")).Column(item.FromLast.ToString("0.##")).Style(item.Category);
+            }
+
+            return root.Build();
+        } 
+    }
+}
