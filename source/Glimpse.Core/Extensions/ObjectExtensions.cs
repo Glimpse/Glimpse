5356e53d7d89a15c2a418061f5ed42a3981b4799
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-11-05 22:03:59 +1000

    New extension method to safely convert values

---
+++ source/Glimpse.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glimpse.Core.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static string ToStringOrDefault(this object value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            return value.ToString();
+        }
+    }
+}
