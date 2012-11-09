bb60013d86f25d780090b6a323be51be2de9f2f7
nikmd23 <nikmd23@gmail.com>
2012-11-09 13:29:48 -0500

    Cleaned up message tests

---
+++ source/Glimpse.Test.Mvc3/Message/TimerResultMessageShould.cs
@@ -0,0 +1,22 @@
+﻿using Glimpse.Core.Extensibility;
+using Glimpse.Core.Message;
+using Glimpse.Test.Common;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Glimpse.Test.Mvc3.Message
+{
+    public class TimerResultMessageShould
+    {
+        [Theory, AutoMock]
+        public void Construct(TimerResult timerResult, string expectedEventName, string expectedCategory)
+        {
+            var sut = new TimerResultMessage(timerResult, expectedEventName, expectedCategory);
+
+            Assert.Equal(expectedCategory, sut.EventCategory);
+            Assert.Equal(timerResult.Duration, sut.Duration);
+            Assert.Equal(expectedEventName, sut.EventName);
+            Assert.Equal(timerResult.Offset, sut.Offset);
+        }
+    }
+}
