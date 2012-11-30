f6100ca45a602b17ae179540a51938267286c030
nikmd23 <nikmd23@gmail.com>
2012-11-30 15:56:25 -0500

    Removed todo items for missing resources: paging and lazy loading

--- source/Glimpse.Core/Framework/GlimpseMetadata.cs
+++ source/Glimpse.Core/Framework/GlimpseMetadata.cs
@@ -8,12 +8,7 @@ namespace Glimpse.Core.Framework
         {
             Plugins = new Dictionary<string, PluginMetadata>();
 
-            // TODO: once the resources below are implemented, this constructor should just instantiate variable.
-            Resources = new Dictionary<string, string>
-                        {
-                            { "paging", "NEED TO IMPLMENT RESOURCE Pager" }, // TODO: Implement resource
-                            { "tab", "NEED TO IMPLMENT RESOURCE test-popup.html" }, // TODO: Implement resource
-                        };
+            Resources = new Dictionary<string, string>();
         }
 
         public string Version { get; set; }
