84f950c36a8f1bd820baaa882ee108a2b743b6b2
nikmd23 <nikmd23@gmail.com>
2012-12-05 14:45:53 -0500

    Made OnResultExecuted.Message read only

--- source/Glimpse.Mvc3/AlternateImplementation/ResultFilter.cs
+++ source/Glimpse.Mvc3/AlternateImplementation/ResultFilter.cs
@@ -79,11 +79,11 @@ namespace Glimpse.Mvc.AlternateImplementation
                     ExceptionHandled = context.ExceptionHandled;
                 }
 
-                public bool ExceptionHandled { get; set; }
+                public bool ExceptionHandled { get; private set; }
 
-                public Type ExceptionType { get; set; }
+                public Type ExceptionType { get; private set; }
 
-                public bool Canceled { get; set; }
+                public bool Canceled { get; private set; }
 
                 public override void BuildDetails(IDictionary<string, object> details)
                 {
