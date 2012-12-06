acc39cc1d47b7462d60826f5e1af8a3364a1bfce
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2012-12-06 14:51:20 -0500

    Refactoring hud sections into modules

--- source/Glimpse.JavaScript/Modules/glimpse.hud.js
+++ source/Glimpse.JavaScript/Modules/glimpse.hud.js
@@ -2,58 +2,65 @@
     var loaded = function() {
             var html = '';
 
-            html += processClientTimings(); 
-            
+            html += clientTimings.render();
+
             elements.opener().find('tr').prepend('<td class="glimpse-hud">' + html + '</td>');
         },
-        processClientTimings = function() {
-            var html = '';
-            
-            if (window.performance.timing) {
-                var timingSum = 0,
-                    timing = populateClientTimings();
-                    
-                html += '<div class="glimpse-hud-section"><span class="glimpse-hud-title">Timings</span><table class="glimpse-hud-graph-bar"><tr>';
-                for (var key in timing) {
-                    var category = timing[key];
-                    
-                    timingSum += category.duration; 
-                    html += '<td style="background-color:' + category.categoryColor + ';width:' + category.percentage + '%" title="' + category.label + ' timing - ' + category.duration + ' ms"></td>';
-                }
-                
-                html += '</tr></table><span class="glimpse-hud-details"><span title="Total request time">' + timingSum + ' ms</span> ';
-                html += '(<span title="Network timing">' + timing.network.duration + ' ms</span> / <span title="Server timing">' + timing.server.duration + ' ms</span> / <span title="Browser timing">' + timing.browser.duration + ' ms</span>)';
-                html += '</span></div>';
-            }
+        clientTimings = (function() {
+            var timingApi = window.performance.timing,
+                render = function() {
+                    var html = '';
 
-            return html;
-        },
-        populateClientTimings = function() {
-            var result = {},
-                timingApi = window.performance.timing,
-                timingOrder = ['navigationStart', 'redirectStart', 'redirectStart', 'redirectEnd', 'fetchStart', 'domainLookupStart', 'domainLookupEnd', 'connectStart', 'secureConnectionStart', 'connectEnd', 'requestStart', 'responseStart', 'responseEnd', 'unloadEventStart', 'unloadEventEnd', 'domLoading', 'domInteractive', 'msFirstPaint', 'domContentLoadedEventStart', 'domContentLoadedEventEnd', 'domContentLoaded', 'domComplete', 'loadEventStart', 'loadEventEnd'],
-                start = timingApi.navigationStart || 0,
-                network = calculateClientTimings(start, timingApi, timingOrder, 'navigationStart', 'connectEnd'),
-                server = calculateClientTimings(start, timingApi, timingOrder, 'requestStart', 'responseEnd'),
-                browser = calculateClientTimings(start, timingApi, timingOrder, 'unloadEventStart', 'loadEventEnd'),
-                total = network + server + browser;
+                    if (timingApi) {
+                        var timingSum = 0,
+                            timing = populate();
 
-            result.network = { label: 'Network', categoryColor: '#FD4545', duration: network, percentage: (network / total) * 100 };
-            result.server = { label: 'Server', categoryColor: '#823BBE', duration: server, percentage: (server / total) * 100 };
-            result.browser = { label: 'Browser', categoryColor: '#5087CF', duration: browser, percentage: (browser / total) * 100 };
+                        html += '<div class="glimpse-hud-section"><span class="glimpse-hud-title">Timings</span><table class="glimpse-hud-graph-bar"><tr>';
+                        for (var key in timing) {
+                            var category = timing[key];
 
-            return result;
-        },
-        calculateClientTimings = function(start, timingApi, timingOrder, startIndex, finishIndex) {
-            var total = 0; 
-            for (var i = timingOrder.indexOf(startIndex); i <= timingOrder.indexOf(finishIndex); i++) {
-                var value = timingApi[timingOrder[i]];
-                if (value && value > total) { 
-                    total = (value - start);
-                }
-            } 
-            return total;
-        }
+                            timingSum += category.duration;
+                            html += '<td style="background-color:' + category.categoryColor + ';width:' + category.percentage + '%" title="' + category.label + ' timing - ' + category.duration + ' ms"></td>';
+                        }
+
+                        html += '</tr></table><span class="glimpse-hud-details"><span title="Total request time">' + timingSum + ' ms</span> ';
+                        html += '(<span title="Network timing">' + timing.network.duration + ' ms</span> / <span title="Server timing">' + timing.server.duration + ' ms</span> / <span title="Browser timing">' + timing.browser.duration + ' ms</span>)';
+                        html += '</span></div>';
+                    }
+
+                    return html;
+                },
+                populate = function() {
+                    var result = { },
+                        timingOrder = ['navigationStart', 'redirectStart', 'redirectStart', 'redirectEnd', 'fetchStart', 'domainLookupStart', 'domainLookupEnd', 'connectStart', 'secureConnectionStart', 'connectEnd', 'requestStart', 'responseStart', 'responseEnd', 'unloadEventStart', 'unloadEventEnd', 'domLoading', 'domInteractive', 'msFirstPaint', 'domContentLoadedEventStart', 'domContentLoadedEventEnd', 'domContentLoaded', 'domComplete', 'loadEventStart', 'loadEventEnd'],
+                        start = timingApi.navigationStart || 0,
+                        network = calculateTimings(start, timingOrder, 'navigationStart', 'connectEnd'),
+                        server = calculateTimings(start, timingOrder, 'requestStart', 'responseEnd'),
+                        browser = calculateTimings(start, timingOrder, 'unloadEventStart', 'loadEventEnd'),
+                        total = network + server + browser;
+
+                    result.network = { label: 'Network', categoryColor: '#FD4545', duration: network, percentage: (network / total) * 100 };
+                    result.server = { label: 'Server', categoryColor: '#823BBE', duration: server, percentage: (server / total) * 100 };
+                    result.browser = { label: 'Browser', categoryColor: '#5087CF', duration: browser, percentage: (browser / total) * 100 };
+
+                    return result;
+                },
+                calculateTimings = function(start, timingOrder, startIndex, finishIndex) {
+                    var total = 0;
+                    for (var i = timingOrder.indexOf(startIndex); i <= timingOrder.indexOf(finishIndex); i++) {
+                        var value = timingApi[timingOrder[i]];
+                        if (value && value > total) {
+                            total = (value - start);
+                        }
+                    }
+                    return total;
+                };
+
+            return {
+                render: render
+            };
+        })();
+        
         /*
         //Resource Spike 
         processResourceEntities = function() {
