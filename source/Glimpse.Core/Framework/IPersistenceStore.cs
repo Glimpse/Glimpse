52021b1b1081efb9de0c7ff8c362c0de12be5692
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-02 14:13:31 +1000

    Doco for IPersistenceStore

--- source/Glimpse.Core/Framework/IPersistenceStore.cs
+++ source/Glimpse.Core/Framework/IPersistenceStore.cs
@@ -1,9 +1,21 @@
 namespace Glimpse.Core.Framework
 {
+    /// <summary>
+    /// Defines methods to implement a store that Glimpse can 
+    /// use to persist requests.
+    /// </summary>
     public interface IPersistenceStore : IReadOnlyPersistenceStore
     {
+        /// <summary>
+        /// Saves the specified request.
+        /// </summary>
+        /// <param name="request">The request.</param>
         void Save(GlimpseRequest request);
-        
+
+        /// <summary>
+        /// Saves the specified metadata.
+        /// </summary>
+        /// <param name="metadata">The metadata.</param>
         void Save(GlimpseMetadata metadata);
     }
 }
