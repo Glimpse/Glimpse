37dd4be6a3c6684a3a99b4b749058fe62598b1a0
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-03 07:51:40 +1000

    Doco for IPrivilegedResource

--- source/Glimpse.Core/Extensibility/IPrivilegedResource.cs
+++ source/Glimpse.Core/Extensibility/IPrivilegedResource.cs
@@ -2,8 +2,21 @@
 
 namespace Glimpse.Core.Extensibility
 {
+    /// <summary>
+    /// Definition of a resource that has access to higher privileges than a 
+    /// normal resource.
+    /// </summary>
     internal interface IPrivilegedResource : IResource
     {
+        /// <summary>
+        /// Executes the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="configuration">The configuration.</param>
+        /// <returns>IResourceResult.</returns>
+        /// <remarks>
+        /// Normal resources shouldn't have access to the <see cref="IGlimpseConfiguration"/>
+        /// </remarks>
         IResourceResult Execute(IResourceContext context, IGlimpseConfiguration configuration); 
     }
 }
