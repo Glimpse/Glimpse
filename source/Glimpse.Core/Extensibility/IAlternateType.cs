a8bf1216d5652e68c3270b6bc1e710563d2abd13
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-02 15:57:29 +1000

    Doco for IAlternateType

--- source/Glimpse.Core/Extensibility/IAlternateType.cs
+++ source/Glimpse.Core/Extensibility/IAlternateType.cs
@@ -1,7 +1,16 @@
 ﻿namespace Glimpse.Core.Extensibility
 {
+    /// <summary>
+    /// Definition for an alternate that should replace a given Type
+    /// </summary>
     public interface IAlternateType<T>
     {
+        /// <summary>
+        /// Tries to create an alternate for the given target.
+        /// </summary>
+        /// <param name="originalObj">The original obj.</param>
+        /// <param name="newObj">The new obj.</param>
+        /// <returns><c>true</c> if the create was successful, <c>false</c> otherwise</returns>
         bool TryCreate(T originalObj, out T newObj);
     }
 }
