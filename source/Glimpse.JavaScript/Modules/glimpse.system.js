bde446202a06e3c3eb1eda1d04f62c32542615c1
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-20 00:41:41 +1000

    Remove not needed timeout

--- source/Glimpse.JavaScript/Modules/glimpse.system.js
+++ source/Glimpse.JavaScript/Modules/glimpse.system.js
@@ -1,15 +1,12 @@
 ﻿(function(pubsub) {
-    var start = function() {
-            // Trigger this as async 
-            setTimeout(function() {
-                pubsub.publish('trigger.system.init');
+    var start = function() { 
+            pubsub.publish('trigger.system.init');
             
-                pubsub.publish('action.system.starting'); 
-                pubsub.publish('trigger.shell.init');
-                pubsub.publish('action.system.started');
+            pubsub.publish('action.system.starting'); 
+            pubsub.publish('trigger.shell.init');
+            pubsub.publish('action.system.started');
             
-                pubsub.publish('trigger.system.ready');
-            }, 0);
+            pubsub.publish('trigger.system.ready'); 
         },
         refresh = function() {
             pubsub.publish('trigger.shell.refresh'); 
