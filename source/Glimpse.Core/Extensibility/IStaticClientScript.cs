911ff473495ba48ae98253225611ee2c2156b188
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-23 14:46:55 +1000

    Documentation for IStaticClientScript

--- source/Glimpse.Core/Extensibility/IStaticClientScript.cs
+++ source/Glimpse.Core/Extensibility/IStaticClientScript.cs
@@ -1,8 +1,20 @@
 ﻿namespace Glimpse.Core.Extensibility
 {
-    // TODO: Provide a config only way to create static client scripts without implementing a class
+    /// <summary>
+    /// Definition for a static client script that will be injected into response payloads.
+    /// TODO: Provide a config only way to create static client scripts without implementing a class
+    /// </summary>
+    /// <remarks>
+    /// This interface only returns URIs that will be included in the payload. It doesn't 
+    /// detail with any raw content in these files.
+    /// </remarks>
     public interface IStaticClientScript : IClientScript
     {
+        /// <summary>
+        /// Gets the URI for where the script is registered.
+        /// </summary>
+        /// <param name="version">The version.</param>
+        /// <returns>The URI where the asset lives.</returns>
         string GetUri(string version);
     }
 }
