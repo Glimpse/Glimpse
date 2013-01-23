55dfd72b8066c4848b02bf7fdd0c5d9779936b38
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-23 12:41:57 +1000

    Documentation for IRuntimePolicy

--- source/Glimpse.Core/Extensibility/IRuntimePolicy.cs
+++ source/Glimpse.Core/Extensibility/IRuntimePolicy.cs
@@ -1,9 +1,29 @@
 ﻿namespace Glimpse.Core.Extensibility
 {
+    /// <summary>
+    /// <para>
+    /// Definition for a runtime policy that will govern whether a 
+    /// the system should run for a particular request. 
+    /// </para>
+    /// <para>
+    /// When implemented, this policy will be added to the pipeline of 
+    /// other registered policies. Together these policies determine 
+    /// what level of access the system has.
+    /// </para>
+    /// </summary>
     public interface IRuntimePolicy
     {
+        /// <summary>
+        /// Gets when the policy should run.
+        /// </summary>
+        /// <value>The level value.</value>
         RuntimeEvent ExecuteOn { get; }
-        
+
+        /// <summary>
+        /// Executes the specified policy in a given context.
+        /// </summary>
+        /// <param name="policyContext">The policy context.</param>
+        /// <returns>What the request is allowed to do.</returns>
         RuntimePolicy Execute(IRuntimePolicyContext policyContext);
     }
 }
