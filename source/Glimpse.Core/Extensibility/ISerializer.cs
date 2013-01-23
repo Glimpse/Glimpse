fda7f8f9ac873e2b48af530f99222a69c869c785
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-23 14:37:04 +1000

    Documentation for ISerializer

--- source/Glimpse.Core/Extensibility/ISerializer.cs
+++ source/Glimpse.Core/Extensibility/ISerializer.cs
@@ -2,10 +2,23 @@ using System.Collections.Generic;
 
 namespace Glimpse.Core.Extensibility
 {
+    /// <summary>
+    /// Definition for a provider that can serialize complex objects to JSON.
+    /// </summary>
     public interface ISerializer
     {
+        /// <summary>
+        /// Serializes the specified object to JSON.
+        /// </summary>
+        /// <param name="obj">The target to be Serialized.</param>
+        /// <returns>Serialized object.</returns>
         string Serialize(object obj);
-        
+
+        /// <summary>
+        /// Registers a collection of serialization converters which can conduct custom
+        /// transformations on given types when processed.
+        /// </summary>
+        /// <param name="converters">The converters.</param>
         void RegisterSerializationConverters(IEnumerable<ISerializationConverter> converters);
     }
 }
