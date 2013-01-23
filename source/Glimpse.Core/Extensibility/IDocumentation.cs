9963815435bcbcff3159f44bbd116c3be11c4afe
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-23 11:09:53 +1000

    Documentation for IDocumentation

--- source/Glimpse.Core/Extensibility/IDocumentation.cs
+++ source/Glimpse.Core/Extensibility/IDocumentation.cs
@@ -1,7 +1,19 @@
 ﻿namespace Glimpse.Core.Extensibility
 {
+    /// <summary>
+    /// Allows a <see cref="ITab"/> to define what URI it should use if the Tab has supporting
+    /// documentation.
+    /// </summary>
+    /// <remarks>
+    /// If this interface isn't implemented on a <see cref="ITab"/> no documentation will be 
+    /// provided.
+    /// </remarks>
     public interface IDocumentation
     {
+        /// <summary>
+        /// Gets the documentation URI.
+        /// </summary>
+        /// <value>The documentation URI.</value>
         string DocumentationUri { get; }
     }
 }
