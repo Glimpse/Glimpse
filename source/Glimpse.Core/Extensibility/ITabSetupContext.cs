9097d15cd963df90ff6db1192b1dbebbeb080a36
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-23 16:04:27 +1000

    Documentation for ITabSetupContext

--- source/Glimpse.Core/Extensibility/ITabSetupContext.cs
+++ source/Glimpse.Core/Extensibility/ITabSetupContext.cs
@@ -1,9 +1,20 @@
 namespace Glimpse.Core.Extensibility
 {
+    /// <summary>
+    /// Definition for the context which is used during tab setup.
+    /// </summary>
     public interface ITabSetupContext : IContext
     {
+        /// <summary>
+        /// Gets the message broker.
+        /// </summary>
+        /// <value>The message broker.</value>
         IMessageBroker MessageBroker { get; }
-        
+
+        /// <summary>
+        /// Gets the tab store.
+        /// </summary>
+        /// <returns>Data store that can be used.</returns>
         IDataStore GetTabStore();
     }
 }
