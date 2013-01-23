8f0f21f68e0f961a1add8ee7b99bf8a57513c90e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-23 11:58:49 +1000

    Documentation for IResourceResultContext

--- source/Glimpse.Core/Extensibility/IResourceResultContext.cs
+++ source/Glimpse.Core/Extensibility/IResourceResultContext.cs
@@ -2,12 +2,28 @@ using Glimpse.Core.Framework;
 
 namespace Glimpse.Core.Extensibility
 {
+    /// <summary>
+    /// Definition for the context that is used when a resource result
+    /// is executed.
+    /// </summary>
     public interface IResourceResultContext : IContext
     {
+        /// <summary>
+        /// Gets the framework provider.
+        /// </summary>
+        /// <value>The framework provider.</value>
         IFrameworkProvider FrameworkProvider { get; }
-        
+
+        /// <summary>
+        /// Gets the serializer.
+        /// </summary>
+        /// <value>The serializer.</value>
         ISerializer Serializer { get; }
-        
+
+        /// <summary>
+        /// Gets the HTML encoder.
+        /// </summary>
+        /// <value>The HTML encoder.</value>
         IHtmlEncoder HtmlEncoder { get; }
     }
 }
