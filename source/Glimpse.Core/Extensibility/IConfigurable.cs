200fd66a6a1a3c1ccfef3541020059cb7f1a4b12
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-23 10:10:14 +1000

    Documentation for IConfigurable

--- source/Glimpse.Core/Extensibility/IConfigurable.cs
+++ source/Glimpse.Core/Extensibility/IConfigurable.cs
@@ -2,8 +2,20 @@
 
 namespace Glimpse.Core.Extensibility
 {
+    /// <summary>
+    /// Definition for an extension to <see cref="IRuntimePolicy"/> that allows the 
+    /// policy to receive current GlimpseSection.
+    /// </summary>
+    /// <remarks>
+    /// An example of its use it to pick up types that are to be used from the Glimpse 
+    /// section in the web.config.
+    /// </remarks>
     public interface IConfigurable
     {
+        /// <summary>
+        /// Allows a given policy using the specified configuration section.
+        /// </summary>
+        /// <param name="section">The Glimpse configuration section.</param>
         void Configure(GlimpseSection section);
     }
 }
