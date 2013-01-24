785f2ea911cd330168a42a660b5419342ebc4fb5
nikmd23 <nikmd23@gmail.com>
2013-01-24 15:51:28 -0500

    Documented the *.ClientScript namespace, and associated types. Sealed several types.

--- source/Glimpse.Core/ClientScript/Client.cs
+++ source/Glimpse.Core/ClientScript/Client.cs
@@ -3,13 +3,34 @@ using Glimpse.Core.Resource;
 
 namespace Glimpse.Core.ClientScript
 {
-    public class Client : IDynamicClientScript
+    /// <summary>
+    /// The <see cref="IDynamicClientScript"/> implementation responsible for adding the Glimpse JavaScript client <c>&lt;script&gt;</c> tag to a page response.
+    /// </summary>
+    public sealed class Client : IDynamicClientScript
     {
+        /// <summary>
+        /// Gets the sorting order in which a <c>&lt;script&gt;</c> tag will be injected a page response, relative to other implementations of <see cref="IClientScript" />.
+        /// </summary>
+        /// <value>
+        /// Any value from the <see cref="ScriptOrder" /> enumeration.
+        /// </value>
+        /// <remarks>
+        /// Multiple <see cref="IClientScript" />'s with the same <see cref="ScriptOrder" /> will be sorted in an indeterminate order.
+        /// </remarks>
         public ScriptOrder Order
         {
             get { return ScriptOrder.ClientInterfaceScript; }
         }
 
+        /// <summary>
+        /// Gets the name of the <see cref="IResource" /> to dynamically generate a Uri for.
+        /// </summary>
+        /// <returns>
+        /// Name of the <see cref="IResource" /> to link to.
+        /// </returns>
+        /// <remarks>
+        ///   <see cref="IResource" /> Uri generation is handled by implementations of <see cref="Glimpse.Core.Framework.ResourceEndpointConfiguration" />.
+        /// </remarks>
         public string GetResourceName()
         {
             return ClientResource.InternalName;
--- source/Glimpse.Core/Extensibility/IStaticClientScript.cs
+++ source/Glimpse.Core/Extensibility/IStaticClientScript.cs
@@ -1,20 +1,32 @@
 ﻿namespace Glimpse.Core.Extensibility
 {
     /// <summary>
-    /// Definition for a static client script that will be injected into response payloads.
-    /// TODO: Provide a config only way to create static client scripts without implementing a class
+    /// <c>IStaticClientScript</c>'s are a special type of <see cref="IClientScript"/> that sets the <c>&lt;script&gt;</c> tag's <c>src</c> attribute to a given uri.
     /// </summary>
     /// <remarks>
-    /// This interface only returns URIs that will be included in the payload. It doesn't 
-    /// detail with any raw content in these files.
+    /// It is up to the implementer of <c>IStaticClientScript</c> to host a given resource. 
     /// </remarks>
-    public interface IStaticClientScript : IClientScript
+    public interface IStaticClientScript : IClientScript // TODO: Provide a config only way to create static client scripts without implementing a class
     {
         /// <summary>
-        /// Gets the URI for where the script is registered.
+        /// Gets the Uri to set as the value for the <c>src</c> attribute.
         /// </summary>
-        /// <param name="version">The version.</param>
-        /// <returns>The URI where the asset lives.</returns>
+        /// <remarks>
+        /// The returned Uri should only point to JavaScript files since the <c>&lt;script&gt;</c> tag will be rendered with a <c>type='text/javascript'</c> attribute.
+        /// </remarks>
+        /// <param name="version">
+        /// A unique string that should be combined with the Uri to allow for Http cache invalidation.
+        /// <example>
+        /// In this example, the version parameter is appended to a query string argument.
+        /// <code>
+        /// public string GetUri(string version)
+        /// {
+        ///     return "//localhost/scripts/myScript.js?v=" + version;
+        /// }
+        /// </code>
+        /// </example>
+        /// </param>
+        /// <returns>A string Uri pointing to a JavaScript asset.</returns>
         string GetUri(string version);
     }
 }
