ee427a556278370c5fce3f346410f8607a1626d2
nikmd23 <nikmd23@gmail.com>
2013-01-29 15:21:07 -0500

    Update #230: Documented all of the Glimpse.Core.ResourceResult namespace

--- source/Glimpse.Core/ResourceResult/RedirectResourceResult.cs
+++ source/Glimpse.Core/ResourceResult/RedirectResourceResult.cs
@@ -5,12 +5,26 @@ using Tavis.UriTemplates;
 
 namespace Glimpse.Core.ResourceResult
 {
+    /// <summary>
+    /// The <see cref="IResourceResult"/> implementation responsible redirecting a client to a new Uri.
+    /// </summary>
     public class RedirectResourceResult : IResourceResult
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RedirectResourceResult" /> class without any parameter values in the Uri template.
+        /// </summary>
+        /// <param name="uriTemplate">The URI template.</param>
         public RedirectResourceResult(string uriTemplate) : this(uriTemplate, new Dictionary<string, object>())
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RedirectResourceResult" /> class.
+        /// </summary>
+        /// <param name="uriTemplate">The Uri template. Uri templates should conform to <see href="http://tools.ietf.org/html/rfc6570"/>RFC 6570</param>.
+        /// <param name="data">The Uri template parameter data.</param>
+        /// <exception cref="System.ArgumentNullException">Exception thrown if <paramref name="uriTemplate"/> is <c>null</c>.</exception>
+        /// <seealso href="http://tools.ietf.org/html/rfc6570">URI Template</seealso>
         public RedirectResourceResult(string uriTemplate, IDictionary<string, object> data)
         {
             if (string.IsNullOrEmpty(uriTemplate))
@@ -31,6 +45,10 @@ namespace Glimpse.Core.ResourceResult
 
         private UriTemplate UriTemplate { get; set; }
 
+        /// <summary>
+        /// Executes the resource result with the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
         public void Execute(IResourceResultContext context)
         {
             var data = Data;
