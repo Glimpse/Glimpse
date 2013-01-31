d95610f237ff68f760e410c5f33f75c9c2b35df4
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-01-31 13:03:11 +1000

    Fix the fact that output of functions wasn't being encoded.

--- source/Glimpse.JavaScript/glimpse.js
+++ source/Glimpse.JavaScript/glimpse.js
@@ -1172,7 +1172,7 @@ glimpse.render.engine.util.raw = (function($, util) {
             return buildOnly(data, level);
         }, 
         buildOnly = function (data, level) {
-            return data.toString();
+            return util.htmlEncode(data.toString());
         },
         buildPreview = function (data, level) { 
             return '<table class="glimpse-preview-table"><tr><td class="glimpse-preview-cell"><div class="glimpse-expand"></div></td><td><div class="glimpse-preview-object">' + buildPreviewOnly(data, level) + '</div><div class="glimpse-preview-show glimpse-code" data-codeType="js">' + buildOnly(data, level) + '</div></td></tr></table>';
