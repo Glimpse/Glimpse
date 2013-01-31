b6ab2592685ff9b623ded598c2ed24b60b4c4de4
nikmd23 <nikmd23@gmail.com>
2013-01-31 17:07:01 -0500

    Fix #205: Added HtmlHelper (MVC) and server control (WebForms) for situations where the HttpFilter is not viable (IE content compression)

---
+++ source/Glimpse.AspNet/Controls/Client.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Glimpse.AspNet.Extensions;
+
+namespace Glimpse.AspNet.Controls
+{
+    public class Client : WebControl
+    {
+        protected override void Render(HtmlTextWriter writer)
+        {
+            var tags = new HttpContextWrapper(Context).GenerateGlimpseScriptTags();
+
+            writer.Write(tags);
+        }
+    }
+}
---
+++ source/Glimpse.AspNet/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+
+namespace Glimpse.AspNet.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static string GenerateGlimpseScriptTags(this HttpContextBase context)
+        {
+            var generateScripts = context.Items["__GlimpseClientScriptsStrategy"] as Func<string>;
+
+            if (generateScripts == null)
+            {
+                return string.Empty;
+            }
+
+            return generateScripts();
+        }
+    }
+}
