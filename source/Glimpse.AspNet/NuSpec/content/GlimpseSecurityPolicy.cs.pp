53979e5c80f38ec5d32d38cb0b5595f79fa6aed2
Nik Molnar <nikmd23@gmail.com>
2013-03-04 13:53:45 -0500

    Fix #274 - Created GetHttpContext() extension method. Updated GlimpseSecurityPolicy.cs.pp to use it.

--- source/Glimpse.AspNet/NuSpec/content/GlimpseSecurityPolicy.cs.pp
+++ source/Glimpse.AspNet/NuSpec/content/GlimpseSecurityPolicy.cs.pp
@@ -1,8 +1,7 @@
 ﻿/*
 // Uncomment this class to provide custom runtime policy for Glimpse
 
-using System.Web;
-using Glimpse.Core;
+using Glimpse.AspNet.Extensions;
 using Glimpse.Core.Extensibility;
 
 namespace $rootnamespace$
@@ -12,8 +11,8 @@ namespace $rootnamespace$
         public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
         {
             // You can perform a check like the one below to control Glimpse's permissions within your application.
-			// var context = policyContext.GetRequestContext<HttpContextBase>();
-            // if (!context.User.IsInRole("Administrator"))
+			// var httpContext = policyContext.GetHttpContext();
+            // if (!httpContext.User.IsInRole("Administrator"))
 			// {
             //     return RuntimePolicy.Off;
 			// }
