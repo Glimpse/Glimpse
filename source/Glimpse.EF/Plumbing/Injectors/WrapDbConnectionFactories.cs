16cd84eec5100680cdb7b1b22a9c44f61dec40b3
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-03-06 16:37:02 +1000

    Update #283 - Missing changes from refactor

--- source/Glimpse.EF/Plumbing/Injectors/WrapDbConnectionFactories.cs
+++ source/Glimpse.EF/Plumbing/Injectors/WrapDbConnectionFactories.cs
@@ -28,11 +28,11 @@ namespace Glimpse.EF.Plumbing.Injectors
             {
            //     Logger.Info("AdoPipelineInitiator for EF: Starting to inject ConnectionFactory");
 
-                var code = GetEmbeddedResource(GetType().Assembly, "Glimpse.EF.Plumbing.Profiler.GlimpseProfileDbConnectionFactory.cs");
+                var code = GetEmbeddedResource(GetType().Assembly, "Glimpse.EF.Plumbing.AlternateType.GlimpseDbConnectionFactory.cs");
                 var assembliesToReference = new[] { type.Assembly, typeof(DbConnection).Assembly, typeof(TypeConverter).Assembly, typeof(IInspectorContext).Assembly };
 
                 var generatedAssembly = CreateAssembly(code, assembliesToReference);
-                var generatedType = generatedAssembly.GetType("Glimpse.EF.Plumbing.Profiler.GlimpseProfileDbConnectionFactory");
+                var generatedType = generatedAssembly.GetType("Glimpse.EF.AlternateType.GlimpseDbConnectionFactory");
                 generatedType.GetMethod("Initialize").Invoke(null, null);
 
                 //Logger.Info("AdoPipelineInitiator for EF: Finished to inject ConnectionFactory");
