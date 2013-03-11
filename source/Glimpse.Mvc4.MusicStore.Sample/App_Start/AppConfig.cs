e0f009699ac63d1e0727972986c41b56a0c28025
Nik Molnar <nikmd23@gmail.com>
2013-03-11 11:40:21 -0400

    Update #276 - Added MVC4 version of MVC Music Store as sample/test application.

---
+++ source/Glimpse.Mvc4.MusicStore.Sample/App_Start/AppConfig.cs
@@ -0,0 +1,39 @@
+﻿using MvcMusicStore.Filters;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace MvcMusicStore
+{
+    public static class AppConfig
+    {
+        public static void Configure()
+        {
+            System.Data.Entity.Database.SetInitializer(new MvcMusicStore.Models.SampleData());
+
+            CreateAdminUser();
+        }
+
+        private static void CreateAdminUser()
+        {
+            string _username = ConfigurationManager.AppSettings["DefaultAdminUsername"];
+            string _password = ConfigurationManager.AppSettings["DefaultAdminPassword"];
+            string _role = "Administrator";
+
+            new InitializeSimpleMembershipAttribute().OnActionExecuting(null);
+
+            if (!WebSecurity.UserExists(_username))
+                WebSecurity.CreateUserAndAccount(_username, _password);
+
+            if (!Roles.RoleExists(_role))
+                Roles.CreateRole(_role);
+
+            if (!Roles.IsUserInRole(_username, _role))
+                Roles.AddUserToRole(_username, _role);
+        }
+    }
+}
