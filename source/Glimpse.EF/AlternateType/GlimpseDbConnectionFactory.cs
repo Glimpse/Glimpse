f3f7f47a137f3b6f033f642ae9b236d812fff28f
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-03-12 19:13:12 +1000

    Update #276 - Simplified implementation Wrapping of DbConnectionFactory

---
+++ source/Glimpse.EF/AlternateType/GlimpseDbConnectionFactory.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+using System.Data.Common;
+using System.Data.Entity.Infrastructure; 
+using System.Reflection; 
+using Glimpse.Ado.AlternateType; 
+using Glimpse.Core.Extensibility;
+
+namespace Glimpse.EF.AlternateType
+{
+    public class GlimpseDbConnectionFactory : IDbConnectionFactory
+    {
+        private readonly IDbConnectionFactory inner; 
+        private readonly IInspectorContext stats;
+
+        public GlimpseDbConnectionFactory(IDbConnectionFactory inner, IInspectorContext stats)
+        {
+            this.inner = inner; 
+            this.stats = stats;
+        }
+
+        public DbConnection CreateConnection(string nameOrConnectionString)
+        {
+            var connection = inner.CreateConnection(nameOrConnectionString); 
+
+            //We need to pull out the factory from the connection 
+            var factory = connection.GetType().GetProperty("ProviderFactory", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(connection, null) as DbProviderFactory;
+            if (factory != null)
+            {
+                if (!(factory is GlimpseDbProviderFactory))
+                {
+                    var factoryType = typeof(GlimpseDbProviderFactory<>).MakeGenericType(factory.GetType());
+                    factory = (DbProviderFactory)factoryType.GetField("Instance").GetValue(null);
+                }
+            }
+
+            return new GlimpseDbConnection(connection, factory, stats, Guid.NewGuid());
+        }
+    }
+}
--- source/Glimpse.EF/Inspector/EntityFrameworkInspector.cs
+++ source/Glimpse.EF/Inspector/EntityFrameworkInspector.cs
@@ -1,17 +1,37 @@
+using System.Data.Common;
+using System.Data.Entity;
+using System.Reflection;
 using Glimpse.Core.Extensibility;
 using Glimpse.EF.Inspector.Support;
+using Glimpse.EF.AlternateType;
 
 namespace Glimpse.EF.Inspector
 {
     public class EntityFrameworkInspector : IInspector
     {
         public void Setup(IInspectorContext context)
-        {             
-            var wrapDbConnectionFactories = new WrapDbConnectionFactories();
-            wrapDbConnectionFactories.Inject();
+        {
+            InitDbConnectionFactories(context);
 
             var wrapCachedMetadata = new WrapCachedMetadata();
             wrapCachedMetadata.Inject();              
+        }
+
+        private void InitDbConnectionFactories(IInspectorContext context)
+        {
+            context.Logger.Info("EntityFrameworkInspector: Starting to replace DefaultConnectionFactory");  
+
+            var databaseType = typeof(Database);
+            var defaultConnectionFactoryChanged = (bool)databaseType.GetProperty("DefaultConnectionFactoryChanged", BindingFlags.Static | BindingFlags.DeclaredOnly | BindingFlags.NonPublic).GetValue(databaseType, null);
+
+            if (defaultConnectionFactoryChanged)
+            {
+                context.Logger.Info("EntityFrameworkInspector: Detected that user is using a custom DefaultConnectionFactory");  
+
+                Database.DefaultConnectionFactory = new GlimpseDbConnectionFactory(Database.DefaultConnectionFactory, context);
+            }
+
+            context.Logger.Info("EntityFrameworkInspector: Finished to replacing DefaultConnectionFactory");  
         }
     }
 }
