bf51e598d3ca153b9715c1e63bcd1cf06848b634
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-03-14 15:59:16 +1000

    Update#283 - Added core projects for ADO across multiple .net versions

---
+++ source/Glimpse.Ado.Net35/Backport/Net35Backport.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace Glimpse.AspNet.Net35.Backport
+{
+    public static class Net35Backport
+    {
+        public static bool IsNullOrWhiteSpace(string value)
+        {
+            if (value == null) return true;
+
+            return value.All(char.IsWhiteSpace);
+        }
+    }
+}
---
+++ source/Glimpse.Ado.Net35/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("b5a203a0-e464-485c-abc5-4c7f26871dd4")]
+
+
+[assembly: AssemblyTitle("Glimpse for ADO Assembly")]
+[assembly: AssemblyDescription("Main extensibility implementations for running Glimpse with ADO.")]
+[assembly: AssemblyProduct("Glimpse")]
+[assembly: AssemblyCopyright("© 2012 Nik Molnar & Anthony van der Hoorn")]
+[assembly: AssemblyTrademark("Glimpse™")]
+
+
+//Version is in major.minor.build format to support http://semver.org/
+//Keep these three attributes in sync
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+//[assembly: AssemblyInformationalVersion("1.0.0-rc")] Package version is driven off Glimpse.AspNet/Properties/AssemblyInfo.cs
