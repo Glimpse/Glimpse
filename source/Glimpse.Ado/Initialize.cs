d479cab32877ff98995f22249d00b5ddac9b3490
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-03-18 17:00:03 +1000

    Update #283 - Added initializer to take advantage of infrastructure in #287

---
+++ source/Glimpse.Ado/Initialize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Glimpse.Ado.Inspector.Core;
+
+namespace Glimpse.Ado
+{
+    public static class Initialize
+    {
+        public static void Ado(this Glimpse.Core.Setting.Initializer initializer)
+        {
+            var wrapDbProviderFactories = new WrapDbProviderFactories();
+            wrapDbProviderFactories.Execute();
+        }
+    }
+}
