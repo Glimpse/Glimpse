4cf89775961c0e5b3892c47410697fa2c3f8d824
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-03-18 16:58:36 +1000

    Fixed #287 - Infrastructure is now in place

---
+++ source/Glimpse.Core/System.cs
@@ -0,0 +1,18 @@
+﻿using Glimpse.Core.Setting;
+
+namespace Glimpse
+{
+    /// <summary>
+    /// Hook that is used to hang any system settings 
+    /// </summary>
+    public static class Settings
+    {
+        /// <summary>
+        /// Hook that is used to hang initialization code from. No method 
+        /// here is required ot be called. You should only need to use this 
+        /// code if Glimpse doesn't initialize soon enough in the application
+        /// life cycle for your purposes.
+        /// </summary>
+        public static Initializer Initialize = new Initializer();
+    }
+}
