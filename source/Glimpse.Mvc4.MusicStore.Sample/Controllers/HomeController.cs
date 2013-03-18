40279bc46bb2516d45dbf1a6be249d75ebc7c001
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-03-18 16:41:30 +1000

    Update #283 - Add raw ado test case to MVC4 project

--- source/Glimpse.Mvc4.MusicStore.Sample/Controllers/HomeController.cs
+++ source/Glimpse.Mvc4.MusicStore.Sample/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -17,6 +22,11 @@ namespace MvcMusicStore.Controllers
         {
             // Get most popular albums
             var albums = await Task.Factory.StartNew(() => GetTopSellingAlbums(6));
+            //var albums = GetTopSellingAlbums(6);
+
+            // Trigger some good old ADO code 
+            var albumCount = GetTotalAlbumns(); 
+            Trace.Write(string.Format("Total number of Albums = {0} and Albums with 'The' = {1}", albumCount.Item1, albumCount.Item2));
 
             return View(albums);
         }
@@ -31,6 +41,36 @@ namespace MvcMusicStore.Controllers
                 .OrderByDescending(a => a.OrderDetails.Count())
                 .Take(count)
                 .ToList();
+        }
+
+        private Tuple<int, int> GetTotalAlbumns()
+        {
+            var result1 = 0;
+            var result2 = 0;
+
+            var connectionString = ConfigurationManager.ConnectionStrings["MusicStoreEntities"];
+            var factory = DbProviderFactories.GetFactory(connectionString.ProviderName);
+            using (var connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString.ConnectionString;
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT COUNT(*) FROM Albums";
+                    command.CommandType = CommandType.Text;
+                    result1 = (int)command.ExecuteScalar();
+                }
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT COUNT(*) FROM Albums WHERE Title LIKE 'The%'";
+                    command.CommandType = CommandType.Text;
+                    result2 = (int)command.ExecuteScalar();
+                }
+            }
+
+            return new Tuple<int, int>(result1, result2);
         }
     }
 }
