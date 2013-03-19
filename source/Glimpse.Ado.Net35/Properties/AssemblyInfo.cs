d7f3f23b704eec6846b787cd99816ef0ad722f12
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-03-19 17:09:02 +1000

    Update #283 - Bring Ado Net35 AssemblyInfo inline with the rest of the projects

--- source/Glimpse.Ado.Net35/Properties/AssemblyInfo.cs
+++ source/Glimpse.Ado.Net35/Properties/AssemblyInfo.cs
@@ -1,9 +1,10 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Glimpse.Core.Extensibility;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("b5a203a0-e464-485c-abc5-4c7f26871dd4")]
-
 
 [assembly: AssemblyTitle("Glimpse for ADO Assembly")]
 [assembly: AssemblyDescription("Main extensibility implementations for running Glimpse with ADO.")]
@@ -11,9 +12,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© 2012 Nik Molnar & Anthony van der Hoorn")]
 [assembly: AssemblyTrademark("Glimpse™")]
 
-
-//Version is in major.minor.build format to support http://semver.org/
-//Keep these three attributes in sync
+// Version is in major.minor.build format to support http://semver.org/
+// Keep these three attributes in sync
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-//[assembly: AssemblyInformationalVersion("1.0.0-rc")] Package version is driven off Glimpse.AspNet/Properties/AssemblyInfo.cs
+[assembly: AssemblyInformationalVersion("1.0.0")] // Used to specify the NuGet version number at build time
+
+[assembly: InternalsVisibleTo("Glimpse.Test.ADO")]
+[assembly: NuGetPackage]
