e3144f80a9bdec9f4615816ea91da6de63b8c665
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-03-21 11:25:59 +1000

    Update #283 - Introduced an Initializer for EF so that things can be setup before client triggers EF setup code

---
+++ source/Glimpse.EF/Initialize.cs
@@ -0,0 +1,13 @@
+﻿using Glimpse.EF.Inspector.Core;
+
+namespace Glimpse.EF
+{
+    public static class Initialize
+    {
+        public static void EF(this Glimpse.Core.Setting.Initializer initializer)
+        {
+            var wrapper = new WrapDbConfigurationDbConnectionFactory();
+            wrapper.Execute();
+        }
+    }
+}
