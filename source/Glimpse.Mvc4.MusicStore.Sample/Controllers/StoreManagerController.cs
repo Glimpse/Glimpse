2b61690938498e34673b77c923181958628fbaa2
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-03-21 11:23:42 +1000

    Update #283 - Switch EF project and MVC4 sample project over to EF6

--- source/Glimpse.Mvc4.MusicStore.Sample/Controllers/StoreManagerController.cs
+++ source/Glimpse.Mvc4.MusicStore.Sample/Controllers/StoreManagerController.cs
@@ -88,7 +88,7 @@ namespace MvcMusicStore.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.Entry(album).State = EntityState.Modified;
+                db.Entry(album).State = System.Data.Entity.EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
