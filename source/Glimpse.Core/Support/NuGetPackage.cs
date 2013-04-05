7e85ecea23db1f3e3531102a2ef64f7755ee1740
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-04-05 16:56:31 +1000

    Abstracted out some common logic that should be able to be used by other components

---
+++ source/Glimpse.Core/Support/NuGetPackage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+using Glimpse.Core.Extensibility;
+
+namespace Glimpse.Core.Support
+{
+    public static class NuGetPackage
+    {
+        public static IDictionary<string, string> GetRegisteredPackageVersions()
+        {
+            var packages = new Dictionary<string, string>();
+
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var nugetPackage = assembly.GetCustomAttributes(typeof(NuGetPackageAttribute), false).Cast<NuGetPackageAttribute>().SingleOrDefault();
+                if (nugetPackage == null)
+                {
+                    continue;
+                }
+
+                var version = nugetPackage.GetVersion(assembly);
+                var id = nugetPackage.GetId(assembly);
+
+                packages[id] = version;
+            }
+
+            return packages;
+        }
+    }
+}
