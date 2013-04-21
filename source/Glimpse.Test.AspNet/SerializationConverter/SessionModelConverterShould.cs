c22630e5eb105f01aaa04fd575e2623e091eb3e3
Taylan Aydinli <taylanaydinli@gmail.com>
2013-04-21 18:27:00 +0300

    Fixed whitespaces in last commit

--- source/Glimpse.Test.AspNet/SerializationConverter/SessionModelConverterShould.cs
+++ source/Glimpse.Test.AspNet/SerializationConverter/SessionModelConverterShould.cs
@@ -25,7 +25,11 @@ namespace Glimpse.Test.AspNet.SerializationConverter
                 };
 
             object rows = new SessionModelConverter().Convert(sessionModels);
+<<<<<<< HEAD
 			List<object> columns = GetFirstValueRowFromConvertedSessionModel(rows);
+=======
+            List<object> columns = GetFirstValueRowFromConvertedSessionModel(rows);
+>>>>>>> Fixed whitespaces in last commit
 
             Assert.Equal(columns[0], "Key1");
             Assert.Equal(columns[1], "Test Value");
@@ -48,7 +52,11 @@ namespace Glimpse.Test.AspNet.SerializationConverter
                 };
 
             object rows = new SessionModelConverter().Convert(sessionModels);
+<<<<<<< HEAD
 			List<object> columns = GetFirstValueRowFromConvertedSessionModel(rows);
+=======
+            List<object> columns = GetFirstValueRowFromConvertedSessionModel(rows);
+>>>>>>> Fixed whitespaces in last commit
 
             Assert.Equal(columns[0], "Key1");
             Assert.Equal(columns[1], testObject);
@@ -71,7 +79,11 @@ namespace Glimpse.Test.AspNet.SerializationConverter
                 };
 
             object rows = new SessionModelConverter().Convert(sessionModels);
+<<<<<<< HEAD
 			List<object> columns = GetFirstValueRowFromConvertedSessionModel(rows);
+=======
+            List<object> columns = GetFirstValueRowFromConvertedSessionModel(rows);
+>>>>>>> Fixed whitespaces in last commit
 
             Assert.Equal(columns[0], "Key1");
             Assert.Equal(columns[1], testObject.TestProperty);
@@ -94,17 +106,28 @@ namespace Glimpse.Test.AspNet.SerializationConverter
                 };
 
             object rows = new SessionModelConverter().Convert(sessionModels);
+<<<<<<< HEAD
 			List<object> columns = GetFirstValueRowFromConvertedSessionModel(rows);
+=======
+            List<object> columns = GetFirstValueRowFromConvertedSessionModel(rows);
+>>>>>>> Fixed whitespaces in last commit
 
             Assert.Equal(columns[0], "Key1");
             Assert.Equal(columns[1], "\\Non serializable type :(\\");
             Assert.Equal(columns[2], typeof(NonSerializableTestObject));
         }
 
+<<<<<<< HEAD
 	    private static List<object> GetFirstValueRowFromConvertedSessionModel(object rows)
 	    {
 		    return ((IEnumerable<object>)((IEnumerable<object>)rows).ToList().Skip(1).First()).ToList(); // skip first row since it only contains column names
 	    }
+=======
+        private static List<object> GetFirstValueRowFromConvertedSessionModel(object rows)
+        {
+            return ((IEnumerable<object>)((IEnumerable<object>)rows).ToList().Skip(1).First()).ToList(); // skip first row since it only contains column names
+        }
+>>>>>>> Fixed whitespaces in last commit
     }
 
     [Serializable]
