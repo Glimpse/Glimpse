ce18306da5b96a794d360f10b48342c6dbacbc20
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-05-03 01:59:41 +0100

    Remove unneeded code

--- source/Glimpse.Core/Resource/PopupResource.cs
+++ source/Glimpse.Core/Resource/PopupResource.cs
@@ -100,18 +100,11 @@ namespace Glimpse.Core.Resource
             }
 #endif
 
-            var version = context.Parameters.GetValueOrDefault(ResourceParameter.VersionNumber.Name);
-            if (string.IsNullOrEmpty(version))
-            {
-                return new StatusCodeResourceResult(404, "Could not get version.");
-            }
-
             var requestStore = configuration.FrameworkProvider.HttpRequestStore;
             var generateScriptTags = requestStore.Get<Func<Guid?, string>>(Constants.ClientScriptsStrategy);
 
-            string scriptTags = generateScriptTags(requestId);
-
-            string html = string.Format("<!DOCTYPE html><html><head><meta charset='utf-8'><title>Glimpse Popup</title></head><body class='glimpse-popup'>{0}</body></html>", scriptTags);
+            var scriptTags = generateScriptTags(requestId); 
+            var html = string.Format("<!DOCTYPE html><html><head><meta charset='utf-8'><title>Glimpse Popup</title></head><body class='glimpse-popup'>{0}</body></html>", scriptTags);
 
             return new HtmlResourceResult(html);
         }
