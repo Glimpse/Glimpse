Glimpse/Glimpse

commit 68e6ee97bcd30c79d38c5e1d8ac2435ea2eaf3ff

Update #283 - Add async support so we don't crash

source/Glimpse.Core/TraceListener.cs
@@ -30,18 +30,7 @@ namespace Glimpse.Core
         public TraceListener(IMessageBroker messageBroker, Func<IExecutionTimer> timerStrategy)
         {
             MessageBroker = messageBroker;
-            TimerStrategy = () =>
-                {
-                    try
-                    {
-                        return timerStrategy();
-                    }
-                    catch
-                    {
-                        // Avoid exception being thrown from threads without access to request store
-                        return null;
-                    }
-                };
+            TimerStrategy = timerStrategy;
         }
 
         internal IMessageBroker MessageBroker 

source/Glimpse.Ado/AlternateType/GlimpseDbCommand.cs
@@ -52,7 +52,7 @@ namespace Glimpse.Ado.AlternateType
 
         private IExecutionTimer TimerStrategy
         {
-            get { return timerStrategy ?? (timerStrategy = GlimpseConfiguration.GetExecutionTimer()); }
+            get { return timerStrategy ?? (timerStrategy = GlimpseConfiguration.GetConfiguredTimerStrategy()()); }
             set { timerStrategy = value; }
         }
 
@@ -175,7 +175,7 @@ namespace Glimpse.Ado.AlternateType
             DbDataReader reader;
             var commandId = Guid.NewGuid();
 
-            var timer = TimerTrigger();
+            var timer = LogCommandSeed();
             LogCommandStart(commandId, timer);  
             try
             {
@@ -197,7 +197,7 @@ namespace Glimpse.Ado.AlternateType
             int num;
             var commandId = Guid.NewGuid();
 
-            var timer = TimerTrigger();
+            var timer = LogCommandSeed();
             LogCommandStart(commandId, timer); 
             try
             {
@@ -218,7 +218,7 @@ namespace Glimpse.Ado.AlternateType
             object result;
             var commandId = Guid.NewGuid();
 
-            var timer = TimerTrigger();
+            var timer = LogCommandSeed();
             LogCommandStart(commandId, timer);  
             try
             {
@@ -286,6 +286,11 @@ namespace Glimpse.Ado.AlternateType
             return parameter.Value;
         }
 
+        private TimeSpan LogCommandSeed()
+        {
+            return TimerStrategy != null ? TimerStrategy.Start() : TimeSpan.Zero;
+        }
+
         private void LogCommandStart(Guid commandId, TimeSpan timerTimeSpan)
         {
             if (MessageBroker != null)
@@ -314,34 +319,24 @@ namespace Glimpse.Ado.AlternateType
 
         private void LogCommandEnd(Guid commandId, TimeSpan timer, int? recordsAffected, string type)
         {
-            if (MessageBroker != null)
+            if (MessageBroker != null && TimerStrategy != null)
             {
                 MessageBroker.Publish(
                     new CommandDurationAndRowCountMessage(InnerConnection.ConnectionId, commandId, recordsAffected)
-                    .AsTimedMessage(TimerStop(timer))
+                    .AsTimedMessage(TimerStrategy.Stop(timer))
                     .AsTimelineMessage("Command Executed", AdoTimelineCategory.Command, type));
             } 
         }
 
         private void LogCommandError(Guid commandId, TimeSpan timer, Exception exception, string type)
         {
-            if (MessageBroker != null)
+            if (MessageBroker != null && TimerStrategy != null)
             {
                 MessageBroker.Publish(
                     new CommandErrorMessage(InnerConnection.ConnectionId, commandId, exception)
-                    .AsTimedMessage(TimerStop(timer))
+                    .AsTimedMessage(TimerStrategy.Stop(timer))
                     .AsTimelineMessage("Command Error", AdoTimelineCategory.Command, type));
             }
-        }
-
-        private TimeSpan TimerTrigger()
-        {
-            return TimerStrategy != null ? TimerStrategy.Start() : TimeSpan.Zero;
-        }
-
-        private TimerResult TimerStop(TimeSpan timer)
-        {
-            return TimerStrategy != null ? TimerStrategy.Stop(timer) : null;
         }
         #endregion
     }

source/Glimpse.Ado/AlternateType/GlimpseDbConnection.cs
@@ -33,7 +33,7 @@ namespace Glimpse.Ado.AlternateType
             InnerProviderFactory = providerFactory;
             ConnectionId = connectionId;
 
-            if (MessageBroker != null)
+            if (MessageBroker != null && TimerStrategy != null)
             {
                 if (connection.State == ConnectionState.Open)
                 {
@@ -65,7 +65,7 @@ namespace Glimpse.Ado.AlternateType
 
         private IExecutionTimer TimerStrategy
         {
-            get { return timerStrategy ?? (timerStrategy = GlimpseConfiguration.GetExecutionTimer()); }
+            get { return timerStrategy ?? (timerStrategy = GlimpseConfiguration.GetConfiguredTimerStrategy()()); }
             set { timerStrategy = value; }
         }
 
@@ -138,6 +138,7 @@ namespace Glimpse.Ado.AlternateType
             if (transaction != null)
             {
                 transaction.TransactionCompleted += OnDtcTransactionCompleted;
+
                 if (MessageBroker != null)
                 {
                     MessageBroker.Publish(new DtcTransactionEnlistedMessage(ConnectionId, transaction.IsolationLevel));
@@ -182,6 +183,7 @@ namespace Glimpse.Ado.AlternateType
                 InnerConnection.Dispose();
                 InnerConnection.StateChange -= StateChangeHaneler;
             }
+
             InnerConnection = null;
             InnerProviderFactory = null;
             base.Dispose(disposing);
@@ -224,7 +226,7 @@ namespace Glimpse.Ado.AlternateType
                 ConnectionId = Guid.NewGuid();
             }
 
-            timerTimeSpan = TimerTrigger();
+            timerTimeSpan = TimerStrategy.Start();
 
             MessageBroker.Publish(
                 new ConnectionStartedMessage(ConnectionId)
@@ -237,18 +239,8 @@ namespace Glimpse.Ado.AlternateType
             
             MessageBroker.Publish(
                 new ConnectionClosedMessage(ConnectionId)
-                .AsTimedMessage(TimerStop(timerTimeSpan))
+                .AsTimedMessage(TimerStrategy.Stop(timerTimeSpan))
                 .AsTimelineMessage("Connection Opened", AdoTimelineCategory.Connection));
-        }
-
-        private TimeSpan TimerTrigger()
-        {
-            return TimerStrategy != null ? TimerStrategy.Start() : TimeSpan.Zero;
-        }
-
-        private TimerResult TimerStop(TimeSpan timer)
-        {
-            return TimerStrategy != null ? TimerStrategy.Stop(timer) : null;
-        }
+        } 
     }
 }

source/Glimpse.Ado/AlternateType/GlimpseDbTransaction.cs
@@ -20,9 +20,9 @@ namespace Glimpse.Ado.AlternateType
             InnerConnection = connection;
             TransactionId = Guid.NewGuid();
 
-            if (MessageBroker != null)
+            if (MessageBroker != null && TimerStrategy != null)
             {
-                timerTimeSpan = TimerTrigger();
+                timerTimeSpan = TimerStrategy.Start();
                 
                 MessageBroker.Publish(
                     new TransactionBeganMessage(connection.ConnectionId, TransactionId, transaction.IsolationLevel)
@@ -51,7 +51,7 @@ namespace Glimpse.Ado.AlternateType
 
         private IExecutionTimer TimerStrategy
         {
-            get { return timerStrategy ?? (timerStrategy = GlimpseConfiguration.GetExecutionTimer()); }
+            get { return timerStrategy ?? (timerStrategy = GlimpseConfiguration.GetConfiguredTimerStrategy()()); }
             set { timerStrategy = value; }
         }
 
@@ -68,11 +68,24 @@ namespace Glimpse.Ado.AlternateType
         public override void Commit()
         {
             InnerTransaction.Commit();
-            if (MessageBroker != null)
+
+            if (MessageBroker != null && TimerStrategy != null)
             { 
                 MessageBroker.Publish(
                     new TransactionCommitMessage(InnerConnection.ConnectionId, TransactionId)
-                    .AsTimedMessage(TimerStop(timerTimeSpan)));
+                    .AsTimedMessage(TimerStrategy.Stop(timerTimeSpan)));
+            }
+        }
+
+        public override void Rollback()
+        {
+            InnerTransaction.Rollback();
+
+            if (MessageBroker != null && TimerStrategy != null)
+            {
+                MessageBroker.Publish(
+                    new TransactionRollbackMessage(InnerConnection.ConnectionId, TransactionId)
+                    .AsTimedMessage(TimerStrategy.Stop(timerTimeSpan)));
             }
         }
 
@@ -80,31 +93,9 @@ namespace Glimpse.Ado.AlternateType
         {
             if (disposing)
             {
-                InnerTransaction.Dispose(); 
+                InnerTransaction.Dispose();
             }
             base.Dispose(disposing);
-        }
-
-        public override void Rollback()
-        {
-            InnerTransaction.Rollback();
-
-            if (MessageBroker != null)
-            {
-                MessageBroker.Publish(
-                    new TransactionRollbackMessage(InnerConnection.ConnectionId, TransactionId)
-                    .AsTimedMessage(TimerStop(timerTimeSpan)));
-            }
-        }
-
-        private TimeSpan TimerTrigger()
-        {
-            return TimerStrategy != null ? TimerStrategy.Start() : TimeSpan.Zero;
-        }
-
-        private TimerResult TimerStop(TimeSpan timer)
-        {
-            return TimerStrategy != null ? TimerStrategy.Stop(timer) : null;
         }
     }
 }

source/Glimpse.Core/Framework/GlimpseConfiguration.cs
@@ -611,15 +611,19 @@ namespace Glimpse.Core.Framework
         [Obsolete("HACK: To support TraceListener with TraceSource via web.config")]
         public static Func<IExecutionTimer> GetConfiguredTimerStrategy()
         {
-            return timerStrategy;
-        }
-
-        [Obsolete("HACK: To support TraceListener with TraceSource via web.config")]
-        public static IExecutionTimer GetExecutionTimer()
-        {
-            var result = GetConfiguredTimerStrategy();
-            return result != null ? result() : null;
-        }
+            return () =>
+            {
+                try
+                {
+                    return timerStrategy();
+                }
+                catch
+                {
+                    // Avoid exception being thrown from threads without access to request store
+                    return null;
+                }
+            }; 
+        } 
 
         [Obsolete("HACK: To support TraceListener with TraceSource via web.config")]
         public static IMessageBroker GetConfiguredMessageBroker()

commit 9647dfdb1005b32646220b5cb4994dc526ec272d

Fix #248 - Corrected faulty constructor that accidentally regressed with commit 68e6ee97bcd30c79d38c5e1d8ac2435ea2eaf3ff

source/Glimpse.Core/TraceListener.cs
@@ -23,8 +23,9 @@ namespace Glimpse.Core
         /// These constructors used by .NET when TraceListener is set via web.config
         /// </summary>
         public TraceListener() 
-            : this(GlimpseConfiguration.GetConfiguredMessageBroker(), GlimpseConfiguration.GetConfiguredTimerStrategy())
         {
+            MessageBroker = GlimpseConfiguration.GetConfiguredMessageBroker();
+            TimerStrategy = GlimpseConfiguration.GetConfiguredTimerStrategy();
         } 
 
         /// <summary>
@@ -32,8 +33,9 @@ namespace Glimpse.Core
         /// </summary>
         /// <param name="initializeData">Initialize data string</param>
         public TraceListener(string initializeData) 
-            : this(GlimpseConfiguration.GetConfiguredMessageBroker(), GlimpseConfiguration.GetConfiguredTimerStrategy())
         {
+            MessageBroker = GlimpseConfiguration.GetConfiguredMessageBroker();
+            TimerStrategy = GlimpseConfiguration.GetConfiguredTimerStrategy();
         }
         //// ReSharper restore UnusedMember.Global
 
@@ -45,7 +47,18 @@ namespace Glimpse.Core
         public TraceListener(IMessageBroker messageBroker, Func<IExecutionTimer> timerStrategy)
         {
             MessageBroker = messageBroker;
-            TimerStrategy = timerStrategy;
+            TimerStrategy = () =>
+            {
+                try
+                {
+                    return timerStrategy();
+                }
+                catch
+                {
+                    // Avoid exception being thrown from threads without access to request store
+                    return null;
+                }
+            };
         }
 
         internal IMessageBroker MessageBroker 