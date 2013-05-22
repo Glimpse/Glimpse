e8f42c8aff64c08507a47b441122340c850ab0a6
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-05-23 08:01:06 +1000

    Update prior to 1.3.2 release

--- source/Glimpse.Core.Net35/Properties/AssemblyInfo.cs
+++ source/Glimpse.Core.Net35/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 // Version is in major.minor.build format to support http://semver.org/
 // Keep these three attributes in sync
-[assembly: AssemblyVersion("1.3.1")]
-[assembly: AssemblyFileVersion("1.3.1")]
-[assembly: AssemblyInformationalVersion("1.3.1")]
+[assembly: AssemblyVersion("1.3.2")]
+[assembly: AssemblyFileVersion("1.3.2")]
+[assembly: AssemblyInformationalVersion("1.3.2")]
