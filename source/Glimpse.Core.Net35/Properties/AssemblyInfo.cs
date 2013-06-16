ddd891fd3d05babbe079852057096e91694ea908
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-06-16 02:04:44 +0100

    Prep for next release which should only see updates to core

--- source/Glimpse.Core.Net35/Properties/AssemblyInfo.cs
+++ source/Glimpse.Core.Net35/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 // Version is in major.minor.build format to support http://semver.org/
 // Keep these three attributes in sync
-[assembly: AssemblyVersion("1.4.1")]
-[assembly: AssemblyFileVersion("1.4.1")]
-[assembly: AssemblyInformationalVersion("1.4.1")]
+[assembly: AssemblyVersion("1.4.2")]
+[assembly: AssemblyFileVersion("1.4.2")]
+[assembly: AssemblyInformationalVersion("1.4.2")]
