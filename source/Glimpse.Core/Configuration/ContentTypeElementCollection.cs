993ced2d3fd669f5abddd61f34bb2f9b9b6c9050
Graham Mendick <graham.mendick@gmail.com>
2013-06-29 11:03:04 +0100

    Tweaked comments

--- source/Glimpse.Core/Configuration/ContentTypeElementCollection.cs
+++ source/Glimpse.Core/Configuration/ContentTypeElementCollection.cs
@@ -6,13 +6,13 @@ namespace Glimpse.Core.Configuration
     /// The Glimpse configuration node for collecting a list of content types.
     /// </summary>
     /// <remarks>
-    /// By default, <c>ContentTypeElementCollection</c>s contain three elements: <c>text/html</c> and <c>application/json</c> and <c>text/plain</c>.
+    /// By default, <c>ContentTypeElementCollection</c>s contain three elements: <c>text/html</c>, <c>application/json</c> and <c>text/plain</c>.
     /// </remarks>
     [ConfigurationCollection(typeof(ContentTypeElement), CollectionType = ConfigurationElementCollectionType.AddRemoveClearMap)]
     public sealed class ContentTypeElementCollection : ConfigurationElementCollection
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ContentTypeElementCollection" /> class with <c>text/html</c> and <c>application/json</c> and <c>text/plain</c> added to the collection.
+        /// Initializes a new instance of the <see cref="ContentTypeElementCollection" /> class with <c>text/html</c>, <c>application/json</c> and <c>text/plain</c> added to the collection.
         /// </summary>
         public ContentTypeElementCollection()
         {
