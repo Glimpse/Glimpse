94e9f99eda38649f16ed10faea672627794e1be8
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-07-17 22:51:32 +1000

    Added styling for the TabCart

--- source/Glimpse.Mvc3.MusicStore.Sample/Framework/TabCart.cs
+++ source/Glimpse.Mvc3.MusicStore.Sample/Framework/TabCart.cs
@@ -1,12 +1,26 @@
 ﻿using Glimpse.AspNet.Extensibility; 
 using Glimpse.AspNet.Extensions;
 using Glimpse.Core.Extensibility;
+using Glimpse.Core.Tab.Assist;
 using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Framework
-{ 
-    public class TabCart : AspNetTab
+{
+    public class TabCart : AspNetTab, ITabLayout
     {
+        private static readonly object Layout = TabLayout.Create()
+                .Row(r =>
+                {
+                    r.Cell("{{albumTitle}} ({{albumId}})").AsKey().WithTitle("Album (Id)");
+                    r.Cell("albumPrice").AlignRight().Prefix("$").WidthInPixels(100).WithTitle("Price");
+                    r.Cell("genreName").WithTitle("Genre");
+                    r.Cell("artistName").WithTitle("Artist");
+                    r.Cell("count").Class("mono").WidthInPixels(70).WithTitle("Count");
+                    r.Cell("dateCreated").WithTitle("Added");
+                    r.Cell("recordId").WithTitle("Record Id");
+                    r.Cell("cartId").WithTitle("Cart Id"); 
+                }).Build();
+         
         public override string Name
         {
             get { return "Cart"; }
@@ -23,6 +37,11 @@ namespace MvcMusicStore.Framework
         public override RuntimeEvent ExecuteOn
         {
             get { return RuntimeEvent.EndSessionAccess; }
+        }
+
+        public object GetLayout()
+        {
+            return Layout;
         }
     }
 }
