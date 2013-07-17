b9107f2163ab85377f3955f545f6805594bf4913
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-07-17 14:22:09 +1000

    Don't want this sample to tab to always run

--- source/Glimpse.Mvc3.MusicStore.Sample/Framework/TabConfiguration.cs
+++ source/Glimpse.Mvc3.MusicStore.Sample/Framework/TabConfiguration.cs
@@ -1,21 +1,21 @@
-﻿using Glimpse.AspNet.Extensibility;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿//using Glimpse.AspNet.Extensibility;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
 
-namespace MvcMusicStore.Framework
-{
-    public class TabConfiguration : AspNetTab 
-    {
-        public override string Name
-        {
-            get { return "Setup"; }
-        }
+//namespace MvcMusicStore.Framework
+//{
+//    public class TabConfiguration : AspNetTab 
+//    {
+//        public override string Name
+//        {
+//            get { return "Setup"; }
+//        }
 
-        public override object GetData(Glimpse.Core.Extensibility.ITabContext context)
-        {
-            return MvcMusicStore.Framework.Configuration.Current;
-        }
-    }
-}
+//        public override object GetData(Glimpse.Core.Extensibility.ITabContext context)
+//        {
+//            return MvcMusicStore.Framework.Configuration.Current;
+//        }
+//    }
+//}
