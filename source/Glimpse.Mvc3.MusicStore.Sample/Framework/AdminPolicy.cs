554a33cee6a423f224fe0bbd15b984c3eadf0ee0
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-07-20 15:12:31 +1000

    Add sample admin policy

---
+++ source/Glimpse.Mvc3.MusicStore.Sample/Framework/AdminPolicy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glimpse.AspNet.Extensions;
+using Glimpse.Core.Extensibility;
+
+namespace MvcMusicStore.Framework
+{
+    public class AdminPolicy : IRuntimePolicy
+    {
+        public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
+        {
+            var httpContext = policyContext.GetHttpContext();
+            if (!httpContext.User.IsInRole("Administrator"))
+            {
+                return RuntimePolicy.Off;
+            }
+
+            return RuntimePolicy.On;
+        }
+
+        public RuntimeEvent ExecuteOn
+        {
+            get { return RuntimeEvent.EndRequest; }
+        }
+    }
+}
