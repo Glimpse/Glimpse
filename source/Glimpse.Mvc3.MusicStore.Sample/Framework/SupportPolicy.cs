82b6d8d078bf12ea55612e0420f98320eb21dc4c
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-07-20 23:04:52 +1000

    Demo support policy added

---
+++ source/Glimpse.Mvc3.MusicStore.Sample/Framework/SupportPolicy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glimpse.AspNet.Extensions;
+using Glimpse.Core.Extensibility;
+
+namespace MvcMusicStore.Framework
+{
+    public class SupportPolicy : IRuntimePolicy
+    {
+        public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
+        {
+            var httpContext = policyContext.GetHttpContext();
+            if (!httpContext.User.IsInRole("Administrator"))
+            {
+                var cookie = policyContext.RequestMetadata.GetCookie("SupportCall");
+                if (!string.IsNullOrEmpty(cookie))
+                {
+                    return RuntimePolicy.PersistResults;
+                }
+
+                return RuntimePolicy.Off;
+            }
+
+            return RuntimePolicy.On;
+        }
+
+        public RuntimeEvent ExecuteOn
+        {
+            get { return RuntimeEvent.EndRequest; }
+        }
+    }
+}
