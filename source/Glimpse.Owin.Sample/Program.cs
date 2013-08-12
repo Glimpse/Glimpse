bbdd54e0f0a83a994f9215584a37485bd924d7df
Nik Molnar <nikmd23@gmail.com>
2013-08-12 16:51:14 -0400

    Added a sample OWIN project for testing purposes.

---
+++ source/Glimpse.Owin.Sample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace Glimpse.Owin.Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:8080/"))
+            {
+                Console.WriteLine("Started");
+                Console.ReadLine();
+                Console.WriteLine("Stopping");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
