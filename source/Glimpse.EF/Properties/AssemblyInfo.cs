2b7847a8f3b3b6c1531dfa815ce36511d2882092
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-09-04 14:59:06 -0400

    Preparation prior to 1.4.0-ef release

--- source/Glimpse.EF/Properties/AssemblyInfo.cs
+++ source/Glimpse.EF/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using Glimpse.Core.Extensibility;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("61266d72-5987-460b-9536-eb164c9e0b4b")]
 
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.0")] // Used to specify the NuGet version number at build time
+[assembly: AssemblyVersion("1.4.0")]
+[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyInformationalVersion("1.4.0")] // Used to specify the NuGet version number at build time
 
 [assembly: InternalsVisibleTo("Glimpse.Test.EF")]
 [assembly: NuGetPackage("Glimpse.EF")]
