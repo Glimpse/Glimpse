50f2ed97860210734f869f7ab5f1cdaeeb67830b
Christophe Gijbels <git@gijbels-it.be>
2013-10-09 00:32:12 +0200

    Fixed PreBodyTagFilter partial writes handling

--- source/Glimpse.AspNet/PreBodyTagFilter.cs
+++ source/Glimpse.AspNet/PreBodyTagFilter.cs
@@ -8,96 +8,167 @@ namespace Glimpse.AspNet
     public class PreBodyTagFilter : Stream
     {
         private const string BodyClosingTag = "</body>";
-
         private const string TroubleshootingDocsUri = "http://getglimpse.com/Help/Troubleshooting";
 
-        public PreBodyTagFilter(string htmlSnippet, Stream outputStream, Encoding contentEncoding, ILogger logger)
+        private readonly ILogger logger;
+        private readonly string htmlSnippet;
+        private readonly Stream outputStream;
+        private readonly Encoding contentEncoding;
+        private readonly Regex bodyEndRegex;
+        private readonly string currentRequestRawUrl;
+        private string unwrittenCharactersFromPreviousCall;
+
+        public PreBodyTagFilter(string htmlSnippet, Stream outputStream, Encoding contentEncoding, string currentRequestRawUrl, ILogger logger)
         {
-            HtmlSnippet = htmlSnippet + BodyClosingTag;
-            OutputStream = outputStream;
-            ContentEncoding = contentEncoding;
-            BodyEnd = new Regex(BodyClosingTag, RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.RightToLeft);
-            Logger = logger;
+            this.htmlSnippet = htmlSnippet + BodyClosingTag;
+            this.outputStream = outputStream;
+            this.contentEncoding = contentEncoding;
+            this.bodyEndRegex = new Regex(BodyClosingTag, RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.RightToLeft);
+            this.currentRequestRawUrl = currentRequestRawUrl ?? "unknown";
+            this.logger = logger;
         }
 
         public override bool CanRead
         {
-            get { return OutputStream.CanRead; }
+            get { return this.outputStream.CanRead; }
         }
 
         public override bool CanSeek
         {
-            get { return OutputStream.CanSeek; }
+            get { return this.outputStream.CanSeek; }
         }
 
         public override bool CanWrite
         {
-            get { return OutputStream.CanWrite; }
+            get { return this.outputStream.CanWrite; }
         }
 
         public override long Length
         {
-            get { return OutputStream.Length; }
+            get { return this.outputStream.Length; }
         }
 
         public override long Position
         {
-            get { return OutputStream.Position; }
-            set { OutputStream.Position = value; }
+            get { return this.outputStream.Position; }
+            set { this.outputStream.Position = value; }
         }
-
-        private ILogger Logger { get; set; }
-
-        private string HtmlSnippet { get; set; }
-
-        private Stream OutputStream { get; set; }
-
-        private Encoding ContentEncoding { get; set; }
-
-        private Regex BodyEnd { get; set; }
 
         public override void Close()
         {
-            OutputStream.Close();
-        }
-
-        public override void Flush()
-        {
-            OutputStream.Flush();
+            this.outputStream.Close();
         }
 
         public override long Seek(long offset, SeekOrigin origin)
         {
-            return OutputStream.Seek(offset, origin);
+            return this.outputStream.Seek(offset, origin);
         }
 
         public override void SetLength(long value)
         {
-            OutputStream.SetLength(value);
+            this.outputStream.SetLength(value);
         }
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            return OutputStream.Read(buffer, offset, count);
+            return this.outputStream.Read(buffer, offset, count);
         }
 
         public override void Write(byte[] buffer, int offset, int count)
         {
-            string contentInBuffer = ContentEncoding.GetString(buffer);
+            // There are different cases we need to deal with
+            // Normally you would expect the contentInBuffer to contain the complete HTML code to return, but this is not always true because it is possible that 
+            // the content that will be send back is larger than the buffer foreseen by ASP.NET (currently the buffer seems to be a little bit less than 16K)
+            // and in that case this method will be called multiple times, which might result in false positives being written to the logs for not finding a </body> 
+            // in the current chunk.
 
-            if (BodyEnd.IsMatch(contentInBuffer))
+            // So we need to be able to deal with the following cases without writing those false positives
+            // 1 - the </body> tag is found
+            // 2 - the </body> tag was not found because
+            //      2.1 - the </body> tag will be available in one of the next calls because the total length of the output is larger than 16K
+            //      2.2 - the </body> tag is split up between this buffer and the next e.g.: "</bo" en "dy>"
+            //      2.3 - the </body> tag will never be available (is missing)
+            //      2.4 - Multiple </body> tags are available of which some might be part of a Javascript string or the markup is badly formatted
+
+            // The easiest way to deal with this is to look for the last match for the </body> tag and if it is found we write everything before it to the
+            // output stream and keep that </body> tag and everything that follows it (normally only a </html> tag but it can also be a 2.4 case) for the next call.
+            // In case there is no match for the </body> tag, then we write everything to the output stream except for the last 10 characters (normally the last 6 would suffice, but we take a little margin to reassure us somehow ;-)) which we keep until the next call.
+
+            // If there is a next call, then we first prepend the characters we kept from the previous call to the content inside the buffer (which might complete a chunked </body> tag for instance) 
+            // and start our check all over again (which might result in finding a </body> tag or discarding a previously found </body> tag because that one was not the last one.
+            // Anyhow, as long as we are not a the end and a </body> tag has been found previously, the output will be buffered, just to make sure there is no other </body> tag further down the stream.
+
+            // If there is no next call, then the Flush method will be called and that one will deal with the current state, which means:
+            // - in case there was a </body> tag found, the replacement will be done
+            // - in case there was no </body> tag found, then the warning will be written to the log, indicating something went wrong
+            // either way, the remaining unwritten characters will be sent down the output stream.
+
+            string contentInBuffer = this.contentEncoding.GetString(buffer, offset, count);
+
+            // Prepend remaining characters from the previous call, if any
+            if (!string.IsNullOrEmpty(this.unwrittenCharactersFromPreviousCall))
             {
-                string bodyCloseWithScript = BodyEnd.Replace(contentInBuffer, HtmlSnippet, 1);
+                contentInBuffer = this.unwrittenCharactersFromPreviousCall + contentInBuffer;
+                this.unwrittenCharactersFromPreviousCall = null;
+            }
 
-                byte[] outputBuffer = ContentEncoding.GetBytes(bodyCloseWithScript);
+            Match closingBodyTagMatch = this.bodyEndRegex.Match(contentInBuffer);
+            if (closingBodyTagMatch.Success)
+            {
+                // Hooray, we found "a" </body> tag, but that doesn't mean that this is "the" last </body> tag we are looking for
 
-                OutputStream.Write(outputBuffer, 0, outputBuffer.Length);
+                // so we write everything before that match to the output stream
+                this.WriteToOutputStream(contentInBuffer.Substring(0, closingBodyTagMatch.Index));
+
+                // and keep the remainder for the next call or the Flush if there is no next call
+                this.unwrittenCharactersFromPreviousCall = contentInBuffer.Substring(closingBodyTagMatch.Index);
             }
             else
             {
-                Logger.Warn("Unable to locate '</body>' with content encoding '{0}'. The response may be compressed. See {1} for information on troubleshooting this issue.", ContentEncoding.EncodingName, TroubleshootingDocsUri);
-                OutputStream.Write(buffer, offset, count);
+                // there is no match found for </body> which could have different reasons like case 2.2 for instance
+                // therefor we'll write everything except the last 10 characters to the output stream and we'll keep the last 10 characters for the next call or the Flush method
+                if (contentInBuffer.Length <= 10)
+                {
+                    // the content has a maximum length of 10 characters, so we don't need to write anything to the output stream and we'll keep those 
+                    // characters for the next call (most likely the Flush)
+                    this.unwrittenCharactersFromPreviousCall = contentInBuffer;
+                }
+                else
+                {
+                    this.WriteToOutputStream(contentInBuffer.Substring(0, contentInBuffer.Length - 10));
+                    this.unwrittenCharactersFromPreviousCall = contentInBuffer.Substring(contentInBuffer.Length - 10);
+                }
             }
+        }
+
+        public override void Flush()
+        {
+            if (!string.IsNullOrEmpty(this.unwrittenCharactersFromPreviousCall))
+            {
+                string finalContentToWrite = this.unwrittenCharactersFromPreviousCall;
+
+                if (this.bodyEndRegex.IsMatch(this.unwrittenCharactersFromPreviousCall))
+                {
+                    // apparently we did seem to match a </body> tag, which means we can replace the last match with our HTML snippet
+                    finalContentToWrite = this.bodyEndRegex.Replace(this.unwrittenCharactersFromPreviousCall, this.htmlSnippet, 1);
+                }
+                else
+                {
+                    // there was no </body> tag found, so we write down a warning to the log
+                    this.logger.Warn("Unable to locate '</body>' with content encoding '{0}' for request '{1}'. The response may be compressed or the markup may actually be missing a '</body>' tag. See {2} for information on troubleshooting this issue.", this.contentEncoding.EncodingName, this.currentRequestRawUrl, TroubleshootingDocsUri);
+                }
+
+                // either way, if a replacement has been done or a warning has been written to the logs, the remaining unwritten characters must be written to the output stream
+                this.WriteToOutputStream(finalContentToWrite);
+            }
+
+            this.outputStream.Flush();
+        }
+
+        private void WriteToOutputStream(string content)
+        {
+            byte[] outputBuffer = this.contentEncoding.GetBytes(content);
+            this.outputStream.Write(outputBuffer, 0, outputBuffer.Length);
         }
     }
 }
--- source/Glimpse.Test.AspNet/PreBodyTagFilterShould.cs
+++ source/Glimpse.Test.AspNet/PreBodyTagFilterShould.cs
@@ -19,34 +19,94 @@ namespace Glimpse.Test.AspNet
         [Fact]
         public void HaveReplacedTheClosingBodyTag()
         {
+            this.DoHaveReplacedTheClosingBodyTag();
+        }
+
+        [Fact]
+        public void HaveReplacedTheClosingBodyTagEvenWhenInputIsChunked()
+        {
+            this.DoHaveReplacedTheClosingBodyTag(10);
+        }
+
+        private void DoHaveReplacedTheClosingBodyTag(int? chunkLastNumberOfCharacters = null)
+        {
             const string htmlSnippet = "MY HTML SNIPPET";
             const string inputToProcess = "<html><body><span>some content</span></body></html>";
             const string expectedResult = "<html><body><span>some content</span>" + htmlSnippet + "</body></html>";
-            string result = this.ProcessInputByPreBodyTagFilter(inputToProcess, htmlSnippet);
+            string result = this.ProcessInputByPreBodyTagFilter(inputToProcess, htmlSnippet, "REQUEST URL", chunkLastNumberOfCharacters);
             Assert.Equal(expectedResult, result);
         }
 
         [Fact]
         public void HaveReplacedTheClosingBodyTagEvenWhenBodyTagIsBadlyCased()
         {
+            this.DoHaveReplacedTheClosingBodyTagEvenWhenBodyTagIsBadlyCased();
+        }
+
+        [Fact]
+        public void HaveReplacedTheClosingBodyTagEvenWhenBodyTagIsBadlyCasedEvenWhenInputIsChunked()
+        {
+            this.DoHaveReplacedTheClosingBodyTagEvenWhenBodyTagIsBadlyCased(10);
+        }
+
+        public void DoHaveReplacedTheClosingBodyTagEvenWhenBodyTagIsBadlyCased(int? chunkLastNumberOfCharacters = null)
+        {
             const string htmlSnippet = "MY HTML SNIPPET";
             const string inputToProcess = "<html><body><span>some content</span></BoDy></html>";
             const string expectedResult = "<html><body><span>some content</span>" + htmlSnippet + "</body></html>";
-            string result = this.ProcessInputByPreBodyTagFilter(inputToProcess, htmlSnippet);
+            string result = this.ProcessInputByPreBodyTagFilter(inputToProcess, htmlSnippet, "REQUEST URL", chunkLastNumberOfCharacters);
             Assert.Equal(expectedResult, result);
         }
 
         [Fact]
         public void HaveWrittenWarningWhenThereIsNoClosingBodyTag()
         {
+            this.DoHaveWrittenWarningWhenThereIsNoClosingBodyTag();
+        }
+
+        [Fact]
+        public void HaveWrittenWarningWhenThereIsNoClosingBodyTagEvenWhenInputIsChunked()
+        {
+            this.DoHaveWrittenWarningWhenThereIsNoClosingBodyTag(10);
+        }
+
+        private void DoHaveWrittenWarningWhenThereIsNoClosingBodyTag(int? chunkLastNumberOfCharacters = null)
+        {
             this.loggerMock.Setup(m => m.Warn(null, (object[])null)).Verifiable();
             const string inputToProcess = "<html><body>some content</html>";
-            string result = this.ProcessInputByPreBodyTagFilter(inputToProcess, "HTML SNIPPET");
+            string result = this.ProcessInputByPreBodyTagFilter(inputToProcess, "HTML SNIPPET", "REQUEST URL", chunkLastNumberOfCharacters);
 
             this.loggerMock.Verify(
                 logger => logger.Warn(
-                            "Unable to locate '</body>' with content encoding '{0}'. The response may be compressed. See {1} for information on troubleshooting this issue.",
-                            It.Is<object[]>(arguments => arguments.Length == 2 && object.Equals(arguments[0], Encoding.UTF8.EncodingName) && object.Equals(arguments[1], "http://getglimpse.com/Help/Troubleshooting"))),
+                            "Unable to locate '</body>' with content encoding '{0}' for request '{1}'. The response may be compressed or the markup may actually be missing a '</body>' tag. See {2} for information on troubleshooting this issue.",
+                            It.Is<object[]>(arguments => arguments.Length == 3 && object.Equals(arguments[0], Encoding.UTF8.EncodingName) && object.Equals(arguments[1], "REQUEST URL") && object.Equals(arguments[2], "http://getglimpse.com/Help/Troubleshooting"))),
+                Times.Once());
+
+            Assert.Equal(inputToProcess, result);
+        }
+
+        [Fact]
+        public void HaveWrittenWarningWhenThereIsNoClosingBodyTagAndSpecifiedRequestUrlIsNull()
+        {
+            this.DoHaveWrittenWarningWhenThereIsNoClosingBodyTagAndSpecifiedRequestUrlIsNull();
+        }
+
+        [Fact]
+        public void HaveWrittenWarningWhenThereIsNoClosingBodyTagAndSpecifiedRequestUrlIsNullEvenWhenInputIsChunked()
+        {
+            this.DoHaveWrittenWarningWhenThereIsNoClosingBodyTagAndSpecifiedRequestUrlIsNull(10);
+        }
+
+        private void DoHaveWrittenWarningWhenThereIsNoClosingBodyTagAndSpecifiedRequestUrlIsNull(int? chunkLastNumberOfCharacters = null)
+        {
+            this.loggerMock.Setup(m => m.Warn(null, (object[])null)).Verifiable();
+            const string inputToProcess = "<html><body>some content</html>";
+            string result = this.ProcessInputByPreBodyTagFilter(inputToProcess, "HTML SNIPPET", null, chunkLastNumberOfCharacters);
+
+            this.loggerMock.Verify(
+                logger => logger.Warn(
+                            "Unable to locate '</body>' with content encoding '{0}' for request '{1}'. The response may be compressed or the markup may actually be missing a '</body>' tag. See {2} for information on troubleshooting this issue.",
+                            It.Is<object[]>(arguments => arguments.Length == 3 && object.Equals(arguments[0], Encoding.UTF8.EncodingName) && object.Equals(arguments[1], "unknown") && object.Equals(arguments[2], "http://getglimpse.com/Help/Troubleshooting"))),
                 Times.Once());
 
             Assert.Equal(inputToProcess, result);
@@ -55,32 +115,64 @@ namespace Glimpse.Test.AspNet
         [Fact]
         public void HaveOnlyReplacedTheLastClosingBodyTag()
         {
+            this.DoHaveOnlyReplacedTheLastClosingBodyTag();
+        }
+
+        [Fact]
+        public void HaveOnlyReplacedTheLastClosingBodyTagEvenWhenInputIsChunked()
+        {
+            this.DoHaveOnlyReplacedTheLastClosingBodyTag(20);
+        }
+
+        private void DoHaveOnlyReplacedTheLastClosingBodyTag(int? chunkLastNumberOfCharacters = null)
+        {
             const string htmlSnippet = "MY HTML SNIPPET";
             const string inputToProcess = "<html><body><span>some content</span></body><p>some more content</p></body></html>";
             const string expectedResult = "<html><body><span>some content</span></body><p>some more content</p>" + htmlSnippet + "</body></html>";
-            string result = this.ProcessInputByPreBodyTagFilter(inputToProcess, htmlSnippet);
+            string result = this.ProcessInputByPreBodyTagFilter(inputToProcess, htmlSnippet, "REQUEST URL", chunkLastNumberOfCharacters);
             Assert.Equal(expectedResult, result);
         }
 
         [Fact]
         public void HaveReplacedTheLastClosingBodyTagWithOnlyAnotherClosingBodyTagWhenTheHtmlSnippetIsNullOrEmpty()
         {
+            this.DoHaveReplacedTheLastClosingBodyTagWithOnlyAnotherClosingBodyTagWhenTheHtmlSnippetIsNullOrEmpty();
+        }
+
+        [Fact]
+        public void HaveReplacedTheLastClosingBodyTagWithOnlyAnotherClosingBodyTagWhenTheHtmlSnippetIsNullOrEmptyEvenWhenInputIsChunked()
+        {
+            this.DoHaveReplacedTheLastClosingBodyTagWithOnlyAnotherClosingBodyTagWhenTheHtmlSnippetIsNullOrEmpty(10);
+        }
+
+        private void DoHaveReplacedTheLastClosingBodyTagWithOnlyAnotherClosingBodyTagWhenTheHtmlSnippetIsNullOrEmpty(int? chunkLastNumberOfCharacters = null)
+        {
             const string inputToProcess = "<html><body><span>some content</span></body><p>some more content</p></body></html>";
-            string result = this.ProcessInputByPreBodyTagFilter(inputToProcess, null);
+            string result = this.ProcessInputByPreBodyTagFilter(inputToProcess, null, "REQUEST URL", chunkLastNumberOfCharacters);
             Assert.Equal(inputToProcess, result);
 
-            result = this.ProcessInputByPreBodyTagFilter(inputToProcess, string.Empty);
+            result = this.ProcessInputByPreBodyTagFilter(inputToProcess, string.Empty, "REQUEST URL", chunkLastNumberOfCharacters);
             Assert.Equal(inputToProcess, result);
         }
 
-        private string ProcessInputByPreBodyTagFilter(string inputToProcess, string htmlSnippet)
+        private string ProcessInputByPreBodyTagFilter(string inputToProcess, string htmlSnippet, string requestUrl, int? chunkLastNumberOfCharacters)
         {
             using (var memoryStream = new MemoryStream())
             {
-                var preBodyTagFilter = new PreBodyTagFilter(htmlSnippet, memoryStream, Encoding.UTF8, this.loggerMock.Object);
+                var preBodyTagFilter = new PreBodyTagFilter(htmlSnippet, memoryStream, Encoding.UTF8, requestUrl, this.loggerMock.Object);
 
-                byte[] buffer = Encoding.UTF8.GetBytes(inputToProcess);
-                preBodyTagFilter.Write(buffer, 0, buffer.Length);
+                string[] inputsToProcess = { inputToProcess };
+                if (chunkLastNumberOfCharacters.HasValue)
+                {
+                    inputsToProcess = new[] { inputToProcess.Substring(0, inputToProcess.Length - chunkLastNumberOfCharacters.Value), inputToProcess.Substring(inputToProcess.Length - chunkLastNumberOfCharacters.Value) };
+                }
+
+                foreach (string inputToProcessChunk in inputsToProcess)
+                {
+                    byte[] buffer = Encoding.UTF8.GetBytes(inputToProcessChunk);
+                    preBodyTagFilter.Write(buffer, 0, buffer.Length);
+                }
+
                 preBodyTagFilter.Flush();
                 preBodyTagFilter.Position = 0;
 
