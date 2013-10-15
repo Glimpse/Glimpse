6667405ed21dc5281ddb9ef3268f44e375c866a3
Kelly Stuard <kellystuard@gmail.com>
2013-10-14 19:57:28 -0500

    Making Glimpse Samples CLS-compliant.

--- source/Glimpse.WebForms.WingTip.Sample/Properties/AssemblyInfo.cs
+++ source/Glimpse.WebForms.WingTip.Sample/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -33,3 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
