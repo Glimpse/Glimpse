6124a8833a0a8fb42c71417e243b5d22fab8ee80
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-10-23 13:03:06 -0400

    Update #575 - Fix the typo

--- source/Glimpse.Core/glimpse.js
+++ source/Glimpse.Core/glimpse.js
@@ -1143,7 +1143,7 @@ glimpse.render.engine.util.table = (function($) {
             if (metadataItem.width) { cellStyle += 'width:' + metadataItem.width + ';'; };
             if (metadataItem.align) { cellStyle += 'text-align:' + metadataItem.align + ';'; };
             if (metadataItem.paddingLeft) { cellStyle += 'padding-left:' + metadataItem.paddingLeft + ';'; };
-            if (metadataItem.paddingRight) { cellStyle += 'padding-right:' + metadataItem.paddingright + ';'; };
+            if (metadataItem.paddingRight) { cellStyle += 'padding-right:' + metadataItem.paddingRight + ';'; };
             if (cellStyle) { cellAttr += ' style="' + cellStyle + '"'; };
             //Cell Span
             if (metadataItem.span) { cellAttr += ' colspan="' + metadataItem.span + '"'; };
