ac91e0ebe9e8d0a390647659857c2995a04a17c7
Bryan J Hogan <bryan@bryanhogan.net>
2013-10-30 17:58:47 -0400

    Added file upload to MVC3 and WebForms apps

---
+++ source/Glimpse.WebForms.WingTip.Sample/FileUpload/FileUpload.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WingtipToys.FileUpload
+{
+    public partial class FileUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Upload_Click(object sender, EventArgs e)
+        {
+            if ((File1.PostedFile != null) && (File1.PostedFile.ContentLength > 0))
+            {
+               UploadResult.Text = "The file has been uploaded.";
+            }
+            else
+            {
+                UploadResult.Text = "File was empty.";
+            }
+        }
+    }
+}
