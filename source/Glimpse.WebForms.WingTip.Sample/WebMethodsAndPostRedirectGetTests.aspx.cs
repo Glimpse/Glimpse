39b84b065a33642584c97d3b5c82218b307960b2
Christophe Gijbels <git@gijbels-it.be>
2013-11-05 11:36:01 +0100

    Added some database access in click handlers

--- source/Glimpse.WebForms.WingTip.Sample/WebMethodsAndPostRedirectGetTests.aspx.cs
+++ source/Glimpse.WebForms.WingTip.Sample/WebMethodsAndPostRedirectGetTests.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Services;
 using System.Web.UI;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -15,23 +17,32 @@ namespace WingtipToys
 
         void buttonThrowAnException_Click(object sender, EventArgs e)
         {
+            DoSomeProductQueryingAndReturnNumberOfProdutsFound();
             throw new Exception("This is a deliberately thrown exception");
         }
 
         void buttonServerTransferToDefaultPage_Click(object sender, EventArgs e)
         {
+            DoSomeProductQueryingAndReturnNumberOfProdutsFound();
             Server.Transfer("/About.aspx");
         }
 
         void buttonRedirectToDefaultPage_Click(object sender, EventArgs e)
         {
-            Response.Redirect("/");
+            DoSomeProductQueryingAndReturnNumberOfProdutsFound();
+            Response.Redirect("/default.aspx");
         }
 
         [WebMethod]
         public static string GetTheTime()
         {
+            DoSomeProductQueryingAndReturnNumberOfProdutsFound();
             return DateTime.Now.ToString();
+        }
+
+        private static int DoSomeProductQueryingAndReturnNumberOfProdutsFound()
+        {
+            return new WingtipToys.Models.ProductContext().Products.ToList().Count;
         }
     }
 }
