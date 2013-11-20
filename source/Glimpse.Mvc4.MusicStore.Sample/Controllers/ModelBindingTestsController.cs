bde00e4cd0ebcf6ca01f065c995d5cefc8827fbb
Christophe Gijbels <git@gijbels-it.be>
2013-11-20 22:57:34 +0100

    Added ModelBinding test case for Ajax failure

---
+++ source/Glimpse.Mvc4.MusicStore.Sample/Controllers/ModelBindingTestsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class ModelBindingTestsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult PostItem(ItemContainingDictionary item)
+        {
+            return new JsonResult { Data = new { PostedItem = item} };
+        }
+
+        public class ItemContainingDictionary
+        {
+            public Dictionary<string, string> Stuff { get; set; }
+        }
+    }
+}
