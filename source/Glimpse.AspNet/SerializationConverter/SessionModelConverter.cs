377e0bb8bc99ef4075269175f00bc8e65689fb6d
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-11-20 09:56:44 -0800

    Add missing files

--- source/Glimpse.AspNet/SerializationConverter/SessionModelConverter.cs
+++ source/Glimpse.AspNet/SerializationConverter/SessionModelConverter.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 
 using Glimpse.AspNet.Extensions;
 using Glimpse.AspNet.Model;
+using Glimpse.AspNet.Support;
 using Glimpse.Core.Extensibility;
 using Glimpse.Core.Tab.Assist;
 
@@ -15,29 +16,7 @@ namespace Glimpse.AspNet.SerializationConverter
             var root = new TabSection("Key", "Value", "Type");
             foreach (var item in obj)
             {
-                var row = root.AddRow().Column(item.Key);
-
-                if (item.Type != null)
-                {
-                    if (item.Type.IsSerializable)
-                    {
-                        row.Column(item.Value);
-                    }
-                    else if (item.Type.GetMethod("ToString").DeclaringType == item.Type)
-                    {
-                        row.Column(item.Value.ToString());
-                    }
-                    else
-                    {
-                        row.Column("Non serializable type :(").Emphasis();
-                    }
-                }
-                else
-                {
-                    row.Column(item.Value);
-                }
-
-                row.Column(item.Type);
+                root.AddRow().Column(item.Key).Column(Serialization.GetValueSafe(item.Value)).Column(item.Type);
             }
 
             return root.Build();
---
+++ source/Glimpse.AspNet/Support/Serialization.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glimpse.AspNet.Support
+{
+    public static class Serialization
+    {
+        public static object GetValueSafe(object value)
+        {
+            if (value != null)
+            {
+                var type = value.GetType();
+                if (!type.IsSerializable)
+                {
+                    if (type.GetMethod("ToString").DeclaringType == type)
+                    {
+                        value = value.ToString();
+                    }
+                    else
+                    {
+                        value = @"\Non serializable type :(\";
+                    }
+                }
+            }
+
+            return value;
+        }
+    }
+}
