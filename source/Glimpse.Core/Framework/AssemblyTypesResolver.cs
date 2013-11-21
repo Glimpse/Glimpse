e5fbe68c0deb7175d73c9df9a05855f76332d75a
pltaylor <pltaylor3@gmail.com>
2013-11-21 13:02:06 -0500

    Test run of storing types
    Working on Feedback
    
    Attempt to make it more performant from comments
    
    Limit access to dictionary and add locks
    
    Check if in GAC
    
    This pretty much eliminates this as a performance concern.  It gets rid
    of 50 of the dlls, and the biggest ones.
    
    Revert "Check if in GAC"
    
    This reverts commit fd94dff1ac6e75a8bc218f843442a6fae9aa6296.
    
    Add a little meat to the class
    
    Add black list
    
    I may have gone a little overboard on blacklisting things.
    
    refactor dictionary name
    
    Move Lock to outer scope
    
    Move classes to their own files
    
    Move files to correct location
    
    Move lock to outer scope.
    
    New line
    
    And clean up Glimpse.Core.Net45 to be able to compile

---
+++ source/Glimpse.Core/Framework/AssemblyTypesResolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Glimpse.Core.Framework
+{
+    public static class AssemblyTypesResolver
+    {
+        private static readonly Dictionary<Assembly, Type[]> typesByAssembly = new Dictionary<Assembly, Type[]>();
+
+        public static IEnumerable<Type> RetrieveTypes(Assembly assembly)
+        {
+            // GetTypes potentially throws an exception. Defensive coding as per http://haacked.com/archive/2012/07/23/get-all-types-in-an-assembly.aspx
+            
+            Type[] allTypes; lock (((IDictionary)typesByAssembly).SyncRoot)
+            {
+                try
+                {
+                    if (!typesByAssembly.TryGetValue(assembly, out allTypes))
+                    {
+                        allTypes = assembly.GetTypes();
+
+                        typesByAssembly.Add(assembly, allTypes);
+                    }
+
+                }
+                catch (ReflectionTypeLoadException ex)
+                {
+                    allTypes = ex.Types.Where(t => t != null).ToArray();
+                    typesByAssembly.Add(assembly, allTypes);
+
+                    foreach (var exception in ex.LoaderExceptions)
+                    {
+                        GlimpseConfiguration.GetLogger().Warn(string.Format(Resources.DiscoverGetType, assembly.FullName), exception);
+                    }
+                }
+            }
+            return allTypes;
+        }
+    }
+}
