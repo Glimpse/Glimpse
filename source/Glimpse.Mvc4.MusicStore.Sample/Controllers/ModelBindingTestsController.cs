031765289ce6ef46dd2d42c542ad4d8fdf428fe8
Christophe Gijbels <git@gijbels-it.be>
2013-11-27 10:13:34 +0100

    Added and updated models for Modelbinding tests

--- source/Glimpse.Mvc4.MusicStore.Sample/Controllers/ModelBindingTestsController.cs
+++ source/Glimpse.Mvc4.MusicStore.Sample/Controllers/ModelBindingTestsController.cs
@@ -7,18 +7,76 @@ namespace MvcMusicStore.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("Index", new ComplexType());
         }
 
         [HttpPost]
-        public JsonResult PostItem(ItemContainingDictionary item)
+        public ActionResult SimplePost(string firstname, string lastname)
         {
-            return new JsonResult { Data = new { PostedItem = item} };
+            return View("Index", new ComplexType());
         }
 
-        public class ItemContainingDictionary
+        [HttpPost]
+        public ActionResult ComplexPost(ComplexType complexType, string additionalSimpleValue)
         {
-            public Dictionary<string, string> Stuff { get; set; }
+            return View("Index", new ComplexType());
+        }
+
+        [HttpPost]
+        public JsonResult PostItem(AjaxPostedData item)
+        {
+            return new JsonResult { Data = new { PostedItem = item } };
+        }
+
+        public class AjaxPostedData
+        {
+            public string MySimpleValue { get; set; }
+            public Dictionary<string, string> MyStringDictionary { get; set; }
+            public Dictionary<string, MyComplexObject> MyComplexObjectDictionary { get; set; }
+            public MyComplexObject MyComplexObject { get; set; }
+            public string[] MyStringArray { get; set; }
+        }
+
+        public class MyComplexObject
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+        }
+
+        public class ComplexType
+        {
+            public ComplexType()
+            {
+                this.Name = "Foo";
+                this.Another = new SecondLevelComplexType();
+            }
+
+            public string Name { get; set; }
+            public SecondLevelComplexType Another { get; set; }
+        }
+
+        public class SecondLevelComplexType
+        {
+            public SecondLevelComplexType()
+            {
+                this.Name = "Bar";
+                this.Values = new ThirdLevelComplexType(this.Name);
+            }
+
+            public string Name { get; set; }
+            public ThirdLevelComplexType Values { get; set; }
+        }
+
+        public class ThirdLevelComplexType
+        {
+            public ThirdLevelComplexType(string valueFor)
+            {
+                this.Value1 = "Value 1 for " + valueFor;
+                this.Value2 = "Value 2 for " + valueFor;
+            }
+
+            public string Value1 { get; set; }
+            public string Value2 { get; set; }
         }
     }
 }
