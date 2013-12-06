c0995e869c741626900fe74abc0518844abf6179
Bryan J Hogan <bryan@bryanhogan.net>
2013-12-06 17:25:26 -0500

    Protect against expired cache entry

---
+++ source/Glimpse.AspNet/Model/CacheModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+
+namespace Glimpse.AspNet.Model
+{
+    public class CacheModel
+    {
+        public CacheModel()
+        {
+            CacheItems = new List<CacheItemModel>(); 
+        }
+
+        public long PhysicalMemoryLimit { get; set; }
+        public long PrivateBytesLimit { get; set; }
+        public IList<CacheItemModel> CacheItems { get; set; }
+
+    }
+
+    public class CacheItemModel
+    {
+        public string Key { get; set; }
+        public object Value { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? ExpiresOn { get; set; }
+        public TimeSpan? SlidingExpiration { get; set; }
+    }
+}
