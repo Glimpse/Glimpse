4139918c1b15ac7db85657e9b26034e3623fdbf3
Bryan J Hogan <bryan@bryanhogan.net>
2013-12-09 18:59:52 -0500

    Adding tests for cache

--- source/Glimpse.Test.AspNet/Tab/CacheShould.cs
+++ source/Glimpse.Test.AspNet/Tab/CacheShould.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web;
 using System.Web.Caching;
+using Glimpse.AspNet.Model;
 using Glimpse.Core.Extensibility;
 using Moq;
 using Xunit;
@@ -10,17 +11,49 @@ namespace Glimpse.Test.AspNet.Tab
     public class CacheShould
     {
         [Fact]
-        public void CacheItems()
+        public void CacheSlidingExpiration()
         {
             var contextMock = new Mock<ITabContext>();
             var cache = new Glimpse.AspNet.Tab.Cache();
+            var slidingExpiration = new TimeSpan(2, 0, 0);
 
-            HttpRuntime.Cache.Add("testItem", "testItemValue", null, DateTime.MaxValue, new TimeSpan(2, 0, 0),
+            HttpRuntime.Cache.Add("testItem", "testItemValue", null, Cache.NoAbsoluteExpiration, slidingExpiration,
                       CacheItemPriority.AboveNormal, null);
 
             var result = cache.GetData(contextMock.Object);
 
             Assert.NotNull(result);
+            Assert.NotNull(result as CacheModel);
+
+            var cacheModel = result as CacheModel;
+           
+            Assert.Equal(cacheModel.CacheItems.Count, 1);
+            Assert.Equal(cacheModel.CacheItems[0].SlidingExpiration, slidingExpiration);
+        }
+
+        [Fact]
+        public void CacheAbsoluteExpiration()
+        {
+            var contextMock = new Mock<ITabContext>();
+            var cache = new Glimpse.AspNet.Tab.Cache();
+            DateTime cacheExpiryDate = DateTime.Now.AddHours(6).ToUniversalTime();
+
+            HttpRuntime.Cache.Add("testItem", "testItemValue", null, cacheExpiryDate, Cache.NoSlidingExpiration,
+                      CacheItemPriority.AboveNormal, null);
+
+            var result = cache.GetData(contextMock.Object);
+
+            Assert.NotNull(result);
+            Assert.NotNull(result as CacheModel);
+
+            var cacheModel = result as CacheModel;
+
+            Assert.Equal(cacheModel.CacheItems.Count, 1);
+
+            var expiresOn = (DateTime)cacheModel.CacheItems[0].ExpiresOn;
+            Assert.Equal(expiresOn.ToUniversalTime(), cacheExpiryDate);
         }
     }
+
+
 }
