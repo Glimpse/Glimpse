7facbf4779591198569657f2d9667408f3ffc6a2
Nik Molnar <nikmd23@gmail.com>
2014-01-15 14:31:40 -0500

    Not moving to OwinHost yet. Added comment with explanation.

--- source/Glimpse.Owin.Sample/Program.cs
+++ source/Glimpse.Owin.Sample/Program.cs
@@ -5,6 +5,9 @@ namespace Glimpse.Owin.Sample
 {
     public class Program
     {
+        // Think about switching to the OwinHost NuGet package and deleting this file when we no longer care about supporting VS 2012
+        // http://blogs.msdn.com/b/webdev/archive/2013/09/11/visual-studio-2013-custom-web-servers-and-owinhost-exe.aspx
+
         public static void Main(string[] args)
         {
             using (WebApp.Start<Startup>("http://localhost:8080/"))
