90cc1c7c68fd42c6b959990b3b89686567426a37
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-01-28 08:35:59 -0500

    Allow more args to be set when interacting with the rendering engine directly

--- source/Glimpse.Core/glimpse.js
+++ source/Glimpse.Core/glimpse.js
@@ -749,8 +749,8 @@ glimpse.render.engine = (function($, pubsub) {
         register = function(name, engine) {
             providers[name] = engine;
         },
-        build = function(data, metadata) {
-            return providers.master.build(data, 0, true, metadata, 1);
+        build = function(data, metadata, level, forceFull) {
+            return providers.master.build(data, level || 0, (forceFull == undefined ? true : forceFull), metadata, 1);
         },
         insert = function(scope, data, metadata) {
             scope.html(build(data, metadata)); 
