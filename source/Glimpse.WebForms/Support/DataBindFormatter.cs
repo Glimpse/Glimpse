b7735a59b5fec2c4730732bb90513c61cecd26ac
Graham Mendick <graham.mendick@gmail.com>
2014-01-28 17:38:28 +0000

    Reformatted with spaces instead of tabs

--- source/Glimpse.WebForms/Support/DataBindFormatter.cs
+++ source/Glimpse.WebForms/Support/DataBindFormatter.cs
@@ -9,26 +9,26 @@ using System.Web;
 
 namespace Glimpse.WebForms.Support
 {
-	public class DataBindFormatter
-	{
+    public class DataBindFormatter
+    {
         public void Process(ControlTreeItemTrackModel root, IEnumerable<PageLifeCycleMessage> pageLifeCycleMessages)
-		{
+        {
             ProcessRecord(root, (Dictionary<string, List<DataBindParameterModel>>)HttpContext.Current.Items["_GlimpseWebFormDataBinding"], pageLifeCycleMessages.OrderByDescending(p => p.Offset));
-		}
+        }
 
         private void ProcessRecord(ControlTreeItemTrackModel item, Dictionary<string, List<DataBindParameterModel>> dataBindInfo, IEnumerable<PageLifeCycleMessage> pageLifeCycleMessages)
-		{
-			if (dataBindInfo.ContainsKey(item.ControlId))
-			{
-				item.Record.DataBindParameters = new Dictionary<string, object>();
+        {
+            if (dataBindInfo.ContainsKey(item.ControlId))
+            {
+                item.Record.DataBindParameters = new Dictionary<string, object>();
                 foreach (var parameterModel in dataBindInfo[item.ControlId])
-				{
+                {
                     var dataBindParameters = new List<DataBindParameter>();
                     var time = DateTime.MinValue;
                     foreach (var parameter in parameterModel.DataBindParameters)
-					{
+                    {
                         dataBindParameters.Add(parameter);
-					}
+                    }
                     var lifeCycleEvent = pageLifeCycleMessages.First(p => p.Offset <= parameterModel.Offset);
                     if (item.Record.DataBindParameters.ContainsKey(lifeCycleEvent.EventName))
                     {
@@ -46,12 +46,12 @@ namespace Glimpse.WebForms.Support
                     {
                         item.Record.DataBindParameters[lifeCycleEvent.EventName] = dataBindParameters;
                     }
-				}
-			}
-			foreach (var child in item.Children)
-			{
+                }
+            }
+            foreach (var child in item.Children)
+            {
                 ProcessRecord(child, dataBindInfo, pageLifeCycleMessages);
-			}
-		}
-	}
+            }
+        }
+    }
 }
