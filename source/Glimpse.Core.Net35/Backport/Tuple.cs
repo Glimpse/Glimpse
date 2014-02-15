1d134542b77e2e8e745c8807e9bfe3b31bd83668
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-02-15 16:58:31 -0500

    Update #740 - Add Tuple type for .net 3.5

---
+++ source/Glimpse.Core.Net35/Backport/Tuple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public class Tuple<T1, T2>
+    {
+        public T1 Item1 { get; private set; }
+
+        public T2 Item2 { get; private set; }
+
+        public Tuple(T1 item1, T2 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+    }
+}
