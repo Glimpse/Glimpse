8eccc1b7d9b25d3ae257fb945c6435738e3caad7
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-03-04 16:17:41 -0500

    Remove invalid namespace

--- source/Glimpse.Core/Metadata/HashMetadata.cs
+++ source/Glimpse.Core/Metadata/HashMetadata.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Glimpse.Core.Extensibility;
 using Glimpse.Core.Framework;
 
