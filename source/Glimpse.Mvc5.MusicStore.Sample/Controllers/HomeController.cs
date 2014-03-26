7c9bf0286c9caf392a12529d2ab1ee2719176f33
PaulAtkins <PaulAtkins@users.noreply.github.com>
2014-03-26 21:38:40 +0000

    Added Glimpse.Mvc5.MusicStore sample

---
+++ source/Glimpse.Mvc5.MusicStore.Sample/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MusicStoreEntities storeDB = new MusicStoreEntities();
+        //
+        // GET: /Home/
+
+        public async Task<ActionResult> Index()
+        {
+            // Get most popular albums
+            var albums = await GetTopSellingAlbums(6);
+            //var albums = GetTopSellingAlbums(6);
+
+            // Trigger some good old ADO code 
+            var albumCount = GetTotalAlbumns(); 
+            Trace.Write(string.Format("Total number of Albums = {0} and Albums with 'The' = {1}", albumCount.Item1, albumCount.Item2));
+
+            return View(albums);
+        }
+
+
+        private Task<List<Album>> GetTopSellingAlbums(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToListAsync();
+        }
+
+        private Tuple<int, int> GetTotalAlbumns()
+        {
+            var result1 = 0;
+            var result2 = 0;
+
+            var connectionString = ConfigurationManager.ConnectionStrings["MusicStoreEntities"];
+            var factory = DbProviderFactories.GetFactory(connectionString.ProviderName);
+            using (var connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString.ConnectionString;
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT COUNT(*) FROM Albums";
+                    command.CommandType = CommandType.Text;
+                    result1 = (int)command.ExecuteScalar();
+                }
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT COUNT(*) FROM Albums WHERE Title LIKE 'The%'";
+                    command.CommandType = CommandType.Text;
+                    result2 = (int)command.ExecuteScalar();
+                }
+            }
+
+            return new Tuple<int, int>(result1, result2);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            storeDB.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ source/Glimpse.Mvc5.MusicStore.Sample/Controllers/ModelBindingTestsController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class ModelBindingTestsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index", new ComplexType());
+        }
+
+        [HttpPost]
+        public ActionResult SimplePost(string firstname, string lastname)
+        {
+            return View("Index", new ComplexType());
+        }
+
+        [HttpPost]
+        public ActionResult ComplexPost(ComplexType complexType, string additionalSimpleValue)
+        {
+            return View("Index", new ComplexType());
+        }
+
+        [HttpPost]
+        public JsonResult PostItem(AjaxPostedData item)
+        {
+            return new JsonResult { Data = new { PostedItem = item } };
+        }
+
+        [HttpPost]
+        public ActionResult HtmlContainedPost(HtmlInputAllowedType htmlInputAllowedType)
+        {
+            return View("Index", new ComplexType());
+        }
+
+        public class AjaxPostedData
+        {
+            public string MySimpleValue { get; set; }
+            public Dictionary<string, string> MyStringDictionary { get; set; }
+            public Dictionary<string, MyComplexObject> MyComplexObjectDictionary { get; set; }
+            public MyComplexObject MyComplexObject { get; set; }
+            public string[] MyStringArray { get; set; }
+        }
+
+        public class MyComplexObject
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+        }
+
+        public class ComplexType
+        {
+            public ComplexType()
+            {
+                this.Name = "Foo";
+                this.Another = new SecondLevelComplexType();
+            }
+
+            public string Name { get; set; }
+            public SecondLevelComplexType Another { get; set; }
+        }
+
+        public class SecondLevelComplexType
+        {
+            public SecondLevelComplexType()
+            {
+                this.Name = "Bar";
+                this.Values = new ThirdLevelComplexType(this.Name);
+            }
+
+            public string Name { get; set; }
+            public ThirdLevelComplexType Values { get; set; }
+        }
+
+        public class ThirdLevelComplexType
+        {
+            public ThirdLevelComplexType(string valueFor)
+            {
+                this.Value1 = "Value 1 for " + valueFor;
+                this.Value2 = "Value 2 for " + valueFor;
+            }
+
+            public string Value1 { get; set; }
+            public string Value2 { get; set; }
+        }
+
+        public class HtmlInputAllowedType
+        {
+            [AllowHtml]
+            public string HtmlContent { get; set; }
+        }
+    }
+}
