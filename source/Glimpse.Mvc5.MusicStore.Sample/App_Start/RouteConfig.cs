6ab3dbb7e1fb218481261685ff088f66c10058a7
PaulAtkins <PaulAtkins@users.noreply.github.com>
2014-03-26 23:40:49 +0000

    Added Attribute Routing Test page

--- source/Glimpse.Mvc5.MusicStore.Sample/App_Start/RouteConfig.cs
+++ source/Glimpse.Mvc5.MusicStore.Sample/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace MvcMusicStore
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
