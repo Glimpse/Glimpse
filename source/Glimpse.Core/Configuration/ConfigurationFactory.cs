1d3f3988eb8e8a87acfd08c9919098ccd61797a8
Christophe Gijbels <git@gijbels-it.be>
2014-04-23 00:11:48 +0200

    Removed unneeded ConfigurationFactory methods

--- source/Glimpse.Core/Configuration/ConfigurationFactory.cs
+++ source/Glimpse.Core/Configuration/ConfigurationFactory.cs
@@ -20,47 +20,17 @@ namespace Glimpse.Core.Configuration
             IPersistenceStore persistenceStore,
             ICurrentGlimpseRequestIdTracker currentGlimpseRequestIdTracker = null)
         {
-            return Create(resourceEndpointConfiguration, persistenceStore, ConfigurationManager.GetSection("glimpse") as Section, currentGlimpseRequestIdTracker);
-        }
-
-        /// <summary>
-        /// Creates an <see cref="IConfiguration" /> based on a <see cref="Section" /> resolved with the <paramref name="xmlConfigurationSectionName"/> configuration section name
-        /// </summary>
-        /// <param name="resourceEndpointConfiguration">The resource endpoint configuration</param>
-        /// <param name="persistenceStore">The persistence store</param>
-        /// <param name="xmlConfigurationSectionName">The configuration section name</param>
-        /// <param name="currentGlimpseRequestIdTracker">An optional <see cref="ICurrentGlimpseRequestIdTracker"/> which will default to <see cref="CallContextCurrentGlimpseRequestIdTracker"/></param>
-        /// <returns>The <see cref="IConfiguration"/></returns>
-        public static IConfiguration Create(
-            ResourceEndpointConfiguration resourceEndpointConfiguration,
-            IPersistenceStore persistenceStore,
-            string xmlConfigurationSectionName,
-            ICurrentGlimpseRequestIdTracker currentGlimpseRequestIdTracker = null)
-        {
-            return Create(resourceEndpointConfiguration, persistenceStore, ConfigurationManager.GetSection(xmlConfigurationSectionName) as Section, currentGlimpseRequestIdTracker);
-        }
-
-        /// <summary>
-        /// Creates an <see cref="IConfiguration" /> based on the given <see cref="Section" />
-        /// </summary>
-        /// <param name="resourceEndpointConfiguration">The resource endpoint configuration</param>
-        /// <param name="persistenceStore">The persistence store</param>
-        /// <param name="xmlConfigurationSection">The configuration section</param>
-        /// <param name="currentGlimpseRequestIdTracker">An optional <see cref="ICurrentGlimpseRequestIdTracker"/> which will default to <see cref="CallContextCurrentGlimpseRequestIdTracker"/></param>
-        /// <returns>The <see cref="IConfiguration"/></returns>
-        public static IConfiguration Create(
-            ResourceEndpointConfiguration resourceEndpointConfiguration,
-            IPersistenceStore persistenceStore,
-            Section xmlConfigurationSection,
-            ICurrentGlimpseRequestIdTracker currentGlimpseRequestIdTracker = null)
-        {
-#warning based on the Section we could allow another IConfigurationSettingsProvider to be set, so that the complete configuration could be loaded from somewhere else... (DB, File, ...)
-
             return new Configuration(
-                new ConfigurationSettingsXmlProvider(xmlConfigurationSection).GetConfigurationSettings(
+                DetermineConfigurationSettingsProvider().GetConfigurationSettings(
                     resourceEndpointConfiguration,
                     persistenceStore,
                     currentGlimpseRequestIdTracker ?? new CallContextCurrentGlimpseRequestIdTracker()));
+        }
+
+        private static IConfigurationSettingsProvider DetermineConfigurationSettingsProvider()
+        {
+#warning based on the Section we could allow another IConfigurationSettingsProvider to be set, so that the complete configuration could be loaded from somewhere else... (DB, File, ...)
+            return new ConfigurationSettingsXmlProvider(ConfigurationManager.GetSection("glimpse") as Section);
         }
     }
 }
