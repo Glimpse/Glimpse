463582b2e5496d8b1fad514a58ce7e20c4ab02f7
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-06-25 14:53:58 -0400

    Update #813 - Adjust content of error message

--- source/Glimpse.Core/Resource/PopupRedirectResource.cs
+++ source/Glimpse.Core/Resource/PopupRedirectResource.cs
@@ -79,7 +79,7 @@ namespace Glimpse.Core.Resource
             //TODO: Can't assume this is here 
             var request = context.PersistenceStore.GetTop(1).FirstOrDefault(); 
             if (request == null)
-                return new HtmlResourceResult("<html><body><h1>Sorry no requires are current available</h1></body></body>");
+                return new HtmlResourceResult("<html><body><h1>Sorry no requests are currently available</h1>This is a current limitation of this feature. For Glimpse client to work, Glimpse has to detect at least one requesting for which it is enabled.<br /><br />If you are using this feature and it is causing you issues, please let us know.</body></html>");
 
             var popupResource = configuration.Resources.FirstOrDefault(r => r.Name.Equals("glimpse_popup", StringComparison.InvariantCultureIgnoreCase));
             var popupUriTemplate = configuration.ResourceEndpoint.GenerateUriTemplate(popupResource, configuration.EndpointBaseUri, configuration.Logger);
