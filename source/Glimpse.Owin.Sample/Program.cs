3464f6b19934a1f2b591b695b9163756dc682ca7
Christophe Gijbels <git@gijbels-it.be>
2014-07-20 12:33:29 +0200

    made OWIN sample port selection rely on user input

--- source/Glimpse.Owin.Sample/Program.cs
+++ source/Glimpse.Owin.Sample/Program.cs
@@ -10,9 +10,22 @@ namespace Glimpse.Owin.Sample
 
         public static void Main(string[] args)
         {
-            using (WebApp.Start<Startup>("http://localhost:8090/"))
+            int port;
+            do
             {
-                Console.WriteLine("Started at http://localhost:8090/");
+                Console.Write("Please enter a port for the web application : ");
+                var enteredPort = Console.ReadLine();
+                if (!int.TryParse(enteredPort, out port))
+                {
+                    Console.WriteLine("Invalid port");
+                }
+            } while (port == 0);
+
+            using (WebApp.Start<Startup>("http://localhost:" + port))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Started at http://localhost:" + port);
+                Console.WriteLine("Press <ENTER> to stop the application");
                 Console.ReadLine();
                 Console.WriteLine("Stopping");
             }
