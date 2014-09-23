50a27087872e2bb9c9a74d925f88693fce948f7c
Christophe Gijbels <git@gijbels-it.be>
2014-09-23 23:49:50 +0200

    made the xml provider work with an XmlDocument instead of the Section

--- source/Glimpse.Core/Configuration/ConfigurationSettingsXmlProvider.cs
+++ source/Glimpse.Core/Configuration/ConfigurationSettingsXmlProvider.cs
@@ -1,22 +1,26 @@
-﻿using Glimpse.Core.Framework;
+﻿using System;
+using System.Linq;
+using System.Xml;
+using Glimpse.Core.Extensibility;
+using Glimpse.Core.Framework;
 
 namespace Glimpse.Core.Configuration
 {
     /// <summary>
-    /// Implementation of an <see cref="IConfigurationSettingsProvider" /> that provides <see cref="ConfigurationSettings" /> based on a <see cref="Section"/> instance
+    /// Implementation of an <see cref="IConfigurationSettingsProvider" /> that provides <see cref="ConfigurationSettings" /> based on a <see cref="XmlDocument"/> instance
     /// </summary>
-    public class ConfigurationSettingsXmlProvider : IConfigurationSettingsProvider
+    internal class ConfigurationSettingsXmlProvider : IConfigurationSettingsProvider
     {
-        private Section XmlConfiguration { get; set; }
+        private XmlDocument ConfigurationDocument { get; set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ConfigurationSettingsXmlProvider" /> class.
         /// </summary>
-        /// <param name="xmlConfiguration">The xml configuration</param>
-        public ConfigurationSettingsXmlProvider(Section xmlConfiguration)
+        /// <param name="configurationDocument">The configuration document</param>
+        public ConfigurationSettingsXmlProvider(XmlDocument configurationDocument)
         {
-            Guard.ArgumentNotNull(xmlConfiguration, "xmlConfiguration");
-            XmlConfiguration = xmlConfiguration;
+            Guard.ArgumentNotNull(configurationDocument, "configurationDocument");
+            ConfigurationDocument = configurationDocument;
         }
 
         /// <summary>
@@ -35,25 +39,49 @@ namespace Glimpse.Core.Configuration
             Guard.ArgumentNotNull(persistenceStore, "persistenceStore");
             Guard.ArgumentNotNull(currentGlimpseRequestIdTracker, "currentGlimpseRequestIdTracker");
 
-            var collectionSettingsFactory = new CollectionSettingsFactory(XmlConfiguration.DiscoveryLocation);
+            var collectionSettingsFactory = new CollectionSettingsFactory(ConfigurationDocument.GetAttributeValue("discoveryLocation", false, "bin\\debug"));
 
             return new ConfigurationSettings(
                resourceEndpointConfiguration,
                persistenceStore,
                currentGlimpseRequestIdTracker,
-               XmlConfiguration.DefaultRuntimePolicy,
-               XmlConfiguration.EndpointBaseUri,
-               new LoggingSettings(XmlConfiguration.Logging.Level, XmlConfiguration.Logging.LogLocation),
-               collectionSettingsFactory.Create(XmlConfiguration.ClientScripts.XmlContent),
-               collectionSettingsFactory.Create(XmlConfiguration.Inspectors.XmlContent),
-               collectionSettingsFactory.Create(XmlConfiguration.Resources.XmlContent),
-               collectionSettingsFactory.Create(XmlConfiguration.RuntimePolicies.XmlContent),
-               collectionSettingsFactory.Create(XmlConfiguration.Tabs.XmlContent),
-               collectionSettingsFactory.Create(XmlConfiguration.SerializationConverters.XmlContent),
-               collectionSettingsFactory.Create(XmlConfiguration.Metadata.XmlContent),
-               collectionSettingsFactory.Create(XmlConfiguration.TabMetadata.XmlContent),
-               collectionSettingsFactory.Create(XmlConfiguration.Displays.XmlContent),
-               collectionSettingsFactory.Create(XmlConfiguration.InstanceMetadata.XmlContent));
+               ConfigurationDocument.GetAttributeValueAsEnum("defaultRuntimePolicy", false, RuntimePolicy.On),
+               ConfigurationDocument.GetAttributeValue("endpointBaseUri", false, "~/Glimpse.axd"),
+               CreateLoggingSettings(),
+               collectionSettingsFactory.Create(GetXmlElementOrDefault("clientScripts")),
+               collectionSettingsFactory.Create(GetXmlElementOrDefault("inspectors")),
+               collectionSettingsFactory.Create(GetXmlElementOrDefault("resources")),
+               collectionSettingsFactory.Create(GetXmlElementOrDefault("runtimePolicies")),
+               collectionSettingsFactory.Create(GetXmlElementOrDefault("tabs")),
+               collectionSettingsFactory.Create(GetXmlElementOrDefault("serializationConverters")),
+               collectionSettingsFactory.Create(GetXmlElementOrDefault("metadata")),
+               collectionSettingsFactory.Create(GetXmlElementOrDefault("tabMetadata")),
+               collectionSettingsFactory.Create(GetXmlElementOrDefault("displays")),
+               collectionSettingsFactory.Create(GetXmlElementOrDefault("instanceMetadata")));
+        }
+
+        private LoggingSettings CreateLoggingSettings()
+        {
+            var loggingElement = GetXmlElementOrDefault("logging");
+            return loggingElement != null
+                            ? new LoggingSettings(
+                                loggingElement.GetAttributeValueAsEnum("level", false, LoggingLevel.Trace),
+                                loggingElement.GetAttributeValue("logLocation", false, string.Empty))
+                            : new LoggingSettings(LoggingLevel.Trace, string.Empty);
+
+        }
+
+        private XmlElement GetXmlElementOrDefault(string elementName)
+        {
+            try
+            {
+                return ConfigurationDocument.GetElementsByTagName(elementName).OfType<XmlElement>().SingleOrDefault();
+            }
+            catch (Exception)
+            {
+#warning log exception
+                return null;
+            }
         }
     }
 }
---
+++ source/Glimpse.Core/Configuration/XmlNodeExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+using Glimpse.Core.Framework;
+
+namespace Glimpse.Core.Configuration
+{
+    internal static class XmlNodeExtensions
+    {
+        public static string GetAttributeValue(this XmlNode xmlNode, string attributeName, bool required, string defaultValue = null, string messageOnMissingAttribute = null)
+        {
+            XmlAttribute attribute = xmlNode.Attributes != null ? xmlNode.Attributes[attributeName] : null;
+            if (attribute != null)
+            {
+                return attribute.Value;
+            }
+            else if (required)
+            {
+                throw new GlimpseException(messageOnMissingAttribute ?? "'" + attributeName + "' attribute missing");
+            }
+
+            return defaultValue;
+        }
+
+        public static Type GetTypeFromTypeAttribute(this XmlNode xmlNode, bool required, string messageOnMissingTypeAttribute = null)
+        {
+            XmlAttribute typeAttribute = xmlNode.Attributes != null ? xmlNode.Attributes["type"] : null;
+            if (typeAttribute != null)
+            {
+                return Type.GetType(typeAttribute.Value, true, true);
+            }
+            else if (required)
+            {
+                throw new GlimpseException(messageOnMissingTypeAttribute ?? "type attribute missing");
+            }
+
+            return null;
+        }
+
+        public static TEnum GetAttributeValueAsEnum<TEnum>(this XmlNode xmlNode, string attributeName, bool required, TEnum defaultValue = default(TEnum), string messageOnMissingAttribute = null)
+            where TEnum : struct
+        {
+            var attributeValue = GetAttributeValue(xmlNode, attributeName, required, defaultValue.ToString(), messageOnMissingAttribute);
+            return (TEnum)Enum.Parse(typeof(TEnum), attributeValue);
+        }
+    }
+}
