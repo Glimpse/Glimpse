e1d93d3cd5e730c2c2c9b2fce0eeb6c0b0a5916c
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-10-23 08:32:17 -0700

    Add updates for AssemblyInfo that got missed in previous commit

--- source/Glimpse.AspNet/Properties/AssemblyInfo.cs
+++ source/Glimpse.AspNet/Properties/AssemblyInfo.cs
@@ -4,21 +4,20 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Glimpse.Core.Extensibility;
 
+[assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
-[assembly: Guid("751c5691-28de-4b8d-ba4d-dde77a91efa9")]
+[assembly: Guid("251c5691-28de-4b8d-ba4d-dde77a91efa9")]
 
 [assembly: AssemblyTitle("Glimpse for ASP.NET Assembly")]
-[assembly: AssemblyDescription("Main extensibility implementations for running Glimpse with ASP.NET.")]
+[assembly: AssemblyDescription("Glimpse extensions and tabs for ASP.NET.")]
 [assembly: AssemblyProduct("Glimpse.AspNet")]
-[assembly: AssemblyCopyright("© 2012 Nik Molnar & Anthony van der Hoorn")]
+[assembly: AssemblyCopyright("© 2015 Nik Molnar & Anthony van der Hoorn")]
 [assembly: AssemblyTrademark("Glimpse™")]
 
-// Version is in major.minor.build format to support http://semver.org/
-// Keep these three attributes in sync
-[assembly: AssemblyVersion("1.8.1")]
-[assembly: AssemblyFileVersion("1.8.1")]
-[assembly: AssemblyInformationalVersion("1.8.1")] // Used to specify the NuGet version number at build time
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0-alpha0")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha0")] // Used to specify the NuGet version number at build time
 
-[assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("Glimpse.Test.AspNet")]
+
 [assembly: NuGetPackage("Glimpse.AspNet")]
