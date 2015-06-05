44bf201c90cfe1ee133883ba9fe658bc64127959
David Gardiner <david@gardiner.net.au>
2015-06-05 12:21:05 +0930

    Use DateTimeOffset and obtain full time zone offset (include minutes component instead of assuming time zones are always on the hour) Label 'UTC' instead of 'GMT'

--- source/Glimpse.AspNet/Display/EnvironmentDisplay.cs
+++ source/Glimpse.AspNet/Display/EnvironmentDisplay.cs
@@ -14,14 +14,14 @@ namespace Glimpse.AspNet.Display
             get { return InternalName; }
         }
 
-        public string Key 
+        public string Key
         {
             get { return InternalName; }
         }
 
         public object GetData(ITabContext context)
         {
-            var time = DateTime.Now;
+            var time = DateTimeOffset.Now;
             var timeZoneInfo = TimeZoneInfo.Local;
             var isDaylightSavingTime = timeZoneInfo.IsDaylightSavingTime(time);
 
@@ -29,8 +29,8 @@ namespace Glimpse.AspNet.Display
                 {
                     serverName = Environment.MachineName,
                     user = Thread.CurrentPrincipal.Identity.Name,
-                    serverTime = time,
-                    serverTimezoneOffset = time.ToString("zz00"), // timeZoneInfo.BaseUtcOffset.Hours + (isDaylightSavingTime ? 1 : 0),
+                    serverTime = time.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff"),
+                    serverTimezoneOffset = time.ToString("zzz"),
                     serverDaylightSavingTime = isDaylightSavingTime
                 };
         }
